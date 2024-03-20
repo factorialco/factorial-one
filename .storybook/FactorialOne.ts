5a352aac43d34924335e92cb7de1923545adbfdd
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-03-20 17:59:36 +0100

    Customize theme

---
+++ .storybook/FactorialOne.ts
@@ -0,0 +1,36 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  brandTitle: "Factorial One",
+  brandUrl: "https://example.com",
+  brandImage: "/one-small.png",
+  brandTarget: "_self",
+
+  //
+  colorPrimary: "#3A10E5",
+  colorSecondary: "#585C6D",
+
+  // UI
+  appBg: "#ffffff",
+  appContentBg: "#ffffff",
+  appPreviewBg: "#ffffff",
+  appBorderColor: "#ffffff",
+  appBorderRadius: 4,
+
+  // Text colors
+  textColor: "#10162F",
+  textInverseColor: "#ffffff",
+
+  // Toolbar default and active colors
+  barTextColor: "#9E9E9E",
+  barSelectedColor: "#585C6D",
+  barHoverColor: "#585C6D",
+  barBg: "#ffffff",
+
+  // Form colors
+  inputBg: "#ffffff",
+  inputBorder: "#10162F",
+  inputTextColor: "#10162F",
+  inputBorderRadius: 2,
+});
---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/manager-api";
+import factorialOne from "./FactorialOne";
+
+addons.setConfig({
+  theme: factorialOne,
+});
