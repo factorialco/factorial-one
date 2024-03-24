2f5fec0c3ecd16c95ecb58e9e8ced28b6ba7e827
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-03-24 13:41:16 +0100

    Add form example

---
+++ src/examples/forms.stories.tsx
@@ -0,0 +1,81 @@
+import { Button } from "@/foundations/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/foundations/form";
+import { Input } from "@/foundations/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+interface Props {
+  onSubmit: (data: z.infer<typeof formSchema>) => void;
+}
+
+const meta: Meta<Props> = {
+  title: "Examples / Forms",
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    onSubmit: fn(),
+  },
+};
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    onSubmit: fn(),
+  },
+  render: ({ onSubmit }) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        username: "",
+      },
+    });
+
+    return (
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit((values) => onSubmit(values))}
+          className="space-y-8"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    );
+  },
+};
