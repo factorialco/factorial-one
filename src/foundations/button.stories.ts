255bc6c743606c9d979a71d0b73a3313b1fbe392
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-03-25 16:40:03 +0100

    Remove unused icon

--- src/foundations/button.stories.ts
+++ src/foundations/button.stories.ts
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 import { Button, sizes, variants } from "./button";
-import { Mail } from "lucide-react";
 
 const meta = {
   component: Button,
