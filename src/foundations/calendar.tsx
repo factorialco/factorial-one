30c0683463de0de3bc494534a2ec24efc9a15c89
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-03-28 10:46:39 +0100

    Calendar styling

--- src/foundations/calendar.tsx
+++ src/foundations/calendar.tsx
@@ -34,11 +34,12 @@ function Calendar({
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
-        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "rounded-full h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-start)]:rounded-r-none [&:has([aria-selected].day-range-end)]:rounded-l-none [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent/70 first:[&:has([aria-selected])]:rounded-r-none last:[&:has([aria-selected])]:rounded-l-none focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
         ),
+        day_range_start: "day-range-start",
         day_range_end: "day-range-end",
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
@@ -47,7 +48,7 @@ function Calendar({
           "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+          "rounded-none aria-selected:bg-accent/70 aria-selected:text-accent-foreground [&:has([aria-selected])]:rounded-l-full last:[&:has([aria-selected])]:rounded-r-full",
         day_hidden: "invisible",
         ...classNames,
       }}
