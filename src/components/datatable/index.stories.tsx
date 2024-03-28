888b5667954fa4e04682eafe5a55963acbe30280
Ray García <ray@rayoplateado.com>
2024-03-28 19:05:18 +0100

    first datatable version. Same as demo

---
+++ src/components/datatable/index.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { DataTable } from "./";
+
+const meta: Meta = {
+    component: DataTable,
+    title: "Components / DataTable",
+    parameters: {
+        tags: ["autodocs"],
+    },
+}
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
