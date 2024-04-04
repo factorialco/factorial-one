68d7a3f6a93fa5b5126a158d088e92b32fd0311b
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-04-04 12:54:37 +0200

    Rename application folder

--- src/examples/layouts/application/index.stories.tsx
+++ src/examples/layouts/application/index.stories.tsx
@@ -1,13 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Layout from "./layout";
+import Layout from ".";
 
 const meta: Meta = {
   component: Layout,
-  title: "Examples / Layout",
   parameters: {
     tags: ["autodocs"],
   },
-}
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
