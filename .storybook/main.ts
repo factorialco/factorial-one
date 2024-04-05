1faddd17cb233a0ab55d433b86eb950114bbe80e
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-04-05 15:00:31 +0200

    Better organize docs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from "@storybook/react-vite"
 
 const config: StorybookConfig = {
   stories: [
+    "../docs/Introduction.mdx",
     "../docs/**/*.mdx",
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
