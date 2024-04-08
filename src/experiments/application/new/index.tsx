3e31f802fd421e38bbc3ab9494feeaead9ddfe24
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-04-08 09:13:25 +0200

    Pages

--- src/experiments/application/new/index.tsx
+++ src/experiments/application/new/index.tsx
@@ -1,24 +1,45 @@
 import React, { useState } from "react"
 
 import { cn } from "@/lib/utils"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/foundations/tooltip"
 
 import { Pages } from "./pages"
 
 const Navigation = ({ activeItem, setActiveItem }) => (
   <div className="flex flex-col gap-1">
-    {Pages.map((item) => (
-      <div
-        className={cn(
-          "flex justify-center items-center w-12 h-12 text-secondary-foreground rounded-2xl transition-colors hover:cursor-pointer",
-          activeItem.title === item.title
-            ? "bg-secondary-foreground/5 border"
-            : "hover:bg-secondary"
-        )}
-        key={item.title}
-        onClick={() => setActiveItem(item)}
-      >
-        <item.icon size={20} />
-      </div>
+    {Pages.map((item, index) => (
+      <Tooltip key={index} delayDuration={200}>
+        <TooltipTrigger asChild>
+          <div
+            className={cn(
+              "relative flex justify-center items-center w-12 h-12 text-secondary-foreground rounded-2xl transition-colors hover:cursor-pointer",
+              activeItem.title === item.title
+                ? "bg-secondary-foreground/5"
+                : "hover:bg-secondary"
+            )}
+            key={item.title}
+            onClick={() => setActiveItem(item)}
+          >
+            <item.icon size={20} />
+            {item.title === "Inbox" && (
+              <div className="absolute text-xs bg-destructive-intermediate text-background flex justify-center items-center min-w-5 h-5 top-0 right-0 rounded-md px-1">
+                3
+              </div>
+            )}
+          </div>
+        </TooltipTrigger>
+        <TooltipContent
+          side="right"
+          className="bg-secondary-foreground text-background rounded-lg"
+        >
+          {item.title}
+        </TooltipContent>
+      </Tooltip>
     ))}
   </div>
 )
@@ -43,50 +64,52 @@ const Layout = () => {
   }
 
   return (
-    <div className="-m-4 bg-secondary/60 min-h-screen h-screen py-4 pr-4 grid grid-cols-1 md:grid-cols-[80px_1fr]">
-      <div className="flex flex-col gap-1 mx-4 pt-3">
-        <div className="flex justify-center items-center w-12 h-12 text-secondary-foreground">
-          A
-        </div>
-        <Navigation
-          activeItem={activeItem}
-          setActiveItem={handleSetActiveItem}
-        />
-      </div>
-      <div
-        className={cn(
-          "bg-card/40 rounded-2xl grid grid-cols-1 shadow-sm border",
-          hasSubItems(activeItem) ? "md:grid-cols-[270px_1fr]" : ""
-        )}
-      >
-        {hasSubItems(activeItem) && (
-          <div>
-            <div className="w-full h-[72px] flex items-center px-6 text-xl text-secondary-foreground">
-              {activeItem.title}
-            </div>
-            <div className="flex flex-col gap-1 px-4">
-              {activeItem.subItems.map((subItem) => (
-                <div
-                  className={cn(
-                    "h-10 flex items-center px-4 py-2 text-secondary-foreground rounded-xl transition-colors hover:cursor-pointer",
-                    subItem === activeSubItem
-                      ? "bg-secondary-foreground/5 text-foreground"
-                      : "hover:bg-secondary"
-                  )}
-                  key={subItem.title}
-                  onClick={() => setActiveSubItem(subItem)}
-                >
-                  {subItem.title}
-                </div>
-              ))}
-            </div>
+    <TooltipProvider>
+      <div className="-m-4 bg-secondary/60 min-h-screen h-screen py-4 pr-4 grid grid-cols-1 md:grid-cols-[80px_1fr]">
+        <div className="flex flex-col gap-1 mx-4 pt-3">
+          <div className="flex justify-center items-center w-12 h-12 text-secondary-foreground">
+            A
           </div>
-        )}
-        <div className="bg-card rounded-2xl">
-          {activeSubItem?.component || activeItem?.component}
+          <Navigation
+            activeItem={activeItem}
+            setActiveItem={handleSetActiveItem}
+          />
+        </div>
+        <div
+          className={cn(
+            "bg-card/40 rounded-2xl grid grid-cols-1 shadow-sm border",
+            hasSubItems(activeItem) ? "md:grid-cols-[270px_1fr]" : ""
+          )}
+        >
+          {hasSubItems(activeItem) && (
+            <div>
+              <div className="w-full h-[72px] flex items-center px-6 text-xl text-secondary-foreground">
+                {activeItem.title}
+              </div>
+              <div className="flex flex-col gap-1 px-4">
+                {activeItem.subItems.map((subItem) => (
+                  <div
+                    className={cn(
+                      "h-10 flex items-center px-4 py-2 text-secondary-foreground rounded-xl transition-colors hover:cursor-pointer",
+                      subItem === activeSubItem
+                        ? "bg-secondary-foreground/10 text-foreground"
+                        : "hover:bg-secondary"
+                    )}
+                    key={subItem.title}
+                    onClick={() => setActiveSubItem(subItem)}
+                  >
+                    {subItem.title}
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+          <div className="bg-card rounded-2xl">
+            {activeSubItem?.component || activeItem?.component}
+          </div>
         </div>
       </div>
-    </div>
+    </TooltipProvider>
   )
 }
 
--- src/experiments/application/new/pages.tsx
+++ src/experiments/application/new/pages.tsx
@@ -7,10 +7,42 @@ import {
   Store,
   Target,
   UserRound,
+  UsersRound,
 } from "lucide-react"
 
+const PageTitle: React.FC<{ title: string }> = ({ title }) => {
+  return (
+    <div className="w-full h-[72px] flex items-center px-6 text-xl text-secondary-foreground">
+      {title}
+    </div>
+  )
+}
+
 const PageHome: React.FC = () => {
-  return <div>This is the homepage!</div>
+  return (
+    <div>
+      <PageTitle title="Home" />
+      <p>This is the homepage!</p>
+    </div>
+  )
+}
+
+const PageInbox: React.FC = () => {
+  return (
+    <div>
+      <PageTitle title="Inbox" />
+      <p>This is the inbox!</p>
+    </div>
+  )
+}
+
+const PageEmployees: React.FC = () => {
+  return (
+    <div>
+      <PageTitle title="Employees" />
+      <p>These are the employees...</p>
+    </div>
+  )
 }
 
 export const Pages = [
@@ -23,7 +55,7 @@ export const Pages = [
   {
     title: "Inbox",
     icon: Inbox,
-    component: "Inbox Component content",
+    component: <PageInbox />,
     subItems: [],
   },
   {
@@ -38,6 +70,25 @@ export const Pages = [
       {
         title: "Personal",
         component: "Profile SubMenu two component content",
+      },
+    ],
+  },
+  {
+    title: "People",
+    icon: UsersRound,
+    component: "",
+    subItems: [
+      {
+        title: "Employees",
+        component: <PageEmployees />,
+      },
+      {
+        title: "Teams",
+        component: "These are the teams...",
+      },
+      {
+        title: "Jobs",
+        component: "These are the jobs...",
       },
     ],
   },
