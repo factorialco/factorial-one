f50166b9ad25518a66a7bc4d14a652a3d1170220
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-04-12 16:16:14 +0200

    Badge design

--- lib/foundations/badge.tsx
+++ lib/foundations/badge.tsx
@@ -9,12 +9,13 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
-        secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "border-secondary-intermediate bg-transparent text-secondary-foreground",
+        neutral: "border-transparent bg-secondary text-secondary-foreground",
         destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        outline: "text-foreground",
+          "border-transparent bg-destructive text-destructive-foreground",
+        positive: "border-transparent bg-positive text-positive-foreground",
+        warning: "border-transparent bg-warning text-warning-foreground",
+        info: "border-transparent bg-info text-info-foreground",
       },
     },
     defaultVariants: {
