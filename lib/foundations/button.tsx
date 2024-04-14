55926e58844ee9defa00c6c788f57b8724ab2ff5
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-04-14 18:05:39 +0200

    Fix styles

--- lib/foundations/button.tsx
+++ lib/foundations/button.tsx
@@ -15,7 +15,7 @@ export const variants = [
 export const sizes = ["default", "sm", "icon", "icon-sm"] as const
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-full border-solid text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
