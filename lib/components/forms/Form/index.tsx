4c10282e1093ef8eb1816859426068774b2ef682
Marc Fuentes Montenegro <mfm@marc.fuentes>
2024-04-18 07:27:13 +0200

    Workaround TS error

--- lib/components/forms/Form/index.tsx
+++ lib/components/forms/Form/index.tsx
@@ -2,17 +2,17 @@ import { Button } from "@/foundations/button"
 import { Form as FormProvider } from "@/foundations/form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { PropsWithChildren } from "react"
-import { SubmitHandler, useForm } from "react-hook-form"
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
 import useFormSchema from "../hooks/useFormSchema"
 
-interface Props<T extends object> {
+interface Props<T extends FieldValues> {
   onSubmit: SubmitHandler<T>
 }
 
-function Form<T extends object>({
+const Form = <T extends FieldValues>({
   children,
   onSubmit,
-}: PropsWithChildren<Props<T>>) {
+}: PropsWithChildren<Props<T>>) => {
   const { schema, defaultValues } = useFormSchema(children)
 
   const form = useForm({
@@ -20,9 +20,12 @@ function Form<T extends object>({
     defaultValues,
   })
 
+  // Type assertion to fix error TS2345 "T could be instantiated with a different subtype of constraint". Need to investigate
+  const handleSubmit = form.handleSubmit(onSubmit as SubmitHandler<FieldValues>)
+
   return (
     <FormProvider {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit}>
         {children}
         <Button type="submit">Submit</Button>
       </form>
