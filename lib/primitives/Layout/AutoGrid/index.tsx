4efb4380518ef56ac619ff19a0cf9febb2d9c010
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-05-25 18:01:32 +0200

    Adjust size

--- lib/primitives/Layout/AutoGrid/index.tsx
+++ lib/primitives/Layout/AutoGrid/index.tsx
@@ -12,9 +12,9 @@ const contentVariants = cva("grid grid-cols-1", {
       //
       // This makes sure that everything will look nice when using pages of 48
       // elements, it will always result in even rows.
-      sm: "@10xl:grid-cols-8 @11xl:grid-cols-12 @12xl:grid-cols-16 @8xl:grid-cols-6 @md:grid-cols-2 @2xl:grid-cols-3 @4xl:grid-cols-4",
-      md: "@10xl:grid-cols-8 @12xl:grid-cols-12 @9xl:grid-cols-6 @lg:grid-cols-2 @4xl:grid-cols-3 @7xl:grid-cols-4",
-      lg: "@12xl:grid-cols-8 @10xl:grid-cols-6 @9xl:grid-cols-4 @xl:grid-cols-2 @7xl:grid-cols-3",
+      sm: "@12xl:grid-cols-16 @md:grid-cols-2 @2xl:grid-cols-3 @4xl:grid-cols-4 @8xl:grid-cols-6 @10xl:grid-cols-8 @11xl:grid-cols-12",
+      md: "@lg:grid-cols-2 @4xl:grid-cols-3 @7xl:grid-cols-4 @9xl:grid-cols-6 @10xl:grid-cols-8 @12xl:grid-cols-12",
+      lg: "@3xl:grid-cols-2 @7xl:grid-cols-3 @9xl:grid-cols-4 @10xl:grid-cols-6 @12xl:grid-cols-8",
     },
     gap: {
       ...gaps,
