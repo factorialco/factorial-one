ec955b11ca95ebb75d6db2a58a0f31499473d9aa
Josep Jaume Rey <josepjaume@gmail.com>
2024-05-28 16:59:17 +0200

    Add layout primitives (#155)

--- lib/primitives/Layout/shared.ts
+++ lib/primitives/Layout/shared.ts
@@ -3,3 +3,14 @@ export const gaps = {
   md: "gap-4",
   lg: "gap-8",
 } as const
+
+export const flexItemVariants = {
+  grow: {
+    true: "grow",
+    false: "grow-0",
+  },
+  shrink: {
+    true: "shrink",
+    false: "shrink-0",
+  },
+} as const
