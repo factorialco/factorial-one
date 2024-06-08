d8e554c4b0855809ab0acd663d065d7f9e02f6e9
Roger <rogermoreta@gmail.com>
2024-06-08 13:24:00 +0200

    adjustments + story expanded with comparison + horizontal scroll (#193)
    * adjustments + story expanded with comparison
    
    * ignore yaml from pnpm lock
    
    * added horizontal scroll

--- lib/components/Utilities/ScrollArea/index.stories.tsx
+++ lib/components/Utilities/ScrollArea/index.stories.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/Actions/Button"
 import { ScrollArea } from "."
 
+import { Alert, AlertTitle } from "@/components/Information/Alert"
+import { Split } from "@/components/Layout/Split"
 import { Stack } from "@/components/Layout/Stack"
 import { Placeholder } from "@/lib/storybook-utils"
 import type { Meta, StoryObj } from "@storybook/react"
@@ -20,24 +22,74 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {},
   render: (props) => (
-    <Stack overflow="hidden" className="h-96">
+    <Stack className="h-96" overflow={`hidden`} gap="4">
       <ScrollArea {...props}>
-        <Stack gap="4">
-          <Button
-            label="Some button"
-            onClick={() =>
-              alert(
-                "This button is needed so the content can be focused for accessibility purposes."
-              )
-            }
-          />
-          {Array.from({ length: 20 }).map((_, i) => (
-            <Placeholder key={i}>
-              Element {i + 1} in a scrollable placeholder
-            </Placeholder>
-          ))}
-        </Stack>
+        <StackWithOverflow />
       </ScrollArea>
     </Stack>
   ),
 }
+
+export const Horizontal: Story = {
+  args: {},
+  render: (props) => (
+    <Stack className="w-48" overflow={`hidden`} gap="4">
+      <ScrollArea {...props}>
+        <StackWithOverflow length={4} />
+      </ScrollArea>
+    </Stack>
+  ),
+}
+
+export const Both: Story = {
+  args: {},
+  render: (props) => (
+    <Stack className="h-96 w-48" overflow={`hidden`} gap="4">
+      <ScrollArea {...props}>
+        <StackWithOverflow />
+      </ScrollArea>
+    </Stack>
+  ),
+}
+
+export const Comparison: Story = {
+  args: {},
+  render: (props) => (
+    <Split className="h-96" gap={"8"}>
+      <Stack className="h-full" overflow={`hidden`} gap="4">
+        <Alert variant="positive">
+          <AlertTitle>New ScrollArea Component</AlertTitle>
+        </Alert>
+        <ScrollArea {...props}>
+          <StackWithOverflow />
+        </ScrollArea>
+      </Stack>
+      <Stack className="h-full" overflow={`hidden`} gap="4">
+        <Alert variant="warning">
+          <AlertTitle>Default ScrollBar</AlertTitle>
+        </Alert>
+        <Stack overflow={`auto`}>
+          <StackWithOverflow />
+        </Stack>
+      </Stack>
+    </Split>
+  ),
+}
+
+const StackWithOverflow = ({ length = 20 }: { length?: number }) => (
+  <Stack gap="4" className="w-96">
+    <Button
+      label="Some button"
+      onClick={() =>
+        alert(
+          "This button is needed so the content can be focused for accessibility purposes."
+        )
+      }
+    />
+    {Array.from({ length }).map((_, i) => (
+      <Placeholder key={i}>
+        Element {i + 1} in a scrollable placeholder
+      </Placeholder>
+    ))}
+  </Stack>
+)
--- lib/ui/scrollarea.tsx
+++ lib/ui/scrollarea.tsx
@@ -17,7 +17,8 @@ const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
+    <ScrollBar orientation="vertical" />
+    <ScrollBar orientation="horizontal" />
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ))
@@ -31,16 +32,14 @@ const ScrollBar = React.forwardRef<
     ref={ref}
     orientation={orientation}
     className={cn(
-      "z-50 flex touch-none select-none transition-colors",
-      orientation === "vertical" &&
-        "h-full w-2.5 border-l border-l-transparent p-[1px]",
-      orientation === "horizontal" &&
-        "h-2.5 flex-col border-t border-t-transparent p-[1px]",
+      "z-50 flex touch-none select-none p-[1px]",
+      orientation === "vertical" && "mr-[2px] h-full w-2.5",
+      orientation === "horizontal" && "mt-[2px] h-2.5 flex-col",
       className
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-foreground opacity-50" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
