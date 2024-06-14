4d0ffc743a486613de5bf4febad73dfc81355d6e
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-06-14 09:24:16 +0200

    Add missing type export

--- lib/components/Overlays/Activity/index.tsx
+++ lib/components/Overlays/Activity/index.tsx
@@ -1,3 +1,5 @@
 export { ActivitiesProvider, useActivities } from "./ActivitiesProvider"
+export type { OpenActivityCallback } from "./ActivitiesProvider"
+
 export { Activity, useActivity } from "./Activity"
 export type * from "./types"
