3dcb9598ccb243fd7a8ce6da6c504bb7df8619b1
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-07-16 09:13:13 +0200

    Refactor insights

---
+++ lib/components/Insights/Container/index.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Placeholder } from "@/lib/storybook-utils"
+import { ComponentProps } from "react"
+import { InsightsContainer } from "."
+
+const meta = {
+  component: InsightsContainer,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: {
+    header: {
+      title: "Area Insight",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    footer: {
+      trend: "Increased by 12%",
+      time: "Since last month",
+    },
+    children: <Placeholder>Put your content in there</Placeholder>,
+  } satisfies ComponentProps<typeof InsightsContainer>,
+} satisfies Meta<typeof InsightsContainer>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
