a069c23d4161de86dbb983fbd29d52f40551575a
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-07-17 09:33:22 +0200

    Add loading state to buttons

--- lib/components/Actions/Button/index.stories.tsx
+++ lib/components/Actions/Button/index.stories.tsx
@@ -60,3 +60,17 @@ export const WithIcon: Story = {
     icon: BellRing,
   },
 }
+
+export const LoadingState: Story = {
+  render: (args) => {
+    const onClick = async (
+      event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+      args.onClick?.(event)
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      alert("Done!")
+    }
+
+    return <Button {...args} onClick={onClick} />
+  },
+}
--- lib/components/Actions/Button/index.tsx
+++ lib/components/Actions/Button/index.tsx
@@ -1,23 +1,45 @@
 import { Button as ShadcnButton } from "@/ui/button"
 import { LucideIcon } from "lucide-react"
-import { ComponentProps, forwardRef } from "react"
+import { ComponentProps, forwardRef, useState } from "react"
 
 type Props = Pick<
   ComponentProps<typeof ShadcnButton>,
-  "variant" | "size" | "onClick"
+  "variant" | "size" | "disabled"
 > & {
+  onClick?: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void | Promise<unknown>
   label: string
   icon?: LucideIcon
   hideLabel?: boolean
 }
 
 const Button: React.FC<Props> = forwardRef<HTMLButtonElement, Props>(
-  ({ label, hideLabel, icon, ...props }, ref) => {
+  ({ label, hideLabel, onClick, disabled, icon, ...props }, ref) => {
     const Icon = icon
+    const [loading, setLoading] = useState(false)
+
+    const handleClick = async (
+      event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+      const result = onClick?.(event)
+
+      if (result instanceof Promise) {
+        setLoading(true)
+
+        try {
+          await result
+        } finally {
+          setLoading(false)
+        }
+      }
+    }
 
     return (
       <ShadcnButton
         title={hideLabel ? label : undefined}
+        onClick={handleClick}
+        disabled={disabled || loading}
         rounded={hideLabel}
         ref={ref}
         {...props}
