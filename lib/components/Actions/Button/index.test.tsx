9a6b66f525fa7330325180169089faf031994c0c
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-07-18 14:30:39 +0200

    Add test to button

--- lib/components/Actions/Button/index.test.tsx
+++ lib/components/Actions/Button/index.test.tsx
@@ -12,3 +12,19 @@ test("Button calls the `onClick` handler when clicked", async () => {
   await userEvent.click(button)
   expect(onClick).toHaveBeenCalled()
 })
+
+test("Button is temporarily disabled when onClick is a promise until the promise resolves", async () => {
+  const onClick = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 100))
+    vi.fn()
+  }
+
+  render(<Button label="Click me" onClick={() => onClick()} />)
+
+  const button = screen.getByRole("button", { name: "Click me" })
+  await userEvent.click(button)
+
+  expect(button.attributes.getNamedItem("disabled")).not.toBeNull()
+  await new Promise((resolve) => setTimeout(resolve, 100))
+  expect(button.attributes.getNamedItem("disabled")).toBeNull()
+})
