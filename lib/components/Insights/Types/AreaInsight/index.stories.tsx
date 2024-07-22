94a3eb7ae13df1e5cb32de72c7927683debe2705
hossamElfarFactorial <hossam.elfar@factorial.co>
2024-07-22 09:44:27 +0000

    chore: update AreaInsight storybook example

--- lib/components/Insights/Types/AreaInsight/index.stories.tsx
+++ lib/components/Insights/Types/AreaInsight/index.stories.tsx
@@ -5,11 +5,8 @@ import { AreaInsight, AreaProps } from "."
 const Component = AreaInsight
 
 const dataConfig = {
-  desktop: {
-    label: "Desktop",
-  },
-  mobile: {
-    label: "Mobile",
+  score: {
+    label: "Score",
   },
 }
 
@@ -22,34 +19,26 @@ const meta = {
   args: {
     xAxis: {
       hide: false,
-      tickFormatter: (value: string) => value.slice(0, 3),
     },
     yAxis: {
-      hide: true,
+      hide: false,
+      tickCount: 3,
     },
     data: [
-      { label: "January", values: { mobile: 4000, desktop: 2400 } },
-      { label: "February", values: { mobile: 3000, desktop: 1398 } },
-      { label: "March", values: { mobile: 2000, desktop: 4000 } },
-      { label: "April", values: { mobile: 1500, desktop: 8000 } },
-      { label: "May", values: { mobile: 2000, desktop: 6000 } },
-      { label: "June", values: { mobile: 3500, desktop: 4000 } },
-      { label: "July", values: { mobile: 4500, desktop: 2000 } },
-      { label: "August", values: { mobile: 5500, desktop: 1000 } },
-      { label: "September", values: { mobile: 6500, desktop: 500 } },
-      { label: "October", values: { mobile: 7500, desktop: 300 } },
-      { label: "November", values: { mobile: 8500, desktop: 200 } },
-      { label: "December", values: { mobile: 9500, desktop: 500 } },
+      { label: "Q1 24", values: { score: 1.2 } },
+      { label: "Q2 24", values: { score: 1.7 } },
+      { label: "Q3 24", values: { score: 2.1 } },
+      { label: "Q4 24", values: { score: 3 } },
+      { label: "Q1 25", values: { score: 3.4 } },
+      { label: "Q2 25", values: { score: 4.2 } },
+      { label: "Q3 25", values: { score: 4.9 } },
     ],
     dataConfig,
     header: {
-      title: "Area Insight",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      title: "3,5 - Meets expectations",
+      description: "Performance evolution",
     },
-    footer: {
-      trend: "Increased by 12%",
-      time: "Since last month",
-    },
+    showLegend: true,
   } satisfies AreaProps<typeof dataConfig>,
 } satisfies Meta<typeof Component>
 
