90e83444095b5d8dcb785ea3853264b5f30813bc
Josep Jaume Rey <josepjaume@gmail.com>
2024-07-24 15:42:26 +0200

    Fix icon compilation (#337)
    * Cleanup icons config
    
    * Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,22 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react"
-import { readdirSync } from "fs"
 import path, { resolve } from "path"
 import { defineConfig } from "vite"
 import dts from "vite-plugin-dts"
 import { libInjectCss } from "vite-plugin-lib-inject-css"
-import svgr from "vite-plugin-svgr"
 import { peerDependencies } from "./package.json"
-
-const iconsPath = path.resolve(__dirname, "lib/icons")
-const iconFiles = readdirSync(iconsPath).filter((file) => file.endsWith(".tsx"))
-
-const iconPaths = iconFiles.reduce((paths, file) => {
-  const fullPath = path.join(iconsPath, file)
-  const iconName = `icons/${path.basename(file, ".tsx")}`
-  return { ...paths, [iconName]: fullPath }
-}, {})
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    svgr({
-      svgrOptions: {
-        ref: true,
-        memo: true,
-        svgo: true,
-        replaceAttrValues: {
-          "#FF355E": "currentColor",
-          "#515164": "currentColor",
-        },
-      },
-    }),
     libInjectCss(),
     ...(process.env.BUILD_TYPES
       ? [
           dts({
             include: ["lib"],
             exclude: ["**/*.stories.tsx"],
+            rollupTypes: true,
             bundledPackages: ["class-variance-authority"],
           }),
         ]
@@ -52,7 +31,6 @@ export default defineConfig({
     lib: {
       entry: {
         ["factorial-one"]: resolve(__dirname, "lib/factorial-one.ts"),
-        ...iconPaths,
       },
       fileName: (_, entryName) => {
         return `${entryName}.js`
