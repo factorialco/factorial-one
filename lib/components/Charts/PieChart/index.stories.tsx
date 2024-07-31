b01c7cef9c25c7b0103459ff4b1ca233d37d76a7
Hossam Elfar <hossam.elfar@factorial.co>
2024-07-31 11:20:58 +0300

    feat: adding pie chart component (#356)
    Co-authored-by: Josep Jaume Rey <josepjaume@gmail.com>

---
+++ lib/components/Charts/PieChart/index.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { PieChart, PieChartProps } from "."
+
+const dataConfig = {
+  january: {
+    label: "January",
+  },
+  february: {
+    label: "February",
+  },
+}
+
+const Component = PieChart<typeof dataConfig>
+
+const meta: Meta<typeof Component> = {
+  component: Component,
+  title: "Charts/PieChart",
+} satisfies Meta<typeof Component>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    dataConfig: {
+      january: {
+        label: "January",
+        color: "hsl(var(--chart-1))",
+      },
+      february: {
+        label: "February",
+        color: "hsl(var(--chart-2))",
+      },
+      march: {
+        label: "March",
+        color: "hsl(var(--chart-3))",
+      },
+      april: {
+        label: "April",
+        color: "hsl(var(--chart-4))",
+      },
+      may: {
+        label: "May",
+        color: "hsl(var(--chart-5))",
+      },
+      june: {
+        label: "June",
+        color: "hsl(var(--chart-6))",
+      },
+    },
+    data: [
+      { label: "january", value: 186 },
+      { label: "february", value: 305 },
+      { label: "march", value: 237 },
+      { label: "april", value: 73 },
+      { label: "may", value: 209 },
+      { label: "june", value: 214 },
+    ],
+  } as PieChartProps<typeof dataConfig>,
+}
