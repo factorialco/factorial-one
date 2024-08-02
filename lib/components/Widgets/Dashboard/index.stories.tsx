299a395b31f7a51dc5e0d3ec9c326e79058e4757
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-08-02 14:50:20 +0200

    Add parameters as main

--- lib/components/Widgets/Dashboard/index.stories.tsx
+++ lib/components/Widgets/Dashboard/index.stories.tsx
@@ -36,7 +36,13 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: "reset",
+    },
+  },
+}
 
 export const Small: Story = {
   args: {
