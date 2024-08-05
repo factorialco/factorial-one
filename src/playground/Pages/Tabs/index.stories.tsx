195f44e803e79961850d87b6282e39cacc207935
Ramiro <64564058+RamProg@users.noreply.github.com>
2024-08-05 14:43:09 +0200

    Feat/fct 16897/new tab layout (#360)
    * FCT-16897: create tab layout
    
    * FCT-16879: implement layout with tabs
    
    * FCT-16879: fix minor prettier format
    
    * FCT-16897: remove extra usage of cn()
    
    * FCT-16897: remove extra colors
    
    * FCT-16897: move tab component to playground
    
    * FCT-16897: add forwardRef to new components
    
    * FCT-16897: refactor icon color
    
    * FCT-16897: add color to icon storie
    
    * FCT-16897: refactor Avatar component
    
    * FCT-16897: replace FlexBox component with normal div
    
    * FCT-16897: switch to default font sizes
    
    * FCT-16897: remove icon color variants
    
    * FCT-16897: replace dashboard on tab storie
    
    * FCT-16897: fix alt text
    
    * FCT-16897: update layour storie
    
    * FCT-16897: update size type
    
    * FCT-16897: skip test
    
    * FCT-16897: create new breadcrumb component
    
    ---------
    
    Co-authored-by: Ramiro de la Pena <ramiro.delapena@ramiro.delapena>

---
+++ src/playground/Pages/Tabs/index.stories.tsx
@@ -0,0 +1,91 @@
+import { AreaChartWidget } from "@/components/Widgets/Charts/AreaChartWidget"
+import AreaChartWidgetStoriesMeta from "@/components/Widgets/Charts/AreaChartWidget/index.stories"
+import { LineChartWidget } from "@/components/Widgets/Charts/LineChartWidget"
+import LineChartWidgetStoriesMeta from "@/components/Widgets/Charts/LineChartWidget/index.stories"
+import { PieChartWidget } from "@/components/Widgets/Charts/PieChartWidget"
+import PieChartWidgetStoriesMeta from "@/components/Widgets/Charts/PieChartWidget/index.stories"
+import { VerticalBarChartWidget } from "@/components/Widgets/Charts/VerticalBarChartWidget"
+import VerticalBarChartWidgetStoriesMeta from "@/components/Widgets/Charts/VerticalBarChartWidget/index.stories"
+import { Dashboard } from "@/components/Widgets/Dashboard"
+import { User } from "@/icons"
+import type { Meta, StoryObj } from "@storybook/react"
+import { Tabs } from "."
+
+const meta = {
+  component: Tabs,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Tabs>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const renderWidget = (index: number) => {
+  const Widgets = [
+    () => <AreaChartWidget {...AreaChartWidgetStoriesMeta.args} />,
+    () => <LineChartWidget {...LineChartWidgetStoriesMeta.args} />,
+    () => <PieChartWidget {...PieChartWidgetStoriesMeta.args} />,
+    () => (
+      <VerticalBarChartWidget {...VerticalBarChartWidgetStoriesMeta.args} />
+    ),
+  ]
+
+  const Component = Widgets[index % Widgets.length]
+  return <Component />
+}
+
+const Overview = () => (
+  <div className="grid grid-cols-[2fr_1fr] divide-x divide-y-0 divide-dashed divide-muted">
+    <div className="pl-10 pr-8 pt-6">
+      <Dashboard>
+        {Array.from({ length: 6 }).map((_, i) => renderWidget(i))}
+      </Dashboard>
+    </div>
+    <div className="pl-8 pr-10 pt-6">
+      Column 2 Column 2 Column 2 Column 2 Column 2 Column 2 Column 2 Column 2
+      Column 2 Column 2 Column 2 Column 2 Column 2 Column 2 Column 2 Column 2
+      Column 2 Column 2 Column 2 Column 2 Column 2 Column 2 Column 2 Column 2
+      Column 2 Column 2 Column 2 Column 2 Column 2 Column 2 Column 2 Column 2
+      Column 2 Column 2 Column 2 Column 2 Column 2 Column 2 Column 2 Column 2
+      Column 2 Column 2 Column 2 Column 2 Column 2 Column 2
+    </div>
+  </div>
+)
+
+const Profile = () => <div>Profile</div>
+const Personal = () => <div>Personal</div>
+const Agreement = () => <div>Agreement</div>
+const TimeOff = () => <div>Time off</div>
+const Competencies = () => <div>Competencies</div>
+const Activity = () => <div>Activity</div>
+
+export const Default: Story = {
+  args: {
+    tabs: [
+      { name: "Overview", key: "overview", content: <Overview /> },
+      { name: "Profile", key: "profile", content: <Profile /> },
+      { name: "Personal", key: "personal", content: <Personal /> },
+      { name: "Agreement", key: "agreement", content: <Agreement /> },
+      { name: "Time off", key: "time-off", content: <TimeOff /> },
+      { name: "Competencies", key: "competencies", content: <Competencies /> },
+      { name: "Activity", key: "activity", content: <Activity /> },
+    ],
+    title: "Alba Horneros",
+    subtitle: "Product Designer",
+    src: "https://github.com/dani-moreno.png",
+    alt: "DM",
+    routes: [{ title: "Employees", url: "/employees" }],
+    breadcrumbTitle: "Alba Horneros",
+    icon: User,
+  },
+  parameters: {
+    layout: "fullscreen",
+    a11y: {
+      config: {
+        rules: [
+          { id: "color-contrast", enabled: false },
+          { id: "svg-img-alt", enabled: false },
+        ],
+      },
+    },
+  },
+}
