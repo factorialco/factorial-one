e320235d30248d1e3b2418cf6de035bb21603b09
Josep Jaume Rey <josepjaume@gmail.com>
2024-08-05 21:00:19 +0200

    Add widget row component (#378)
    * Add widget row component
    
    * Rename to "WidgetStrip"

---
+++ lib/components/Widgets/WidgetStrip/index.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { WidgetStrip } from "."
+import { AreaChartWidget } from "../Charts/AreaChartWidget"
+
+import AreaChartWidgetStoriesMeta from "../Charts/AreaChartWidget/index.stories"
+import { LineChartWidget } from "../Charts/LineChartWidget"
+import LineChartWidgetStoriesMeta from "../Charts/LineChartWidget/index.stories"
+import { PieChartWidget } from "../Charts/PieChartWidget"
+import PieChartWidgetStoriesMeta from "../Charts/PieChartWidget/index.stories"
+import { WidgetContainer } from "../WidgetContainer"
+
+const renderWidget = (index: number) => {
+  const Widgets = [
+    () => <AreaChartWidget {...AreaChartWidgetStoriesMeta.args} />,
+    () => <LineChartWidget {...LineChartWidgetStoriesMeta.args} />,
+    () => <PieChartWidget {...PieChartWidgetStoriesMeta.args} />,
+    () => (
+      <WidgetContainer
+        header={{ title: "A form widget", description: "Enter your data" }}
+      >
+        <p>
+          Never gonna give you up. Never gonna let you down. Never gonna turn
+          around and desert you.
+        </p>
+      </WidgetContainer>
+    ),
+  ]
+
+  const Component = Widgets[index % Widgets.length]
+  return <Component key={index} />
+}
+
+const renderWidgets = (length: number) =>
+  Array.from({ length }).map((_, i) => renderWidget(i))
+
+const meta = {
+  component: WidgetStrip,
+  tags: ["autodocs"],
+  args: {
+    children: renderWidgets(3),
+  },
+  parameters: {
+    a11y: {
+      config: {
+        rules: [
+          { id: "color-contrast", enabled: false },
+          { id: "svg-img-alt", enabled: false },
+        ],
+      },
+    },
+  },
+} satisfies Meta<typeof WidgetStrip>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+export const Single: Story = {
+  args: {
+    children: renderWidgets(1),
+  },
+}
+
+export const Overflow: Story = {
+  args: {
+    children: renderWidgets(10),
+  },
+}
