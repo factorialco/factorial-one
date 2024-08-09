1f6e667b8f5c72decbe04bc34dbf55c42f3b49d7
Josep Jaume Rey <josepjaume@gmail.com>
2024-08-09 14:30:16 +0200

    Blend skeletons (#406)

--- lib/components/Widgets/WidgetStrip/index.tsx
+++ lib/components/Widgets/WidgetStrip/index.tsx
@@ -1,5 +1,6 @@
 import { ScrollArea } from "@/components/Utilities/ScrollArea"
-import { withSkeleton } from "@/lib/skeleton"
+import { Blend, withSkeleton } from "@/lib/skeleton"
+import { cn } from "@/lib/utils"
 import { forwardRef, ReactNode } from "react"
 import { WidgetContainer } from "../WidgetContainer"
 
@@ -7,22 +8,36 @@ type DashboardProps = {
   children: ReactNode[]
 }
 
+const Container: React.FC<{ children: ReactNode; className?: string }> = ({
+  children,
+  className,
+}) => (
+  <div
+    className={cn(
+      "flex min-h-72 flex-row items-stretch gap-4 [&>*]:min-w-96 [&>*]:max-w-lg [&>*]:flex-grow [&>*]:basis-0",
+      className
+    )}
+  >
+    {children}
+  </div>
+)
+
 const WidgetStripComponent = forwardRef<HTMLDivElement, DashboardProps>(
   ({ children }, ref) => {
     return (
       <ScrollArea ref={ref}>
-        <div className="flex min-h-72 flex-row items-stretch gap-4 [&>*]:min-w-96 [&>*]:max-w-lg [&>*]:flex-grow [&>*]:basis-0">
-          {children}
-        </div>
+        <Container>{children}</Container>
       </ScrollArea>
     )
   }
 )
 
 export const WidgetStrip = withSkeleton(WidgetStripComponent, () => (
-  <WidgetStripComponent>
-    <WidgetContainer.Skeleton />
-    <WidgetContainer.Skeleton />
-    <WidgetContainer.Skeleton />
-  </WidgetStripComponent>
+  <Blend orientation="horizontal">
+    <Container className="overflow-hidden">
+      <WidgetContainer.Skeleton />
+      <WidgetContainer.Skeleton />
+      <WidgetContainer.Skeleton />
+    </Container>
+  </Blend>
 ))
