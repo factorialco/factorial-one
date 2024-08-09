87f50893264af43468f99ded8b3cb5fc696abf73
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-08-09 10:37:30 +0200

    Improve typing of the skeleton helper

--- lib/lib/skeleton.tsx
+++ lib/lib/skeleton.tsx
@@ -1,6 +1,20 @@
-export function withSkeleton<T extends object, U>(
-  Component: T,
-  Skeleton: U
-): T & { Skeleton: U } {
+import {
+  ComponentClass,
+  ComponentType,
+  ForwardRefExoticComponent,
+  RefAttributes,
+} from "react"
+
+type AnyReactComponent<P> =
+  | ComponentType<P>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  | ForwardRefExoticComponent<P & RefAttributes<any>>
+  | ComponentClass<P>
+
+export function withSkeleton<
+  T extends object,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  U extends AnyReactComponent<any>,
+>(Component: T, Skeleton: U): T & { Skeleton: U } {
   return Object.assign(Component, { Skeleton })
 }
