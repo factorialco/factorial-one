c3a82201cbc12ab303bcfb1c1b9e6a99cf5bebdf
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-09-02 17:30:38 +0200

    Summaries in ChartContainer

--- lib/components/Widgets/Charts/ChartContainer.tsx
+++ lib/components/Widgets/Charts/ChartContainer.tsx
@@ -2,20 +2,23 @@ import { forwardRef, ReactNode } from "react"
 import { WidgetContainer, WidgetContainerProps } from "../WidgetContainer"
 
 export type ChartContainerPropsBase = WidgetContainerProps & {
-  chart?: ReactNode
+  summaries?: WidgetContainerProps["summaries"]
 }
 
-const Container = forwardRef<HTMLDivElement, ChartContainerPropsBase>(
-  ({ chart, summaries, ...props }, ref) => (
-    <WidgetContainer ref={ref} {...props} summaries={summaries}>
-      {chart && (
-        <div className="relative flex min-h-40 grow items-stretch pt-6">
-          {chart}
-        </div>
-      )}
-    </WidgetContainer>
-  )
-)
+const Container = forwardRef<
+  HTMLDivElement,
+  ChartContainerPropsBase & {
+    chart?: ReactNode
+  }
+>(({ chart, summaries, ...props }, ref) => (
+  <WidgetContainer ref={ref} {...props} summaries={summaries}>
+    {chart && (
+      <div className="relative flex min-h-40 grow items-stretch pt-6">
+        {chart}
+      </div>
+    )}
+  </WidgetContainer>
+))
 
 export type ComposeChartContainerProps<T extends object> =
   ChartContainerPropsBase & {
