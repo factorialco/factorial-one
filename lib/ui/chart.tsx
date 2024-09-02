a12f7a046675c9059168657831ab812ee0450c6e
Roger De Moreta <roger.demoreta@factorial.co>
2024-09-02 13:10:25 +0200

    add dashed prop to ChartConfig

--- lib/ui/chart.tsx
+++ lib/ui/chart.tsx
@@ -21,6 +21,7 @@ export type ChartConfig = {
   [k in string]: {
     label?: React.ReactNode
     icon?: React.ComponentType
+    dashed?: boolean
   } & (
     | { color?: string; theme?: never }
     | { color?: never; theme: Record<keyof typeof THEMES, string> }
