0bb793fa78340b90098d25709b6ae8fd17157502
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-09-02 17:49:55 +0200

    Remove muted

--- lib/components/Charts/CategoryBarChart/index.stories.tsx
+++ lib/components/Charts/CategoryBarChart/index.stories.tsx
@@ -25,27 +25,13 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
 
-export const WithMutedCategory: Story = {
-  args: {
-    data: [
-      { name: "Worked hours", value: 79 },
-      {
-        name: "Remaining hours",
-        value: 19,
-        color: "hsl(var(--muted-foreground))",
-        muted: true,
-      },
-    ],
-  },
-}
-
 export const MultipleValues: Story = {
   args: {
     data: [
       { name: "Employee Eng.", value: 42 },
       { name: "Time Off Req.", value: 91 },
       { name: "New Hires", value: 13 },
-      { name: "Training Sess.", value: 67, muted: true },
+      { name: "Training Sess.", value: 67 },
       { name: "Performance Rev.", value: 85 },
     ],
   },
