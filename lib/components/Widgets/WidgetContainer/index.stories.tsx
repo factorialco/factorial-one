75c6df77962594e66d51129dc8bd9a12a8efaf80
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-09-03 12:36:27 +0200

    Resolve conflict

--- lib/components/Widgets/WidgetContainer/index.stories.tsx
+++ lib/components/Widgets/WidgetContainer/index.stories.tsx
@@ -58,13 +58,15 @@ export const WithAction: Story = {
 export const MultipleContent: Story = {
   args: {
     ...meta.args,
-    children: Array.from({ length: 3 }).map((_, index) => (
-      <div
-        key={index}
-        className="rounded-lg bg-muted p-3 text-center text-foreground"
-      >
-        Content {index + 1}
-      </div>
-    )),
+    children: [
+      Array.from({ length: 3 }).map((_, index) => (
+        <div
+          key={index}
+          className="rounded-lg bg-muted p-3 text-center text-foreground"
+        >
+          Content {index + 1}
+        </div>
+      )),
+    ],
   },
 }
