9e49509cfb87b086f11aa4a3671edcf95fc291be
Nikolai Lopin <nik.lopin@factorial.co>
2024-09-13 13:15:21 +0200

    Use rems for height, width and related properties (#480)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss"
 
+import { absoluteSpacing, relativeSpacing } from "./tailwind/spacing"
+
 export default {
   darkMode: ["class"],
   corePlugins: {
@@ -12,7 +14,7 @@ export default {
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
     "./lib/**/*.{ts,tsx}",
-    "./docs/**/*.mdx",
+    "./docs/**/*.{mdx,ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -62,44 +64,17 @@ export default {
         },
       ],
     },
-    spacing: {
-      px: "1px",
-      0: "0px",
-      0.5: "2px",
-      1: "4px",
-      1.5: "6px",
-      2: "8px",
-      2.5: "10px",
-      3: "12px",
-      3.5: "14px",
-      4: "16px",
-      5: "20px",
-      6: "24px",
-      7: "28px",
-      8: "32px",
-      9: "36px",
-      10: "40px",
-      11: "44px",
-      12: "48px",
-      14: "56px",
-      16: "64px",
-      20: "80px",
-      24: "96px",
-      28: "112px",
-      32: "128px",
-      36: "144px",
-      40: "160px",
-      44: "176px",
-      48: "192px",
-      52: "208px",
-      56: "224px",
-      60: "240px",
-      64: "256px",
-      72: "288px",
-      80: "320px",
-      96: "384px",
-    },
+    // use pixel scale by default
+    spacing: absoluteSpacing,
     extend: {
+      flexBasis: relativeSpacing,
+      height: relativeSpacing,
+      maxHeight: relativeSpacing,
+      maxWidth: relativeSpacing,
+      minHeight: relativeSpacing,
+      minWidth: relativeSpacing,
+      textIndent: relativeSpacing,
+      width: relativeSpacing,
       colors: {
         border: "hsl(var(--border))",
         input: {
