b5ba89315ce2edb47a75a1d558948c82a77e5b61
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-09-13 11:46:44 +0200

    Conflict

---
+++ lib/ui/indicator.tsx
@@ -0,0 +1,28 @@
+import { Icon, IconType } from "@/components/Utilities/Icon"
+import { cn } from "@/lib/utils"
+import { forwardRef } from "react"
+
+interface IndicatorProps {
+  count: number
+  label: string
+  color: string
+  icon?: IconType
+}
+
+export const Indicator = forwardRef<HTMLDivElement, IndicatorProps>(
+  ({ count, label, icon, color }, ref) => {
+    return (
+      <div key={label} className="grid-row-2 col-span-1 grid" ref={ref}>
+        <p className="font-medium text-intermediate">{label}</p>
+        <div className="flex items-center gap-1">
+          <p className="text-xl font-semibold">{count}</p>
+          {icon && (
+            <span className={cn("flex", color)}>
+              <Icon icon={icon} size="md" />
+            </span>
+          )}
+        </div>
+      </div>
+    )
+  }
+)
