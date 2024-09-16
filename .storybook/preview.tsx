fcec0d09afcb2ec3a74900c0a5f52988792a5b62
Niki Lopin <nik.lopin@factorial.co>
2024-09-16 18:24:45 +0200

    add documentation for color tokens (#517)
    Co-authored-by: Dani Moreno <96433370+dani-moreno@users.noreply.github.com>

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -77,7 +77,9 @@ const preview: Preview = {
     },
     docs: {
       container: DocsContainer,
-      toc: true,
+      toc: {
+        headingSelector: 'h2, h3'
+      },
     },
     controls: {
       expanded: true,
@@ -90,7 +92,7 @@ const preview: Preview = {
       storySort: {
         order: [
           "Foundations",
-          ["Typography", "Spacing"],
+          ["Colors", "Typography", "Spacing"],
           "Components",
           "Playground",
           "Experiments",
