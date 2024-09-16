0d8bef72018c77c4b5b4023f0c4447d3f587b28a
Nik Lopin <nik.lopin@factorial.co>
2024-09-16 17:33:25 +0200

    add documentation about color tokens

---
+++ docs/components/ColorToken.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+import { Button } from "@/components/Actions/Button"
+import { cn } from "@/lib/utils"
+import { CopyIcon } from "lucide-react"
+
+type Props = {
+  name: string
+  description?: string
+  cssColor?: string
+}
+
+export function ColorToken({ name, description }: Props) {
+  const [copied, setCopied] = useState(false)
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(name)
+    setCopied(true)
+    setTimeout(() => setCopied(false), 1000)
+  }
+
+  return (
+    <div className="bg-f1-background-secondary border-f1-border flex max-w-[600px] flex-row items-center space-x-3 rounded-lg border border-solid px-2 py-1.5">
+      <div className={cn("w-12 h-8 rounded-lg", `bg-${name}`)} />
+      <div className="flex flex-1 flex-col space-y-0">
+        <span className="font-mono text-f1-foreground flex-grow font-semibold">
+          {name}
+        </span>
+        <p className="text-f1-foreground-secondary text-sm">{description}</p>
+      </div>
+      <Button
+        onClick={copyToClipboard}
+        variant="ghost"
+        disabled={copied}
+        label={copied ? "Copied!" : "Copy"}
+        icon={copied ? undefined : CopyIcon}
+      />
+    </div>
+  )
+}
