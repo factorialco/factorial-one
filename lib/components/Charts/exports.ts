a62d5b78789ef6285e7540ae590da3fe2e68ad51
Nik Lopin <nik.lopin@factorial.co>
2024-09-17 17:22:45 +0200

    export CategoryBarChart

--- lib/components/Charts/exports.ts
+++ lib/components/Charts/exports.ts
@@ -2,6 +2,7 @@ import { Component } from "@/lib/component"
 
 import { AreaChart as AreaChartComponent } from "./AreaChart"
 import { BarChart as BarChartComponent } from "./BarChart"
+import { CategoryBarChart as CategoryBarChartComponent } from "./CategoryBarChart"
 import { LineChart as LineChartComponent } from "./LineChart"
 import { PieChart as PieChartComponent } from "./PieChart"
 import { VerticalBarChart as VerticalBarChartComponent } from "./VerticalBarChart"
@@ -21,6 +22,15 @@ export const BarChart = Component(
   },
   BarChartComponent
 )
+
+export const CategoryBarChart = Component(
+  {
+    name: "CategoryBarChart",
+    type: "info",
+  },
+  CategoryBarChartComponent
+)
+
 
 export const LineChart = Component(
   {
