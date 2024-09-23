0a66dde0bcb9be4533c41b130dc5780d5e029634
RamProg <ramirodlp@gmail.com>
2024-09-23 10:26:21 +0200

    chore: convert to forwardRef

--- lib/experimental/Layouts/ProfileLayout/index.tsx
+++ lib/experimental/Layouts/ProfileLayout/index.tsx
@@ -1,16 +1,20 @@
+import React, { forwardRef } from "react"
+
 interface ProfileLayoutProps {
   mainContent: React.ReactNode
   sideContent: React.ReactNode
 }
 
-export const ProfileLayout: React.FC<ProfileLayoutProps> = ({
-  mainContent,
-  sideContent,
-}) => {
-  return (
-    <div className="grid min-h-svh grid-cols-1 py-6 sm:grid-cols-[7fr_3fr] sm:divide-x sm:divide-y-0 sm:divide-solid sm:divide-f1-foreground-disabled">
-      <div className="order-2 px-6 sm:order-1">{mainContent}</div>
-      <div className="order-1 px-6 sm:order-2">{sideContent}</div>
-    </div>
-  )
-}
+export const ProfileLayout = forwardRef<HTMLDivElement, ProfileLayoutProps>(
+  ({ mainContent, sideContent }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className="grid min-h-svh grid-cols-1 py-6 sm:grid-cols-[7fr_3fr] sm:divide-x sm:divide-y-0 sm:divide-solid sm:divide-f1-foreground-disabled"
+      >
+        <div className="order-2 px-6 sm:order-1">{mainContent}</div>
+        <div className="order-1 px-6 sm:order-2">{sideContent}</div>
+      </div>
+    )
+  }
+)
