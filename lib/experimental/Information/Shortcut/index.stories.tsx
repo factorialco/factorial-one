2c331959b641a6bd8132e57a1488fb52e810ef60
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-09-25 15:57:14 +0200

    shortcut

---
+++ lib/experimental/Information/Shortcut/index.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { ComponentProps } from "react"
+import { Shortcut } from "."
+
+const meta = {
+  component: Shortcut,
+  parameters: {
+    layout: "centered",
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/pZzg1KTe9lpKTSGPUZa8OJ/Web-Components?node-id=172-2793",
+    },
+  },
+  tags: ["autodocs"],
+  args: {
+    keys: ["cmd", "k"],
+  } satisfies ComponentProps<typeof Shortcut>,
+} satisfies Meta<typeof Shortcut>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+
+export const MultipleKeys: Story = {
+  args: {
+    keys: ["↑", "↑", "↓", "↓", "←", "→", "←", "→", "B", "A"],
+  },
+}
