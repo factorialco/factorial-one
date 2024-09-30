d8a02a0f5d29f2565a834cc2f441d62c0d6e6e6c
RamProg <ramirodlp@gmail.com>
2024-09-30 14:41:02 +0200

    chore: create tags list

--- lib/experimental/Widgets/Content/Tag/index.tsx
+++ lib/experimental/Widgets/Content/Tag/index.tsx
@@ -1,20 +1,25 @@
 import { Avatar } from "@/components/Information/Avatar"
-import { ComponentProps } from "react"
+import { ComponentProps, forwardRef } from "react"
 
 interface TagProps {
   text: string
   avatar: ComponentProps<typeof Avatar>
 }
 
-const Tag: React.FC<TagProps> = ({ text, avatar }) => {
-  return (
-    <div className="flex flex-row items-center justify-start rounded-md border border-solid border-f1-border py-1 pl-1 pr-2 font-medium text-f1-foreground">
-      <span className="mr-1.5">
-        <Avatar alt={avatar.alt || text[0]} src={avatar.src} size="xsmall" />
-      </span>
-      <p>{text}</p>
-    </div>
-  )
-}
+export const Tag = forwardRef<HTMLDivElement, TagProps>(
+  ({ text, avatar }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className="flex flex-row items-center justify-start rounded-md border border-solid border-f1-border py-1 pl-1 pr-2 font-medium text-f1-foreground"
+      >
+        <span className="mr-1.5">
+          <Avatar alt={avatar.alt || text[0]} src={avatar.src} size="xsmall" />
+        </span>
+        <p>{text}</p>
+      </div>
+    )
+  }
+)
 
-export { Tag }
+Tag.displayName = "Tag"
---
+++ lib/experimental/Widgets/Content/TagsList/index.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { TagsList } from "."
+
+const meta: Meta = {
+  component: TagsList,
+  parameters: {
+    layout: "centered",
+    tags: ["autodocs"],
+  },
+  args: {
+    tags: [
+      {
+        text: "Management",
+        avatar: {
+          alt: "M",
+        },
+      },
+      {
+        text: "Engineering",
+        avatar: {
+          alt: "E",
+        },
+      },
+      {
+        text: "Managers",
+        avatar: {
+          alt: "M",
+        },
+      },
+      {
+        text: "Office/Spain",
+        avatar: {
+          alt: "O",
+        },
+      },
+      {
+        text: "Office/Remote",
+        avatar: {
+          alt: "O",
+        },
+      },
+      {
+        text: "Engineering/Management",
+        avatar: {
+          alt: "E",
+        },
+      },
+    ],
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
