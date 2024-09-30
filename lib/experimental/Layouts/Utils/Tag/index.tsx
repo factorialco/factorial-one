871106bf85d726da370a845f88f70cbbd2042596
Ramiro <64564058+RamProg@users.noreply.github.com>
2024-09-30 17:18:02 +0200

    Create Tag component (#624)
    * chore: create tag component
    
    * chore: create tags list
    
    * chore: remove old list and move tags utils
    
    * chore: add onclick
    
    * chore: make length two to alternative text

---
+++ lib/experimental/Layouts/Utils/Tag/index.tsx
@@ -0,0 +1,40 @@
+import { Avatar } from "@/components/Information/Avatar"
+import { cn } from "@/lib/utils"
+import { ComponentProps, forwardRef } from "react"
+
+interface TagProps {
+  text: string
+  avatar?: ComponentProps<typeof Avatar>
+  onClick?: () => void
+}
+
+export const Tag = forwardRef<HTMLDivElement, TagProps>(
+  ({ text, avatar, onClick }, ref) => {
+    const short =
+      avatar?.alt ??
+      text
+        .split(/\s+/)
+        .slice(0, 2)
+        .map((e) => e[0])
+        .join("")
+        .toLocaleUpperCase()
+
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          "flex flex-row items-center justify-start gap-1.5 rounded-md border border-solid border-f1-border py-1 pl-1 pr-2 font-medium text-f1-foreground",
+          onClick && "cursor-pointer"
+        )}
+        onClick={onClick}
+      >
+        <span>
+          <Avatar alt={short} src={avatar?.src} size="xsmall" />
+        </span>
+        <p>{text}</p>
+      </div>
+    )
+  }
+)
+
+Tag.displayName = "Tag"
