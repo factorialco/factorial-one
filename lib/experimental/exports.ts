9c3a40143d96e52ed15c557d34976f33f5c965b6
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-10-01 12:13:04 +0200

    Move components

--- lib/experimental/exports.ts
+++ lib/experimental/exports.ts
@@ -1,4 +1,16 @@
+import { Component } from "@/lib/component"
+import { ScrollArea as ScrollAreaComponent } from "./Utilities/ScrollArea"
+
 export * from "./Forms/exports"
 export * from "./Layouts/exports"
+export * from "./Overlays/exports"
 export * from "./Utilities/exports"
 export * from "./Widgets/exports"
+
+export const ScrollArea = Component(
+  {
+    name: "ScrollArea",
+    type: "layout",
+  },
+  ScrollAreaComponent
+)
