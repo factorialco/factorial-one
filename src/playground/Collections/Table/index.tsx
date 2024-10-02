f3d6dae2a0367a0df69bf3d687d30f9268f21b63
alexbhdez <abhbretones@gmail.com>
2024-10-02 14:49:59 +0200

    add table editable

--- src/playground/Collections/Table/index.tsx
+++ src/playground/Collections/Table/index.tsx
@@ -1,61 +1,91 @@
 import {
+  Cell,
   ColumnDef,
   flexRender,
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table"
+import { FocusEvent, useState } from "react"
+
+export type { ColumnDef } from "@tanstack/react-table"
 
 interface TableProps<TData> {
-  columns: ColumnDef<TData>[]
   data: TData[]
+  columns: ColumnDef<TData>[]
+  onDataChange?: (updatedData: TData[]) => void
+  isEditable?: boolean
 }
 
 export const Table = <TData extends object>({
-  columns,
   data,
+  onDataChange,
+  columns: propColumns,
+  isEditable = false,
 }: TableProps<TData>) => {
+  const [tableData, setTableData] = useState<TData[]>(data)
+
+  const columns = propColumns
+
+  const updateData = (rowIndex: number, columnId: string, value: unknown) => {
+    const updatedData = tableData.map((row, index) => {
+      if (index === rowIndex) {
+        return {
+          ...row,
+          [columnId]: value,
+        }
+      }
+      return row
+    })
+    setTableData(updatedData)
+    onDataChange?.(updatedData)
+  }
+
+  const renderCell = (cell: Cell<TData, string>) => {
+    if (!isEditable) {
+      return flexRender(cell.column.columnDef.cell, cell.getContext())
+    }
+
+    const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+      const newValue = e.target.value
+
+      updateData(cell.row.index, cell.column.id, newValue)
+    }
+
+    return <input defaultValue={cell.getValue()} onBlur={handleBlur} />
+  }
+
   const table = useReactTable({
     columns,
-    data,
+    data: tableData,
     getCoreRowModel: getCoreRowModel(),
   })
 
   return (
-    <table>
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => {
-          return (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <th key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </th>
-                )
-              })}
-            </tr>
-          )
-        })}
+    <table className="border-collapse">
+      <thead className="bg-f1-background-secondary">
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
       </thead>
       <tbody>
-        {table.getRowModel().rows.map((row) => {
-          return (
-            <tr>
-              {row.getVisibleCells().map((cell) => {
-                return (
-                  <td key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                )
-              })}
-            </tr>
-          )
-        })}
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>{renderCell(cell)}</td>
+            ))}
+          </tr>
+        ))}
       </tbody>
     </table>
   )
