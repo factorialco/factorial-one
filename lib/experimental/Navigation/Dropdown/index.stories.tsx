62687b59adcdb3e724fe7b98a2e49b17a958fc6c
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-10-08 12:08:04 +0200

    Simpler approach

---
+++ lib/experimental/Navigation/Dropdown/index.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { Dropdown } from "."
+
+const meta: Meta<typeof Dropdown> = {
+  component: Dropdown,
+  tags: ["autodocs"],
+}
+
+export default meta
+
+type Story = StoryObj<typeof Dropdown>
+
+export const Default: Story = {
+  args: {
+    items: [
+      {
+        label: "Create",
+        onClick: () => console.log("Create clicked"),
+        icon: "Add",
+        description: "New creation process",
+      },
+      {
+        label: "Edit",
+        onClick: () => console.log("Edit clicked"),
+        icon: "Pencil",
+        description: "Edit item's information",
+      },
+      {
+        label: "Save",
+        onClick: () => console.log("Save clicked"),
+        icon: "Save",
+        description: "Preserve changes",
+      },
+      {
+        label: "Delete",
+        onClick: () => console.log("Delete clicked"),
+        description: "Remove item",
+        critical: true,
+        icon: "Delete",
+      },
+    ],
+  },
+}
+
+export const WithCustomTrigger: Story = {
+  args: {
+    items: [
+      {
+        label: "Create",
+        onClick: () => console.log("Create clicked"),
+      },
+      {
+        label: "Delete",
+        onClick: () => console.log("Delete clicked"),
+        critical: true,
+      },
+    ],
+  },
+  render: (args) => (
+    <Dropdown {...args}>
+      <span className="cursor-pointer underline">Open dropdown</span>
+    </Dropdown>
+  ),
+}
