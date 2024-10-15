057de0f3ad2a1ca617b58af66deba718f6122117
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-10-15 08:39:54 +0200

    Unlocked sidebar

--- lib/experimental/Navigation/ApplicationFrame/index.tsx
+++ lib/experimental/Navigation/ApplicationFrame/index.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils"
 import { Button } from "@/ui/button"
+import { motion, MotionConfig } from "framer-motion"
 import { Menu } from "lucide-react"
 import { FrameProvider, useSidebar } from "./FrameProvider"
 
@@ -22,28 +23,46 @@ function ApplicationFrameContent({ children, sidebar }: ApplicationFrameProps) {
   const { isSidebarExpanded, toggleSidebar } = useSidebar()
 
   return (
-    <div
-      className={cn(
-        "relative flex h-full flex-row",
-        isSidebarExpanded && "gap-3"
-      )}
-    >
-      <div
-        className={cn("text-f1-foreground", isSidebarExpanded && "w-64 pl-3")}
-      >
-        {sidebar}
+    <MotionConfig transition={{ ease: [0.165, 0.84, 0.44, 1], duration: 0.3 }}>
+      <div className="relative flex h-full flex-row">
+        <div
+          className={cn(
+            "transition-all",
+            isSidebarExpanded ? "w-64 pl-3" : "w-0"
+          )}
+        >
+          <motion.div
+            initial={false}
+            animate={{
+              opacity: 1,
+            }}
+          >
+            {sidebar}
+          </motion.div>
+        </div>
+        <motion.div
+          className="flex flex-1"
+          layout
+          transition={{
+            duration: 0.3,
+            type: "spring",
+            stiffness: 300,
+            damping: 30,
+          }}
+        >
+          {children}
+        </motion.div>
+        <Button
+          variant="outline"
+          size="md"
+          className="fixed bottom-4 right-4 z-50"
+          onClick={toggleSidebar}
+          aria-label="Toggle Sidebar"
+        >
+          <Menu className="h-4 w-4" />
+          Toggle sidebar
+        </Button>
       </div>
-      <div className="flex flex-1">{children}</div>
-      <Button
-        variant="outline"
-        size="md"
-        className="fixed bottom-4 right-4 z-50"
-        onClick={toggleSidebar}
-        aria-label="Toggle Sidebar"
-      >
-        <Menu className="h-4 w-4" />
-        Toggle sidebar
-      </Button>
-    </div>
+    </MotionConfig>
   )
 }
--- lib/experimental/Navigation/Sidebar/Sidebar.tsx
+++ lib/experimental/Navigation/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils"
+import { motion } from "framer-motion"
 import { ReactNode } from "react"
 import { useSidebar } from "../ApplicationFrame/FrameProvider"
 
@@ -12,15 +13,24 @@ export function Sidebar({ header, body, footer }: SidebarProps) {
   const { isSidebarExpanded } = useSidebar()
 
   return (
-    <div
+    <motion.div
+      initial={false}
       className={cn(
-        "flex h-full flex-col gap-2",
-        isSidebarExpanded ? "" : "fixed left-0 top-0"
+        "absolute bottom-0 left-0 top-0 flex w-64 flex-col gap-2 px-3",
+        isSidebarExpanded
+          ? "h-screen"
+          : "border-f1-border-bold/5 h-[calc(100vh-16px)] bg-f1-background-bold/5 pb-3 backdrop-blur-3xl"
       )}
+      animate={{
+        opacity: isSidebarExpanded ? 1 : 1,
+        top: isSidebarExpanded ? 0 : "8px",
+        borderRadius: isSidebarExpanded ? "0" : "12px",
+        left: isSidebarExpanded ? "0" : "8px",
+      }}
     >
       {header}
       {body && <div className="flex-grow overflow-y-auto">{body}</div>}
       {footer}
-    </div>
+    </motion.div>
   )
 }
