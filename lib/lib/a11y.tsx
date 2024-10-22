9884300501eca92a61fea05f1994434fc0a926d5
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-10-22 14:03:23 +0200

    Initialize hook with value

--- lib/lib/a11y.tsx
+++ lib/lib/a11y.tsx
@@ -1,6 +1,12 @@
 import { useMediaQuery } from "usehooks-ts"
 
 export const useReducedMotion = () => {
-  const prefersReducedMotion = useMediaQuery("(prefers-reduced-motion: reduce)")
+  const prefersReducedMotion = useMediaQuery(
+    "(prefers-reduced-motion: reduce)",
+    {
+      initializeWithValue: true,
+      defaultValue: false,
+    }
+  )
   return prefersReducedMotion
 }
