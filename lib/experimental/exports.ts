ac2ad163a928f9846ad863984edfc121501632b5
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2024-10-24 15:48:26 +0200

    Add missing export

--- lib/experimental/exports.ts
+++ lib/experimental/exports.ts
@@ -2,6 +2,7 @@ import { Component } from "@/lib/component"
 import { ScrollArea as ScrollAreaComponent } from "./Utilities/ScrollArea"
 
 export * from "./Forms/exports"
+export * from "./Information/exports"
 export * from "./Layouts/exports"
 export * from "./Navigation/exports"
 export * from "./Overlays/exports"
