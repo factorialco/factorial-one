c8478a7cd69be3331fceb5f77a7f923d99a62be4
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-10-29 13:19:35 +0100

    Remove unneeded check

--- lib/experimental/Information/Avatars/BaseAvatar/index.tsx
+++ lib/experimental/Information/Avatars/BaseAvatar/index.tsx
@@ -34,8 +34,6 @@ function getInitials(
 }
 
 function getAvatarColor(text: string): ShadAvatarProps["color"] {
-  if (!text) return "viridian"
-
   let hash = 0
 
   for (let i = 0; i < text.length; i++) {
