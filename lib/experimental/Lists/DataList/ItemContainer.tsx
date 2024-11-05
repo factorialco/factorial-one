f02e3a0057d9e6616b6f14b1fb73abc93cd72df7
Nik Lopin <nik.lopin@factorial.co>
2024-11-05 10:49:17 +0100

    add datalist component (#780)

--- lib/experimental/Lists/DataList/ItemContainer.tsx
+++ lib/experimental/Lists/DataList/ItemContainer.tsx
@@ -1,91 +1,96 @@
 import { Icon, IconType } from "@/components/Utilities/Icon"
-import { Link } from "@/lib/linkHandler"
 import { cn } from "@/lib/utils"
-import {
-  AnchorHTMLAttributes,
-  ButtonHTMLAttributes,
-  forwardRef,
-  HTMLAttributes,
-  ReactElement,
-  ReactNode,
-} from "react"
+import { forwardRef, ReactElement, ReactNode } from "react"
+import { CopyAction } from "./actions/CopyAction"
+import { NavigateAction } from "./actions/NavigateAction"
 
-type CommonProp = {
-  leftIcon?: IconType
-  actionIcon?: ({ className }: { className: string }) => ReactElement
+type ItemContainerProps = {
+  leftIcon?: IconType | (() => ReactElement)
+  action?: InternalActionType
+  text: string
+  className?: string
+}
+
+// internally all action fields are mandatory
+export type InternalActionType =
+  | InternalCopyActionType
+  | InternalNavigateActionType
+  | InternalNoopActionType
+
+export type InternalCopyActionType = {
+  type: "copy"
   text: string
 }
 
-type ItemContainerProps = CommonProp & SemanticTagProps
+export type InternalNavigateActionType = {
+  type: "navigate"
+  href: string
+}
 
-type SemanticTagProps =
-  | (HTMLAttributes<HTMLDivElement> & { as?: "div" })
-  | (ButtonHTMLAttributes<HTMLButtonElement> & { as: "button" })
-  | (AnchorHTMLAttributes<HTMLAnchorElement> & { as: "a" })
+export type InternalNoopActionType = {
+  type: "noop"
+}
 
 export const ItemContainer = forwardRef<HTMLLIElement, ItemContainerProps>(
   (props, ref) => {
-    const { text, leftIcon: LeftIcon, actionIcon, className, ...rest } = props
+    const {
+      text,
+      leftIcon: LeftIcon,
+      className,
+      action = { type: "noop" },
+    } = props
 
     return (
       <li
         className="flex rounded font-medium text-f1-foreground *:flex-1"
         ref={ref}
       >
-        <SemanticTag
-          {...rest}
+        <Action
+          action={action}
           className={cn("flex items-center gap-1.5 p-1.5", className)}
         >
-          {LeftIcon && <Icon icon={LeftIcon} size="md" aria-hidden="true" />}
+          {LeftIcon &&
+            (typeof LeftIcon === "function" ? (
+              LeftIcon({})
+            ) : (
+              <Icon icon={LeftIcon} size="md" aria-hidden="true" />
+            ))}
           <div className="line-clamp-2 flex-1 text-left">{text}</div>
-          {actionIcon !== undefined && (
-            <div className="grid">
-              {actionIcon({
-                className:
-                  "opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-focus-visible:opacity-100",
-              })}
-            </div>
-          )}
-        </SemanticTag>
+        </Action>
       </li>
     )
   }
 )
-
 ItemContainer.displayName = "ItemContainer"
 
-const SemanticTag = ({
+const Action = ({
   children,
-  ...rest
-}: SemanticTagProps & { children: ReactNode }) => {
-  switch (rest.as) {
-    case "button":
+  action,
+  ...props
+}: {
+  className: string
+  action: InternalActionType
+  children: ReactNode
+}) => {
+  const type = action.type
+  switch (type) {
+    case "copy":
       return (
-        <button
-          {...rest}
-          className={cn(
-            "group flex items-center gap-1.5 rounded p-1.5",
-            "focus-visible:outline focus-visible:outline-2 focus-visible:outline-f1-border-selected-bold",
-            "hover:bg-f1-background-hover active:bg-f1-background-secondary-hover",
-            rest.className
-          )}
-        >
+        <CopyAction {...action} {...props}>
           {children}
-        </button>
+        </CopyAction>
       )
-    case "a":
+    case "navigate":
       return (
-        <Link
-          {...rest}
-          className={cn(
-            rest.className,
-            "text-inherit group flex items-center gap-1.5 rounded p-1.5 text-f1-foreground no-underline hover:bg-f1-background-hover focus-visible:outline focus-visible:outline-2 focus-visible:outline-f1-border-selected-bold"
-          )}
-        >
+        <NavigateAction {...action} {...props}>
           {children}
-        </Link>
+        </NavigateAction>
       )
-    default:
-      return <div {...rest}>{children}</div>
+    case "noop":
+      return <div {...props}>{children}</div>
+    default: {
+      const _exhaustiveCheck: never = type
+      return _exhaustiveCheck
+    }
   }
 }
