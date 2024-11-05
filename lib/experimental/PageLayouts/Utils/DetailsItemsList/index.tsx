3a1a230f6d12471619ef4f5ced00d190b19f3a63
Saúl Domínguez Gómez <saulydominguez@gmail.com>
2024-11-05 13:52:21 +0100

    Start using DataList for sidebar (#773)
    * start using DataList for sidebar
    
    * restrict item types to be datalist items
    
    * fix types
    
    * use DataList properly
    
    * add support to add spacing
    
    * do not use lodash
    
    * fix tests

--- lib/experimental/PageLayouts/Utils/DetailsItemsList/index.tsx
+++ lib/experimental/PageLayouts/Utils/DetailsItemsList/index.tsx
@@ -13,20 +13,19 @@ export const DetailsItemsList = forwardRef<
   return (
     <div ref={ref} className="flex flex-col gap-4">
       {!!title && (
-        <p className="mb-1 text-sm font-semibold text-f1-foreground-secondary">
+        <p className="mb-1 pl-3 text-sm font-semibold text-f1-foreground-secondary">
           {title.toLocaleUpperCase()}
         </p>
       )}
-      <div className="flex flex-col gap-5">
-        {details?.map((item) => {
-          return !item?.title || !item?.content ? null : (
-            <DetailsItem
-              title={item.title}
-              key={item.title}
-              content={item.content}
-            />
-          )
-        })}
+      <div className="flex flex-col gap-1">
+        {details?.map((item) => (
+          <DetailsItem
+            title={item.title}
+            key={item.title}
+            content={item.content}
+            spacingAtTheBottom={item.spacingAtTheBottom}
+          />
+        ))}
       </div>
     </div>
   )
