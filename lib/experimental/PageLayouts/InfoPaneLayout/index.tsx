d77d80d62cd2bd4a6487ae5dbe1de7f330112b3b
Saúl Domínguez Gómez <saulydominguez@gmail.com>
2024-11-06 12:19:22 +0100

    update InfoPaneLayout component spacings (#800)

--- lib/experimental/PageLayouts/InfoPaneLayout/index.tsx
+++ lib/experimental/PageLayouts/InfoPaneLayout/index.tsx
@@ -10,8 +10,8 @@ export const InfoPaneLayout = forwardRef<HTMLDivElement, InfoPaneLayoutProps>(
     return (
       <div ref={ref} className="overflow-auto sm:min-h-full">
         <div className="flex flex-col-reverse overflow-auto text-f1-foreground sm:min-h-full sm:flex-row">
-          <div className="p-6 sm:basis-3/4">{mainContent}</div>
-          <div className="border-0 p-6 pb-0 sm:basis-1/4 sm:border-l sm:border-solid sm:border-l-f1-border-secondary sm:pb-6">
+          <div className="px-6 py-5 sm:basis-3/4">{mainContent}</div>
+          <div className="border-0 py-5 pl-2 pr-4 sm:basis-1/4 sm:border-l sm:border-solid sm:border-l-f1-border-secondary sm:pb-6">
             {sideContent}
           </div>
         </div>
