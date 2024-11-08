6b75f0de40ed7472ca33ea779644510978850887
Saúl Domínguez Gómez <saulydominguez@gmail.com>
2024-11-08 17:11:16 +0100

    Create base WidgetSimpleListItem component (#811)
    * simplify TasksList component and add stories for TaskItem
    
    * update Tasks component
    
    * create base WidgetSimpleListItem component
    
    * do not center task title with icon
    
    * add missing hideIcon in useMemo deps

--- lib/experimental/Widgets/Content/TasksList/TaskItem/index.tsx
+++ lib/experimental/Widgets/Content/TasksList/TaskItem/index.tsx
@@ -1,9 +1,6 @@
-import { Icon } from "@/components/Utilities/Icon"
-import { Counter } from "@/experimental/Information/Counter"
-import { AlertTag } from "@/experimental/Information/Tags/AlertTag"
-import { RawTag } from "@/experimental/Information/Tags/RawTag"
 import { Calendar, DottedCircle, InProgressTask } from "@/icons/app"
-import { cn } from "@/lib/utils"
+import { useMemo } from "react"
+import { WidgetSimpleListItem } from "../../ListItems/WidgetSimpleListItem"
 
 export type TaskStatus = "in-progress" | "todo"
 export interface Task {
@@ -20,64 +17,51 @@ export type TaskItemProps = {
   hideIcon?: boolean
 }
 
-type WrapperProps = {
-  onClick?: (ev: React.MouseEvent<HTMLAnchorElement>) => void
-  className: string
-  children: React.ReactNode
-}
-
-const Wrapper: React.FC<WrapperProps> = ({ onClick, className, children }) => {
-  return onClick ? (
-    <a className={className} onClick={onClick} tabIndex={0}>
-      {children}
-    </a>
-  ) : (
-    <div className={className} tabIndex={-1}>
-      {children}
-    </div>
-  )
-}
-
 export function TaskItem({
   task,
   status,
   onClick,
   hideIcon = false,
 }: TaskItemProps) {
-  const className = cn(
-    "flex flex-row items-center gap-1 rounded-sm border border-solid border-transparent px-2 py-1.5 text-f1-foreground",
-    onClick
-      ? "cursor-pointer hover:bg-f1-background-tertiary focus:border-f1-background-selected-bold focus:outline-none"
-      : undefined
-  )
-  const handleOnClick = (ev: React.MouseEvent<HTMLAnchorElement>) => {
-    ev.preventDefault()
+  const handleOnClick = () => {
     onClick?.(task)
   }
 
+  const icon = useMemo(() => {
+    if (hideIcon) return
+
+    if (status === "todo") {
+      return DottedCircle
+    }
+    if (status === "in-progress") {
+      return InProgressTask
+    }
+  }, [status, hideIcon])
+
   return (
-    <Wrapper onClick={handleOnClick} className={className}>
-      {!hideIcon && status === "todo" && (
-        <Icon
-          icon={DottedCircle}
-          size="md"
-          className="text-f1-icon-secondary"
-        />
-      )}
-      {!hideIcon && status === "in-progress" && (
-        <Icon icon={InProgressTask} size="md" className="text-f1-icon-info" />
-      )}
-      <p className="mt-0.5 line-clamp-2 flex-1 font-medium">{task.text}</p>
-      {!!task.badge && (
-        <>
-          {task.badge.isPastDue ? (
-            <AlertTag text={task.badge.text} level="critical" />
-          ) : (
-            <RawTag text={task.badge.text} icon={Calendar} />
-          )}
-        </>
-      )}
-      {!!task.counter && <Counter value={task.counter} />}
-    </Wrapper>
+    <WidgetSimpleListItem
+      id={task.id}
+      title={task.text}
+      icon={icon}
+      iconClassName={status === "todo" ? undefined : "text-f1-icon-info"}
+      alert={
+        task.badge?.isPastDue
+          ? {
+              text: task.badge.text,
+              level: "critical",
+            }
+          : undefined
+      }
+      rawTag={
+        task.badge && !task.badge.isPastDue
+          ? {
+              text: task.badge.text,
+              icon: Calendar,
+            }
+          : undefined
+      }
+      count={task.counter}
+      onClick={handleOnClick}
+    />
   )
 }
