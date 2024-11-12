6cc55fb646e471d948b247038f6e69c828d18503
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-11-12 09:32:12 +0100

    Test sidebar icons

--- lib/icons/animated/CheckCircle.tsx
+++ lib/icons/animated/CheckCircle.tsx
@@ -43,8 +43,6 @@ const CheckCircleAnimated = React.forwardRef<SVGSVGElement, CheckCircleProps>(
     return (
       <svg
         ref={ref}
-        width="24"
-        height="24"
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
