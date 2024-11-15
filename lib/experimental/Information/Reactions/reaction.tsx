b0332caacf1c2e4ddecd041ba1399f32801001a6
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-11-15 17:11:44 +0100

    confetti dependencies

--- lib/experimental/Information/Reactions/reaction.tsx
+++ lib/experimental/Information/Reactions/reaction.tsx
@@ -55,7 +55,7 @@ export function Reaction({
         disableInteraction: shouldReduceMotion,
       })
     }
-  }, [])
+  }, [emoji, shouldReduceMotion])
 
   const handleClick = () => {
     setCount(count + (isActive ? -1 : 1))
