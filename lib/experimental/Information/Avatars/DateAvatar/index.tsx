731ccf54b4b9083523d74716062236ed680a9ea0
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-11-18 16:49:49 +0100

    update background

--- lib/experimental/Information/Avatars/DateAvatar/index.tsx
+++ lib/experimental/Information/Avatars/DateAvatar/index.tsx
@@ -7,8 +7,8 @@ export const DateAvatar = ({ month, day }: Props) => {
   const monthName = month.slice(0, 3)
 
   return (
-    <div className="flex h-10 w-10 flex-col items-center justify-center rounded border border-solid border-f1-border-secondary">
-      <div className="text-xs pt-0.5 font-semibold uppercase leading-3 text-f1-foreground-critical">
+    <div className="bg-f1-background-inverse-secondary flex h-10 w-10 flex-col items-center justify-center rounded border border-solid border-f1-border-secondary">
+      <div className="text-xs pt-0.5 font-semibold uppercase leading-3 text-f1-foreground-critical dark:text-f1-foreground-inverse-secondary">
         {monthName}
       </div>
       <div className="flex items-center justify-center text-lg font-medium leading-tight">
