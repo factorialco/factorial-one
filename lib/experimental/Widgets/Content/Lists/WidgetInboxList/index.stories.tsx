db75793551310c104ff3228dba2fa911a8d2b5e3
Saúl Domínguez Gómez <saulydominguez@gmail.com>
2024-11-19 10:02:25 +0100

    Create widget inbox components (#850)
    * create widget inbox components
    
    * remove unused vars
    
    * add missing export

---
+++ lib/experimental/Widgets/Content/Lists/WidgetInboxList/index.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { WidgetInboxListItemProps } from "../../ListItems/WidgetInboxListItem"
+import {
+  Default as DefaulWidgetInboxListItemStory,
+  WithLongTitle as WithLongTitleWidgetInboxListItemStory,
+} from "../../ListItems/WidgetInboxListItem/index.stories"
+import { WidgetInboxList, WidgetInboxListProps } from "./index"
+
+const meta: Meta<WidgetInboxListProps> = {
+  component: WidgetInboxList,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  decorators: [
+    (Story) => (
+      <div className="w-[348px]">
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export default meta
+type Story = StoryObj<WidgetInboxListProps>
+
+export const Default: Story = {
+  args: {
+    items: new Array(5).fill(null).map(() => ({
+      ...(DefaulWidgetInboxListItemStory.args as WidgetInboxListItemProps),
+    })),
+    onClickItem: () => {},
+  },
+}
+
+export const WithLongTitles: Story = {
+  args: {
+    ...Default.args,
+    items: new Array(5).fill(null).map(() => ({
+      ...(WithLongTitleWidgetInboxListItemStory.args as WidgetInboxListItemProps),
+    })),
+  },
+}
