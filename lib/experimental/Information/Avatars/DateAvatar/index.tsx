fb8f5070e29930493053f4e91b2291400c036715
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-11-19 12:33:40 +0100

    Add DateAvatar (#849)
    * Add DateAvatar
    
    * update background
    
    * prettier

---
+++ lib/experimental/Information/Avatars/DateAvatar/index.tsx
@@ -0,0 +1,19 @@
+type Props = {
+  month: string
+  day: number
+}
+
+export const DateAvatar = ({ month, day }: Props) => {
+  const monthName = month.slice(0, 3)
+
+  return (
+    <div className="flex h-10 w-10 flex-col items-center justify-center rounded border border-solid border-f1-border-secondary bg-f1-background-inverse-secondary">
+      <div className="pt-0.5 text-xs font-semibold uppercase leading-3 text-f1-foreground-critical dark:text-f1-foreground-inverse-secondary">
+        {monthName}
+      </div>
+      <div className="flex items-center justify-center text-lg font-medium leading-tight">
+        {day}
+      </div>
+    </div>
+  )
+}
