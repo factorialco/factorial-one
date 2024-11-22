52a6e79243ffec8e262b8760bf93e560a62f9be8
Nik Lopin <nik.lopin@factorial.co>
2024-11-22 10:41:51 +0100

    add contribution page (#808)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -100,6 +100,7 @@ const preview: Preview = {
       storySort: {
         order: [
           "Introduction",
+          "How to contribute?",
           "Foundations",
           ["Colors", "Typography", "Spacing", "Borders", "Shadows"],
           "Components",
