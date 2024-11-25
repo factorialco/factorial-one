cc7665fc8b7a6506d33c41c26eff7fcfd2c82226
Nik Lopin <nik.lopin@factorial.co>
2024-11-25 13:27:20 +0100

    CompanySelector: do not render controls if there is only one company to select (#883)

--- lib/experimental/Navigation/Sidebar/CompanySelector/index.tsx
+++ lib/experimental/Navigation/Sidebar/CompanySelector/index.tsx
@@ -4,7 +4,7 @@ import { AvatarVariant } from "@/experimental/Information/Avatars/utils"
 import ChevronDown from "@/icons/app/ChevronDown"
 import { cn, focusRing } from "@/lib/utils"
 import { motion } from "framer-motion"
-import { useState } from "react"
+import { ReactNode, useMemo, useState } from "react"
 
 interface Company {
   id: string
@@ -23,26 +23,61 @@ export function CompanySelector({
   selected,
   onChange,
 }: CompanySelectorProps) {
+  const selectedCompany = useMemo(
+    () => companies.find((company) => company.id === selected) || companies[0],
+    [companies, selected]
+  )
+
+  if (companies.length === 1) {
+    return (
+      <div className="p-1.5">
+        <SelectedCompanyLabel company={selectedCompany} />
+      </div>
+    )
+  }
+
+  return (
+    <Selector
+      companies={companies}
+      selected={selectedCompany}
+      onChange={onChange}
+    >
+      <SelectedCompanyLabel company={selectedCompany} />
+    </Selector>
+  )
+}
+
+const Selector = ({
+  companies,
+  selected,
+  onChange,
+  children,
+}: {
+  companies: Company[]
+  selected: Company
+  onChange: (value: string) => void
+  children: ReactNode
+}) => {
   const [open, setOpen] = useState(false)
-
-  const options = companies.map((company) => ({
-    value: company.id,
-    label: company.name,
-    avatar: {
-      type: "company",
-      name: company.name,
-      src: company.logo,
-      "aria-label": `${company.name} logo`,
-    } satisfies AvatarVariant,
-  }))
-
-  const selectedCompany =
-    companies.find((company) => company.id === selected) || companies[0]
+  const options = useMemo(
+    () =>
+      companies.map((company) => ({
+        value: company.id,
+        label: company.name,
+        avatar: {
+          type: "company",
+          name: company.name,
+          src: company.logo,
+          "aria-label": `${company.name} logo`,
+        } satisfies AvatarVariant,
+      })),
+    [companies]
+  )
 
   return (
     <Select
       options={options}
-      value={selected}
+      value={selected.id}
       onChange={onChange}
       placeholder="Select a company"
       open={open}
@@ -50,32 +85,49 @@ export function CompanySelector({
     >
       <div
         className={cn(
-          "group flex w-fit flex-nowrap items-center justify-center gap-2 truncate rounded p-1.5 text-lg font-semibold text-f1-foreground transition-colors hover:bg-f1-background-hover data-[state=open]:bg-f1-background-hover",
+          "group flex w-fit flex-nowrap items-center justify-center gap-1 truncate rounded p-1.5 text-f1-foreground transition-colors hover:bg-f1-background-hover data-[state=open]:bg-f1-background-hover",
           focusRing()
         )}
         tabIndex={0}
-        title={selectedCompany?.name}
+        title={selected?.name}
       >
-        <CompanyAvatar
-          name={selectedCompany?.name?.[0]}
-          src={selectedCompany?.logo}
-          size="xsmall"
-        />
-        <div className="flex flex-row items-center gap-1">
-          <span className="truncate">{selectedCompany?.name}</span>
-          <div className="flex h-5 w-5 shrink-0 items-center justify-center">
-            <div className="flex h-3 w-3 items-center justify-center rounded-2xs bg-f1-background-secondary transition-all">
-              <motion.div
-                animate={{ rotate: open ? 180 : 0 }}
-                transition={{ duration: 0.2 }}
-                className="flex h-3 w-3 items-center justify-center"
-              >
-                <ChevronDown className="h-3 w-3 shrink-0 text-f1-icon-bold" />
-              </motion.div>
-            </div>
+        {children}
+        <div className="flex w-5 shrink-0 items-center justify-center">
+          <div className="flex h-3 w-3 items-center justify-center rounded-2xs bg-f1-background-secondary transition-all">
+            <motion.div
+              animate={{ rotate: open ? 180 : 0 }}
+              transition={{ duration: 0.2 }}
+              className="flex h-3 w-3 items-center justify-center"
+            >
+              <ChevronDown className="h-3 w-3 shrink-0 text-f1-icon-bold" />
+            </motion.div>
           </div>
         </div>
       </div>
     </Select>
+  )
+}
+
+const SelectedCompanyLabel = ({
+  company,
+}: {
+  company: CompanySelectorProps["companies"][number]
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex w-fit flex-nowrap items-center justify-center gap-2 rounded text-lg font-semibold text-f1-foreground transition-colors"
+      )}
+      title={company?.name}
+    >
+      <CompanyAvatar
+        name={company?.name?.[0]}
+        src={company?.logo}
+        size="xsmall"
+      />
+      <div className="flex flex-row items-center">
+        <span className="truncate">{company?.name}</span>
+      </div>
+    </div>
   )
 }
