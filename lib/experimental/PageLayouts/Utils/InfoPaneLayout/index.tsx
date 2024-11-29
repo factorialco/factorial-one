2ac3138207bb9119f60b8d7de6185aaa5365ad36
Saul Dominguez <saulydominguez@gmail.com>
2024-11-29 15:26:14 +0100

    make line full height

--- lib/experimental/PageLayouts/Utils/InfoPaneLayout/index.tsx
+++ lib/experimental/PageLayouts/Utils/InfoPaneLayout/index.tsx
@@ -10,8 +10,10 @@ export const InfoPaneLayout = forwardRef<HTMLDivElement, InfoPaneLayoutProps>(
     return (
       <div ref={ref} className="h-full overflow-auto">
         <div className="flex h-full flex-col-reverse overflow-auto text-f1-foreground sm:flex-row">
-          <main className="px-6 py-5 sm:basis-3/4">{mainContent}</main>
-          <aside className="border-0 py-5 pl-2 pr-4 sm:basis-1/4 sm:border-l sm:border-solid sm:border-l-f1-border-secondary sm:pb-6">
+          <main className="h-fit min-h-full border-0 px-6 py-5 sm:basis-3/4 sm:border-r sm:border-solid sm:border-r-f1-border-secondary">
+            {mainContent}
+          </main>
+          <aside className="py-5 pl-2 pr-4 sm:basis-1/4 sm:pb-6">
             {sideContent}
           </aside>
         </div>
