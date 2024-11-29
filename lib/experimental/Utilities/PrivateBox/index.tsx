fd75047f9453720153c71432ce26a32a45ec97b8
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-11-29 11:58:37 +0100

    New privacy style

--- lib/experimental/Utilities/PrivateBox/index.tsx
+++ lib/experimental/Utilities/PrivateBox/index.tsx
@@ -5,5 +5,15 @@ import { FC, PropsWithChildren } from "react"
 export const PrivateBox: FC<PropsWithChildren> = ({ children }) => {
   const { enabled } = usePrivacyMode()
 
-  return <div className={cn(enabled && "blur-md", "inline")}>{children}</div>
+  return (
+    <div
+      className={cn(
+        "inline-flex",
+        enabled &&
+          "overflow-hidden rounded-sm ring-1 ring-inset ring-f1-border-secondary"
+      )}
+    >
+      <div className={cn(enabled && "opacity-30 blur")}>{children}</div>
+    </div>
+  )
 }
