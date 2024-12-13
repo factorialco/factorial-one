edd385d8a8b7255e29072ce351b2c5faea892877
Saul Dominguez <saulydominguez@gmail.com>
2024-12-13 17:04:49 +0100

    get rid of CI issue with code quality

--- lib/experimental/Information/Avatars/DateAvatar/index.tsx
+++ lib/experimental/Information/Avatars/DateAvatar/index.tsx
@@ -10,7 +10,7 @@ export const DateAvatar = ({ date }: Props) => {
 
   return (
     <div className="flex h-10 w-10 flex-col items-center justify-center rounded border border-solid border-f1-border-secondary bg-f1-background-inverse-secondary">
-      <div className="text-f1-special-highlight pt-0.5 text-xs font-semibold uppercase leading-3 dark:text-f1-foreground-inverse-secondary">
+      <div className="pt-0.5 text-xs font-semibold uppercase leading-3 text-f1-special-highlight dark:text-f1-foreground-inverse-secondary">
         {month}
       </div>
       <div className="flex items-center justify-center text-lg font-medium leading-tight text-f1-foreground">
