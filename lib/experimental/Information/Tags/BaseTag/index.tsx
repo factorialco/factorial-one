4fd61d5fa0d6c5636920e37361a13d1d8a8f6da8
Saul Dominguez <saulydominguez@gmail.com>
2024-12-17 16:19:15 +0100

    better typescript definition for props

--- lib/experimental/Information/Tags/BaseTag/index.tsx
+++ lib/experimental/Information/Tags/BaseTag/index.tsx
@@ -2,8 +2,6 @@ import { cn } from "@/lib/utils"
 import { forwardRef, ReactNode } from "react"
 
 type Props = {
-  left?: ReactNode
-  text?: string
   /**
    * Sometimes you need to clarify the status for screen reader users
    * E.g., when showing a tooltip for sighted user, provide the tootip text to this prop because tooltips aren't accessible
@@ -11,34 +9,37 @@ type Props = {
   additionalAccesibleText?: string
   onClick?: () => void
   className?: string
-}
+} & (
+  | {
+      left: ReactNode
+      text?: string
+    }
+  | {
+      left?: ReactNode
+      text: string
+    }
+)
 
 export const BaseTag = forwardRef<HTMLDivElement, Props>(
-  ({ left, text, additionalAccesibleText, onClick, className }, ref) => {
-    if (!text && !left) {
-      throw Error("You need to specify either `text` or `left` prop")
-    }
-
-    return (
-      <div
-        ref={ref}
-        className={cn(
-          "line-clamp-1 flex flex-row items-center justify-start gap-0.5 rounded-full py-0.5 pr-2 text-base font-medium",
-          onClick && "cursor-pointer hover:bg-f1-background-hover",
-          !text && "aspect-square w-6 items-center justify-center p-1",
-          !left ? "pl-2" : "pl-1",
-          className
-        )}
-        onClick={onClick}
-      >
-        {left}
-        {!!text && <span>{text}</span>}
-        {additionalAccesibleText && (
-          <span className="sr-only">{additionalAccesibleText}</span>
-        )}
-      </div>
-    )
-  }
+  ({ left, text, additionalAccesibleText, onClick, className }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "line-clamp-1 flex flex-row items-center justify-start gap-0.5 rounded-full py-0.5 pr-2 text-base font-medium",
+        onClick && "cursor-pointer hover:bg-f1-background-hover",
+        !text && "aspect-square w-6 items-center justify-center p-1",
+        !left ? "pl-2" : "pl-1",
+        className
+      )}
+      onClick={onClick}
+    >
+      {left}
+      {!!text && <span>{text}</span>}
+      {additionalAccesibleText && (
+        <span className="sr-only">{additionalAccesibleText}</span>
+      )}
+    </div>
+  )
 )
 
 BaseTag.displayName = "BaseTag"
