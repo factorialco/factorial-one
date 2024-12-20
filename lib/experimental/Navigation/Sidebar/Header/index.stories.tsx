1e11e79ad2dfa71e6340a9d0b193d1fa430a2321
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2024-12-20 10:58:25 +0100

    `CompanySelector` notification & additional options (#995)
    * Display Badge in avatars
    
    * avatar size
    
    * update badge
    
    * Notification badge
    
    * Additional options
    
    * separator
    
    * fix stories
    
    * feedback

--- lib/experimental/Navigation/Sidebar/Header/index.stories.tsx
+++ lib/experimental/Navigation/Sidebar/Header/index.stories.tsx
@@ -1,3 +1,4 @@
+import { Image } from "@/icons/app"
 import type { Meta, StoryObj } from "@storybook/react"
 import { SidebarHeader } from "./index"
 
@@ -12,12 +13,31 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {
     companies: [
-      { id: "1", name: "Factorial", logo: "https://github.com/factorial.png" },
+      {
+        id: "1",
+        name: "Factorial",
+        logo: "https://github.com/factorialco.png",
+      },
       { id: "2", name: "Dazlog", logo: "https://github.com/dazlog.png" },
       { id: "3", name: "Acme Corp" },
     ],
     selected: "1",
     onChange: (company) => console.log("Selected company:", company),
     isExpanded: true,
+  },
+}
+
+export const Notification: Story = {
+  args: {
+    ...Default.args,
+    withNotification: true,
+    additionalOptions: [
+      {
+        label: "Upload company avatar",
+        value: "upload",
+        description: "Personalise your experience",
+        icon: Image,
+      },
+    ],
   },
 }
