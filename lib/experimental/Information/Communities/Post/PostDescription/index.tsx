3e7db94eeaef6cc24d79de26743e30da09c13eee
Saúl Domínguez Gómez <saulydominguez@gmail.com>
2025-01-08 17:49:39 +0100

    Create CommunityPost component (#1011)
    * create CommunityPost component
    
    * add loading cursor to skeleton
    
    * format ago using F1
    
    * small fixes
    
    * create PostDescription component
    
    * add PostDescription Skeleton story
    
    * apply styles only to F1 TextEditor
    
    * add some spacing
    
    * proposed changes
    
    * get rid of accesibility violations

---
+++ lib/experimental/Information/Communities/Post/PostDescription/index.tsx
@@ -0,0 +1,35 @@
+import { withSkeleton } from "@/lib/skeleton"
+import { cn } from "@/lib/utils"
+import { Skeleton } from "@/ui/skeleton"
+import "./TextEditorTheme.css"
+
+type HTMLString = string
+
+export type PostDescriptionProps = {
+  content: HTMLString
+  collapsed?: boolean
+}
+
+export const BasePostDescription = ({
+  content,
+  collapsed,
+}: PostDescriptionProps) => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: content }}
+      className={cn("FactorialOneTextEditor", collapsed && "line-clamp-5")}
+    />
+  )
+}
+
+export const PostDescriptionSkeleton = () => (
+  <div className="flex flex-col justify-around gap-3 py-2">
+    <Skeleton className="h-2.5 w-1/2 rounded-2xs" />
+    <Skeleton className="h-2.5 w-2/3 rounded-2xs" />
+  </div>
+)
+
+export const PostDescription = withSkeleton(
+  BasePostDescription,
+  PostDescriptionSkeleton
+)
