ca6c51edf08a4a78e744d578d160ae509a34d726
Niki Lopin <nik.lopin@factorial.co>
2025-01-10 12:24:50 +0100

    add matchMedia mock

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -7,3 +7,14 @@ afterEach(() => {
 })
 
 vi.stubGlobal("CSS", { supports: () => true })
+
+vi.stubGlobal("matchMedia", (query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: vi.fn(),
+  removeListener: vi.fn(),
+  addEventListener: vi.fn(),
+  removeEventListener: vi.fn(),
+  dispatchEvent: vi.fn(),
+}))
