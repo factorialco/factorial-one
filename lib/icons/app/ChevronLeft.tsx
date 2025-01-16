597e60f292446244b99a0a447317b85eef210cf9
Josep Jaume Rey <josepjaume@gmail.com>
2025-01-16 20:59:00 +0100

    Remove react import (#1062)
    * Add react import
    
    * Change icon name
    
    * Fix icons build
    
    * FIx icon colors

--- lib/icons/app/ChevronLeft.tsx
+++ lib/icons/app/ChevronLeft.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 import { Ref, forwardRef } from "react";
 const SvgChevronLeft = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" ref={ref} {...props}><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M15 6L9 12" vectorEffect="non-scaling-stroke" /><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M15 18L9 12" vectorEffect="non-scaling-stroke" /></svg>;
