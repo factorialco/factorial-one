2c3bb67f0903a693c70ec438535a581eaab35de0
Niki Lopin <nik.lopin@factorial.co>
2025-01-22 18:29:51 +0100

    fix tests

--- lib/lib/user-platform/UserPlatformProvider.tsx
+++ lib/lib/user-platform/UserPlatformProvider.tsx
@@ -5,16 +5,27 @@ export type Platform = "mac" | "windows" | "linux" | "mobile" | "unknown"
 
 const PlatformContext = createContext<Platform | null>(null)
 
+type UserPlatformProviderProps = {
+  children: React.ReactNode
+  /**
+   * Force set the platform. Won't trigger platform auto-detection if set.
+   */
+  platform?: Platform
+}
+
 export const UserPlatformProvider = ({
   children,
-}: {
-  children: React.ReactNode
-}) => {
-  const [userPlatform, setUserPlatform] = useState<Platform>("unknown")
+  platform,
+}: UserPlatformProviderProps) => {
+  const [userPlatform, setUserPlatform] = useState<Platform>(
+    platform ?? "unknown"
+  )
 
   useEffect(() => {
-    detectPlatform().then(setUserPlatform)
-  }, [])
+    if (platform === undefined) {
+      detectPlatform().then(setUserPlatform)
+    }
+  }, [platform])
 
   return (
     <PlatformContext.Provider value={userPlatform}>
