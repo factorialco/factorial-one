22c5d5a824042cf858d789b45876e14bade8c813
Sergio Carracedo <sergio.carracedo@factorial.co>
2025-02-06 09:37:43 +0100

    feat: F1SearchBox component

--- lib/ui/input.tsx
+++ lib/ui/input.tsx
@@ -12,9 +12,11 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <div
         className={cn(
-          "flex h-10 w-full rounded-sm border-2 border-solid border-f1-border bg-f1-background px-3 py-2 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-f1-foreground-secondary/60 hover:border-f1-border-hover disabled:cursor-not-allowed disabled:bg-f1-background-secondary disabled:opacity-50",
+          "flex h-10 w-full rounded-sm border-2 border-solid border-f1-border bg-f1-background px-3 py-2 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-f1-foreground-secondary/60 hover:border-f1-border-hover",
           focusRing("focus-visible:border-f1-border-hover"),
           icon ? "flex gap-1.5 ps-2" : "ps-3",
+          props.disabled &&
+            "cursor-not-allowed bg-f1-background-secondary opacity-50",
           className
         )}
       >
@@ -25,7 +27,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           {...props}
           className={cn(
             !clearable ? "[&::-webkit-search-cancel-button]:hidden" : "",
-            "growth w-full shrink"
+            "growth w-full shrink disabled:cursor-not-allowed"
           )}
         />
       </div>
