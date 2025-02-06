2b5b83514ac9262ec048ad4adc1d53bb7117ee69
Dani Moreno <hi@dazlog.com>
2025-02-06 09:03:56 +0100

    link

--- lib/experimental/Information/Headers/SectionHeader/index.stories.tsx
+++ lib/experimental/Information/Headers/SectionHeader/index.stories.tsx
@@ -15,16 +15,29 @@ type Story = StoryObj<typeof SectionHeader>
 
 export const Default: Story = {
   args: {
-    title: "Section title",
-    description: "Section description",
+    title: "What is a template?",
+    description:
+      "Create document templates to automate the generation of documents with employee information. You can upload PDFs with fillable capabilities containing variables, DOCx files with variables, or any other types of documents that you need to send to employees.",
     action: {
-      label: "Action",
+      label: "Add template",
       icon: Icon.Add,
       onClick: fn(),
     },
     help: {
       label: "Help Center link",
-      onClick: fn(),
+      href: "https://help.factorialhr.com/",
+    },
+    separator: "bottom",
+  },
+}
+
+export const NoAction: Story = {
+  args: {
+    title: "Course catalog",
+    description: "Select any course you would like to request.",
+    help: {
+      label: "Help Center link",
+      href: "https://help.factorialhr.com/",
     },
   },
 }
--- lib/experimental/Information/Headers/SectionHeader/index.tsx
+++ lib/experimental/Information/Headers/SectionHeader/index.tsx
@@ -1,47 +1,83 @@
 import { Button, ButtonProps } from "@/components/Actions/Button"
-import { IconType } from "@/components/Utilities/Icon"
-import { ExternalLink } from "@/icons/app"
+import { Icon, IconType } from "@/components/Utilities/Icon"
+import ExternalLink from "@/icons/app/ExternalLink"
+import { cn, focusRing } from "@/lib/utils"
 
 type Props = {
+  /** Main heading text */
   title: string
+  /** Description text below the title */
   description: string
+  /** Optional action button */
   action?: Pick<ButtonProps, "label" | "onClick"> & {
-    icon: IconType
+    icon?: IconType
   }
+  /** Optional help link, usually to a Help Center article */
   help?: {
     label: string
-    onClick: () => void
+    href: string
   }
+  /** Optional separator */
+  separator?: "top" | "bottom"
 }
 
-export const SectionHeader = ({ title, description, action, help }: Props) => {
+export const SectionHeader = ({
+  title,
+  description,
+  action,
+  help,
+  separator,
+}: Props) => {
   return (
-    <div className="flex justify-between px-6 py-5">
+    <div
+      className={cn(
+        "border-1 flex flex-col justify-between gap-4 border-dashed border-transparent px-6 py-5 md:flex-row md:gap-2",
+        separator === "top" && "border-t-f1-border",
+        separator === "bottom" && "border-b-f1-border"
+      )}
+    >
       <div className="flex grow flex-col gap-3">
-        <div className="flex flex-col gap-1">
+        <div className="flex max-w-[640px] flex-col gap-1">
           <h2 className="text-lg font-semibold text-f1-foreground">{title}</h2>
           <p className="text-f1-foreground-secondary">{description}</p>
         </div>
         {help && (
           <div className="w-fit">
-            <Button
-              label={help.label}
-              variant="neutral"
-              icon={ExternalLink}
-              size="sm"
-              onClick={help.onClick}
-            />
+            <a
+              href={help.href}
+              target="_blank"
+              className={cn(
+                "flex items-center gap-1 rounded-sm bg-f1-background-secondary px-2 py-0.5 text-base font-medium text-f1-foreground no-underline transition-colors hover:bg-f1-background-secondary-hover [&>svg]:text-f1-foreground-secondary",
+                focusRing()
+              )}
+            >
+              {help.label}
+              <Icon icon={ExternalLink} size="sm" />
+            </a>
           </div>
         )}
       </div>
       {action && (
-        <Button
-          label={action.label}
-          variant="ghost"
-          icon={action.icon}
-          size="md"
-          onClick={action.onClick}
-        />
+        <>
+          <div className="hidden md:block">
+            <Button
+              label={action.label}
+              variant="ghost"
+              icon={action.icon}
+              size="md"
+              onClick={action.onClick}
+            />
+          </div>
+          <div className="w-full md:hidden [&>button]:w-full">
+            <Button
+              label={action.label}
+              variant="outline"
+              icon={action.icon}
+              size="lg"
+              onClick={action.onClick}
+            />
+          </div>
+        </>
       )}
     </div>
   )
