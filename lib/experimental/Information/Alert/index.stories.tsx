1e7ac1e2d76e8acce723363a9805c5ead386f89f
Dani Moreno <hi@dazlog.com>
2025-02-10 14:11:47 +0100

    fix lint

--- lib/experimental/Information/Alert/index.stories.tsx
+++ lib/experimental/Information/Alert/index.stories.tsx
@@ -36,7 +36,8 @@ export const Positive: Story = {
     <Alert {...props}>
       <AlertTitle>Training completed!</AlertTitle>
       <AlertDescription>
-        You successfully completed the training 'Eat. Sleep. Command Z. Repeat'.
+        You successfully completed the training &apos;Eat. Sleep. Command Z.
+        Repeat&apos;.
       </AlertDescription>
     </Alert>
   ),
