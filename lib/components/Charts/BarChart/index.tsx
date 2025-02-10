50b4666a0ac291e2a55884831f26ec85a7675e8e
Saúl Domínguez Gómez <saulydominguez@gmail.com>
2025-02-10 17:19:03 +0100

    Add support to show value under x label in bar chart (#1165)
    * add support to show value under x label in bar chart
    
    * add support to highlight last bar
    
    * localize string

--- lib/components/Charts/BarChart/index.tsx
+++ lib/components/Charts/BarChart/index.tsx
@@ -44,6 +44,8 @@ export type BarChartProps<K extends ChartConfig = ChartConfig> =
     type?: "simple" | "stacked" | "stacked-by-sign"
     label?: boolean
     legend?: boolean
+    showValueUnderLabel?: boolean
+    highlightLastBar?: boolean
     onClick?: (data: ChartDataPoint<K>) => void
   }
 
@@ -57,12 +59,26 @@ const _BarChart = <K extends ChartConfig>(
     type = "simple",
     aspect,
     legend,
+    showValueUnderLabel = false,
+    highlightLastBar = false,
     onClick,
   }: BarChartProps<K>,
   ref: ForwardedRef<HTMLDivElement>
 ) => {
   const bars = Object.keys(dataConfig) as (keyof ChartConfig)[]
-  const preparedData = prepareData(data)
+  const preparedData = prepareData(data).map((item, index, array) => {
+    if (highlightLastBar && bars.length === 1 && !dataConfig[bars[0]]?.color) {
+      return {
+        ...item,
+        fill:
+          index === array.length - 1
+            ? autoColor(0)
+            : `hsl(${autoColor(0, false)} / 0.5)`,
+      }
+    }
+
+    return item
+  })
   const maxLabelWidth = Math.max(
     ...preparedData.flatMap((el) =>
       bars.map((key) =>
@@ -84,6 +100,7 @@ const _BarChart = <K extends ChartConfig>(
           left: yAxis && !yAxis.hide ? 0 : 12,
           right: 12,
           top: label ? 24 : 0,
+          bottom: showValueUnderLabel ? 24 : 12,
         }}
         stackOffset={type === "stacked-by-sign" ? "sign" : undefined}
         onClick={(data) => {
@@ -114,7 +131,54 @@ const _BarChart = <K extends ChartConfig>(
           width={yAxis.width ?? maxLabelWidth + 20}
           hide={yAxis.hide}
         />
-        <XAxis {...xAxisProps(xAxis)} hide={xAxis?.hide} />
+        <XAxis
+          {...xAxisProps(xAxis)}
+          hide={xAxis?.hide}
+          tick={
+            showValueUnderLabel
+              ? (props) => {
+                  const { x, y, payload } = props
+                  const values =
+                    data.find((d) => d.label === payload.value)?.values || ""
+
+                  const value =
+                    Object.keys(values).length === 1
+                      ? Object.values(values)?.[0]
+                      : undefined
+
+                  const normalizedValue =
+                    value !== undefined && yAxis.tickFormatter
+                      ? yAxis.tickFormatter(`${value}`)
+                      : value.toLocaleString()
+
+                  return (
+                    <g transform={`translate(${x},${y})`}>
+                      <text
+                        x={0}
+                        y={0}
+                        dy={12}
+                        textAnchor="middle"
+                        className="text-sm font-medium !text-f1-foreground-secondary"
+                      >
+                        {payload.value}
+                      </text>
+                      {!!value && (
+                        <text
+                          x={0}
+                          y={0}
+                          dy={28}
+                          textAnchor="middle"
+                          className="!fill-f1-foreground text-sm font-medium"
+                        >
+                          {normalizedValue}
+                        </text>
+                      )}
+                    </g>
+                  )
+                }
+              : undefined
+          }
+        />
 
         {bars.map((key, index) => (
           <Bar
@@ -126,7 +190,11 @@ const _BarChart = <K extends ChartConfig>(
                 ? "stack"
                 : undefined
             }
-            fill={dataConfig[key].color || autoColor(index)}
+            fill={
+              highlightLastBar
+                ? (((data: { fill: string }) => data.fill) as unknown as string)
+                : (dataConfig[key].color ?? autoColor(index))
+            }
             radius={type === "stacked-by-sign" ? [4, 4, 0, 0] : 4}
             maxBarSize={32}
           >
--- lib/components/Charts/utils/colors.tsx
+++ lib/components/Charts/utils/colors.tsx
@@ -1,19 +1,20 @@
 const availableColors = {
-  "chart-1": "hsl(var(--chart-1))",
-  "chart-2": "hsl(var(--chart-2))",
-  "chart-3": "hsl(var(--chart-3))",
-  "chart-4": "hsl(var(--chart-4))",
-  "chart-5": "hsl(var(--chart-5))",
-  "chart-6": "hsl(var(--chart-6))",
-  "chart-7": "hsl(var(--chart-7))",
-  "chart-8": "hsl(var(--chart-8))",
+  "chart-1": "var(--chart-1)",
+  "chart-2": "var(--chart-2)",
+  "chart-3": "var(--chart-3)",
+  "chart-4": "var(--chart-4)",
+  "chart-5": "var(--chart-5)",
+  "chart-6": "var(--chart-6)",
+  "chart-7": "var(--chart-7)",
+  "chart-8": "var(--chart-8)",
 }
 
 export type ChartColor = keyof typeof availableColors
 
-export const autoColor = (index: number) => {
+export const autoColor = (index: number, withHSL = true) => {
   const colors = Object.values(availableColors)
-  return colors[index % colors.length]
+  const color = colors[index % colors.length]
+  return withHSL ? `hsl(${color})` : color
 }
 
 export const chartColor = (color: ChartColor) => availableColors[color]
