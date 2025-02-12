67a72a9bf4ef5c948cae94959ff67101821965c8
Sergio Carracedo <sergio.carracedo@factorial.co>
2025-02-12 14:45:10 +0100

    feat: Select slots for top and botton

--- lib/ui/select.tsx
+++ lib/ui/select.tsx
@@ -57,36 +57,79 @@ const SelectScrollDownButton = React.forwardRef<
 SelectScrollDownButton.displayName =
   SelectPrimitive.ScrollDownButton.displayName
 
+type SelectContentComponent = React.ForwardRefExoticComponent<
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content> &
+    React.RefAttributes<HTMLDivElement>
+> & {
+  Top: ({ children }: { children: React.ReactNode }) => React.ReactNode
+  Bottom: ({ children }: { children: React.ReactNode }) => React.ReactNode
+}
+
+const SelectContentTop = ({ children }: { children: React.ReactNode }) => (
+  <div>{children}</div>
+)
+
+const SelectContentBottom = ({ children }: { children: React.ReactNode }) => (
+  <div>{children}</div>
+)
+
 const SelectContent = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
-  <SelectPrimitive.Portal>
-    <SelectPrimitive.Content
-      ref={ref}
-      className={cn(
-        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-solid border-f1-border-secondary bg-f1-background text-f1-foreground shadow-md data-[state=closed]:fade-out-0 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 motion-safe:data-[state=open]:animate-in motion-safe:data-[state=closed]:animate-out motion-safe:data-[state=open]:fade-in-0 motion-safe:data-[state=closed]:zoom-out-95 motion-safe:data-[state=open]:zoom-in-95 motion-safe:data-[side=bottom]:slide-in-from-top-2",
-        position === "popper" &&
-          "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
-        className
-      )}
-      position={position}
-      {...props}
-    >
-      <SelectScrollUpButton />
-      <SelectPrimitive.Viewport
+>(({ className, children, position = "popper", ...props }, ref) => {
+  // Split children into top, bottom and main
+  const childrenArray = React.Children.toArray(children)
+  const childrenTop = childrenArray.filter(
+    (child) => React.isValidElement(child) && child.type === SelectContentTop
+  )
+
+  const childrenBottom = childrenArray.filter(
+    (child) => React.isValidElement(child) && child.type === SelectContentBottom
+  )
+
+  const childrenMain = childrenArray.filter(
+    (child) =>
+      React.isValidElement(child) &&
+      child.type !== SelectContentTop &&
+      child.type !== SelectContentBottom
+  )
+  // ------------------------------------
+
+  return (
+    <SelectPrimitive.Portal>
+      <SelectPrimitive.Content
+        ref={ref}
         className={cn(
+          "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-solid border-f1-border-secondary bg-f1-background text-f1-foreground shadow-md data-[state=closed]:fade-out-0 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 motion-safe:data-[state=open]:animate-in motion-safe:data-[state=closed]:animate-out motion-safe:data-[state=open]:fade-in-0 motion-safe:data-[state=closed]:zoom-out-95 motion-safe:data-[state=open]:zoom-in-95 motion-safe:data-[side=bottom]:slide-in-from-top-2",
           position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+            "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
+          className
         )}
+        position={position}
+        {...props}
       >
-        {children}
-      </SelectPrimitive.Viewport>
-      <SelectScrollDownButton />
-    </SelectPrimitive.Content>
-  </SelectPrimitive.Portal>
-))
+        {!!childrenTop.length && childrenTop}
+        <SelectScrollUpButton />
+        <SelectPrimitive.Viewport
+          className={cn(
+            position === "popper" &&
+              "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+          )}
+        >
+          {childrenMain}
+        </SelectPrimitive.Viewport>
+        <SelectScrollDownButton />
+        {!!childrenBottom.length && childrenBottom}
+      </SelectPrimitive.Content>
+    </SelectPrimitive.Portal>
+  )
+}) as SelectContentComponent
+
 SelectContent.displayName = SelectPrimitive.Content.displayName
+
+SelectContent.Top = SelectContentTop
+
+SelectContent.Bottom = SelectContentBottom
 
 const SelectLabel = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Label>,
