7bb3c920de47258e8e71dfe1559f341f6402848d
Dani Moreno <hi@dazlog.com>
2025-02-18 12:16:27 +0100

    tooltip on max counter

--- lib/experimental/Information/Avatars/AvatarList/MaxCounter.tsx
+++ lib/experimental/Information/Avatars/AvatarList/MaxCounter.tsx
@@ -1,14 +1,11 @@
 import { Icon } from "@/components/Utilities/Icon"
 import { EllipsisHorizontal } from "@/icons/app"
+import { cn } from "@/lib/utils"
 import { sizes, type } from "@/ui/avatar"
-import {
-  TooltipContent,
-  Tooltip as TooltipPrimitive,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/ui/tooltip"
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/ui/hover-card"
+import { ScrollArea, ScrollBar } from "@/ui/scrollarea"
 import { cva } from "cva"
-import { type AvatarVariant } from "../Avatar"
+import { Avatar, AvatarVariant } from "../Avatar"
 
 const sizeVariants = cva({
   base: "flex shrink-0 items-center justify-center bg-f1-background-secondary font-medium text-f1-foreground-secondary",
@@ -25,6 +22,28 @@ const sizeVariants = cva({
       rounded: "!rounded-full",
     },
   },
+  compoundVariants: [
+    {
+      size: "small",
+      type: "rounded",
+      className: "px-1.5",
+    },
+    {
+      size: "medium",
+      type: "rounded",
+      className: "px-2",
+    },
+    {
+      size: "large",
+      type: "rounded",
+      className: "px-4",
+    },
+    {
+      size: "xlarge",
+      type: "rounded",
+      className: "px-5",
+    },
+  ],
   defaultVariants: {
     size: "medium",
     type: "base",
@@ -45,7 +64,7 @@ export const MaxCounter = ({
   list,
 }: Props) => {
   const counter = (
-    <div className={sizeVariants({ size, type })}>
+    <div className={cn("font-medium", sizeVariants({ size, type }))}>
       {size === "xsmall" ? (
         <Icon icon={EllipsisHorizontal} size="xs" />
       ) : (
@@ -57,21 +76,31 @@ export const MaxCounter = ({
   if (!list?.length) return counter
 
   return (
-    <TooltipProvider>
-      <TooltipPrimitive>
-        <TooltipTrigger asChild>{counter}</TooltipTrigger>
-        <TooltipContent>
-          <div className="flex flex-col gap-1">
-            {list.map((avatar, index) => (
-              <div key={index}>
+    <HoverCard>
+      <HoverCardTrigger asChild>{counter}</HoverCardTrigger>
+      <HoverCardContent side="top">
+        <ScrollArea className="[*[data-state=visible]_div]:bg-f1-background flex max-h-[172px] flex-col">
+          {list.map((avatar, index) => (
+            <div
+              key={index}
+              className="flex w-[180px] min-w-0 items-center gap-1.5 px-2 py-1 [&:first-child]:pt-2 [&:last-child]:pb-2"
+            >
+              <div className="h-6 w-6 shrink-0">
+                <Avatar avatar={avatar} size="small" />
+              </div>
+              <div className="min-w-0 flex-1 truncate font-semibold">
                 {avatar.type === "person"
                   ? `${avatar.firstName} ${avatar.lastName}`
                   : avatar.name}
               </div>
-            ))}
-          </div>
-        </TooltipContent>
-      </TooltipPrimitive>
-    </TooltipProvider>
+            </div>
+          ))}
+          <ScrollBar
+            orientation="vertical"
+            className="[&_div]:bg-f1-background"
+          />
+        </ScrollArea>
+      </HoverCardContent>
+    </HoverCard>
   )
 }
--- lib/experimental/Information/Avatars/AvatarList/index.stories.tsx
+++ lib/experimental/Information/Avatars/AvatarList/index.stories.tsx
@@ -1,6 +1,63 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import { AvatarList } from "./index"
 
+const dummyPeople = [
+  {
+    type: "person" as const,
+    firstName: "Nik",
+    lastName: "Lopin",
+    src: "https://github.com/nlopin.png",
+  },
+  {
+    type: "person" as const,
+    firstName: "Josep Jaume",
+    lastName: "Rey",
+    src: "https://github.com/josepjaume.png",
+  },
+  {
+    type: "person" as const,
+    firstName: "Saúl",
+    lastName: "Domínguez",
+  },
+]
+
+const dummyCompanies = [
+  {
+    type: "company" as const,
+    name: "Factorial",
+    src: "https://github.com/factorialco.png",
+  },
+  {
+    type: "company" as const,
+    name: "Itnig",
+  },
+  {
+    type: "company" as const,
+    name: "Another cool company",
+  },
+]
+
+const dummyTeams = [
+  { type: "team" as const, name: "Designers" },
+  { type: "team" as const, name: "Engineering" },
+  { type: "team" as const, name: "Product Management" },
+]
+
+function getDummyAvatars(
+  count: number,
+  type: "person" | "company" | "team" = "person"
+) {
+  const sourceData = {
+    person: dummyPeople,
+    company: dummyCompanies,
+    team: dummyTeams,
+  }[type]
+
+  return Array.from({ length: count }, (_, index) => ({
+    ...sourceData[index % sourceData.length],
+  }))
+}
+
 const meta: Meta<typeof AvatarList> = {
   component: AvatarList,
   title: "Avatars/AvatarList",
@@ -23,6 +80,9 @@ const meta: Meta<typeof AvatarList> = {
       },
       { type: "person", firstName: "Saúl", lastName: "Domínguez" },
     ],
+  },
+  parameters: {
+    layout: "centered",
   },
 } satisfies Meta<typeof AvatarList>
 
@@ -76,13 +136,7 @@ export const CompaniesWithTooltip: Story = {
 export const WithMaxAvatars: Story = {
   args: {
     ...Default.args,
-    avatars: Array(50)
-      .fill(0)
-      .map((_, index: number) => ({
-        type: "person" as const,
-        firstName: `Employee ${index + 1}`,
-        lastName: "Surname",
-      })),
+    avatars: getDummyAvatars(50, "person"),
     max: 3,
   },
 }
@@ -90,12 +144,7 @@ export const WithMaxAvatars: Story = {
 export const CompaniesWithMaxAvatars: Story = {
   args: {
     ...Companies.args,
-    avatars: Array(50)
-      .fill(0)
-      .map((_, index: number) => ({
-        type: "company" as const,
-        name: `Company ${index + 1}`,
-      })),
+    avatars: getDummyAvatars(50, "company"),
     max: 3,
   },
 }
