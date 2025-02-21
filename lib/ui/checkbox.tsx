6c1fc3d4d1c1e3296ad623579afbdace39baae16
Dani Moreno <hi@dazlog.com>
2025-02-21 17:58:57 +0100

    Update checkbox

--- lib/ui/checkbox.tsx
+++ lib/ui/checkbox.tsx
@@ -1,6 +1,8 @@
+import { Icon } from "@/components/Utilities/Icon"
+import { Check, Minus } from "@/icons/app"
 import { cn, focusRing } from "@/lib/utils"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { Check, Minus } from "lucide-react"
+import { AnimatePresence, motion } from "framer-motion"
 import * as React from "react"
 
 const Checkbox = React.forwardRef<
@@ -8,38 +10,69 @@ const Checkbox = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> & {
     indeterminate?: boolean
   }
->(({ className, indeterminate, ...props }, ref) => (
-  <div className="flex items-center gap-4">
-    <CheckboxPrimitive.Root
-      ref={ref}
-      id={props.id}
-      className={cn(
-        "ring-offset-background focus-visible:ring-ring data-[state=checked]:bg-primary peer h-4 w-4 shrink-0 rounded-2xs border-2 border-solid border-f1-border disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:text-f1-foreground",
-        focusRing(),
-        className
-      )}
-      {...props}
-      checked={props.checked}
-      onCheckedChange={props.onCheckedChange}
-    >
-      <CheckboxPrimitive.Indicator className="flex items-center justify-center text-current">
-        {indeterminate ? (
-          <Minus className="h-3 w-3" />
-        ) : (
-          <Check className="h-3 w-3" />
+>(({ className, indeterminate, disabled, ...props }, ref) => {
+  // Generate a unique ID if one isn't provided
+  const uniqueId = React.useId()
+  const checkboxId = props.id || uniqueId
+
+  return (
+    <div className="flex items-center">
+      <CheckboxPrimitive.Root
+        {...props}
+        ref={ref}
+        id={checkboxId}
+        name={checkboxId}
+        className={cn(
+          "h-5 w-5 shrink-0 rounded-xs border border-solid border-f1-border text-f1-foreground-selected transition-[background-color,border-color] hover:border-f1-border-hover data-[state=checked]:bg-f1-background-selected-bold data-[state=checked]:text-f1-foreground-inverse hover:data-[state=checked]:border-transparent",
+          disabled && "cursor-not-allowed opacity-50 hover:border-f1-border",
+          indeterminate &&
+            "data-[state=checked]:bg-f1-background data-[state=checked]:text-f1-foreground-selected hover:data-[state=checked]:border-f1-border-hover",
+          props.checked &&
+            disabled &&
+            "data-[state=checked]:bg-f1-background-secondary data-[state=checked]:text-f1-foreground-secondary",
+          focusRing(),
+          className
         )}
-      </CheckboxPrimitive.Indicator>
-    </CheckboxPrimitive.Root>
-    {props.title && (
-      <label
-        htmlFor={props.id}
-        className="flex items-center justify-center text-current hover:cursor-pointer"
+        checked={props.checked}
+        onCheckedChange={props.onCheckedChange}
+        disabled={disabled}
       >
-        {props.title}
-      </label>
-    )}
-  </div>
-))
+        <AnimatePresence>
+          <CheckboxPrimitive.Indicator className="flex items-center justify-center text-current transition-none">
+            <motion.div
+              initial={{ opacity: 0, scale: 0.6 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.6 }}
+              transition={{
+                ease: [0.175, 0.885, 0.32, 1.275],
+                duration: 0.4,
+              }}
+              className="flex items-center justify-center"
+            >
+              {indeterminate ? (
+                <Icon icon={Minus} size="sm" />
+              ) : (
+                <Icon icon={Check} size="sm" />
+              )}
+            </motion.div>
+          </CheckboxPrimitive.Indicator>
+        </AnimatePresence>
+      </CheckboxPrimitive.Root>
+      {props.title && (
+        <label
+          htmlFor={checkboxId}
+          className={cn(
+            "flex items-center justify-center pl-2.5 text-current hover:cursor-pointer disabled:cursor-not-allowed disabled:opacity-50",
+            disabled && "cursor-not-allowed opacity-50 hover:cursor-not-allowed"
+          )}
+        >
+          {props.title}
+        </label>
+      )}
+    </div>
+  )
+})
+
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
 const CheckboxRoot = CheckboxPrimitive.Root
