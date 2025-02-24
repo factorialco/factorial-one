9c3cb6ed32d5f5ac0cad4deeb16d81019b559af5
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2025-02-24 12:12:21 +0100

    Update Checkbox component (#1225)
    * Update checkbox
    
    * better examples
    
    * useId

--- lib/experimental/Forms/Fields/Checkbox/index.tsx
+++ lib/experimental/Forms/Fields/Checkbox/index.tsx
@@ -1,13 +1,63 @@
-import { Checkbox as ShadcnInput } from "@/ui/checkbox"
+import { Checkbox as CheckboxRoot } from "@/ui/checkbox"
 
-type CheckboxProps = Omit<
-  React.ComponentPropsWithoutRef<typeof ShadcnInput>,
-  "asChild"
-> & {
+interface CheckboxProps {
+  /**
+   * The title of the checkbox
+   */
   title?: string
+
+  /**
+   * The id of the checkbox
+   */
+  id?: string
+
+  /**
+   * The checked state of the checkbox
+   * @default false
+   */
+  checked?: boolean
+
+  /**
+   * Whether the checkbox is indeterminate
+   * @default false
+   */
+  indeterminate?: boolean
+
+  /**
+   * The callback function that is called when the checkbox is checked
+   */
   onCheckedChange?: (checked: boolean) => void
+
+  /**
+   * Whether the checkbox is disabled
+   * @default false
+   */
+  disabled?: boolean
+
+  /**
+   * The value of the checkbox
+   */
+  value?: string
 }
 
-const Checkbox: React.FC<CheckboxProps> = ShadcnInput
-
-export default Checkbox
+export function Checkbox({
+  title,
+  onCheckedChange,
+  id,
+  disabled,
+  indeterminate = false,
+  checked = false,
+  value,
+}: CheckboxProps) {
+  return (
+    <CheckboxRoot
+      title={title}
+      onCheckedChange={onCheckedChange}
+      id={id}
+      disabled={disabled}
+      indeterminate={indeterminate}
+      checked={checked}
+      value={value}
+    />
+  )
+}
