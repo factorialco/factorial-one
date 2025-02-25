612972c008fd527877c49d3d610f1be1bde33aab
Nik Lopin <nik.lopin@factorial.co>
2025-02-25 14:44:34 +0100

    Revert "Chore/exports (#1230)" (#1236)
    This reverts commit 8810cb3603242a334f1bac48f5d00ca1a6d40a88.

--- docs/index.md
+++
@@ -1,7 +0,0 @@
-# factorial-one
-
-`factorial-one` is a set of component, hooks, utilities used as foundations for the factorial's app
-
-- [Getting started](./getting-started.md)
-- [Local Development](development/development.md)
-    - [Using factorial-one without build it](development/using-factorial-one-source.md)
--- vite.config.ts
+++ vite.config.ts
@@ -1,64 +1,25 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react"
-import { consola } from "consola"
-import dotenv from "dotenv"
 import path, { resolve } from "path"
-import { defineConfig, Plugin } from "vite"
+import { defineConfig } from "vite"
 import dts from "vite-plugin-dts"
 import { libInjectCss } from "vite-plugin-lib-inject-css"
-import { buildSyncPlugin } from "./vite/build-sync.plugin"
-
-dotenv.config({
-  path: [".env.local", ".env"],
-})
-
-const extraPlugins: Plugin[] = []
-
-/* Build sync */
-const defaultCoderWorkspaceFolder =
-  "/home/factorial/workspace/factorial/frontend/node_modules/@factorialco/factorial-one"
-
-const buildSyncArg = process.argv.find((arg) => arg.startsWith("--buildSync"))
-const buildSync = !!buildSyncArg
-const buildSyncValue = buildSyncArg
-  ? buildSyncArg.split("=")[1] || process.env.CODER_REMOTE
-  : null
-
-if (buildSync) {
-  if (!buildSyncValue) {
-    consola.error(
-      "The buildSync flag must remote target or you can set it in the env variable CODER_REMOTE in the `.env.local` file"
-    )
-    process.exit(1)
-  }
-
-  const [remote, remoteFolder] = buildSyncValue.split(":")
-
-  const target = [remote, remoteFolder || defaultCoderWorkspaceFolder]
-    .filter(Boolean)
-    .join(":")
-
-  extraPlugins.push(
-    buildSyncPlugin({
-      target,
-    })
-  )
-}
-/* ------------ Build sync end ------*/
-
-if (process.env.BUILD_TYPES) {
-  extraPlugins.push(
-    dts({
-      include: ["lib", "src"],
-      exclude: ["**/*.stories.tsx"],
-      rollupTypes: true,
-    })
-  )
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss(), ...extraPlugins],
+  plugins: [
+    react(),
+    libInjectCss(),
+    ...(process.env.BUILD_TYPES
+      ? [
+          dts({
+            include: ["lib", "src"],
+            exclude: ["**/*.stories.tsx"],
+            rollupTypes: true
+          }),
+        ]
+      : []),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./lib"),
@@ -76,11 +37,9 @@ export default defineConfig({
       },
       formats: ["es"],
     },
-    outDir: "dist/lib",
     copyPublicDir: false,
     rollupOptions: {
       external: ["react/jsx-runtime", "react", "react-dom"],
-      maxParallelFileOps: 100, // Workaround to fix rebuild https://github.com/vitejs/vite/issues/19410#issuecomment-2661835482
       output: {
         globals: {
           react: "React",
@@ -90,6 +49,6 @@ export default defineConfig({
   },
   test: {
     environment: "jsdom",
-    setupFiles: ["./vite/vitest.setup.ts"],
+    setupFiles: ["./vitest.setup.ts"],
   },
 })
