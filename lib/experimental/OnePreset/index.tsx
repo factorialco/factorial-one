356ee2ba3e925832f03f4b2f92de1dca1854d2d0
Dani Moreno <hi@dazlog.com>
2025-02-26 19:32:44 +0100

    Add `Preset` component

---
+++ lib/experimental/OnePreset/index.tsx
@@ -0,0 +1,29 @@
+import { Counter } from "@/experimental/Information/Counter"
+import { cn, focusRing } from "@/lib/utils"
+interface PresetProps {
+  label: string
+  number?: number
+  onClick?: () => void
+  selected?: boolean
+}
+
+export const Preset = ({ label, number, onClick, selected }: PresetProps) => {
+  return (
+    <button
+      className={cn(
+        "flex cursor-default appearance-none items-center gap-2 rounded px-2.5 py-1.5 font-medium outline outline-1 outline-f1-border transition-all",
+        "focus:outline focus:outline-1",
+        number && "pr-1.5",
+        onClick && "cursor-pointer hover:outline-f1-border-hover",
+        selected && "bg-f1-background-selected text-f1-foreground-selected",
+        onClick && focusRing()
+      )}
+      onClick={onClick}
+    >
+      <span>{label}</span>
+      {number && (
+        <Counter value={number} type={selected ? "selected" : "default"} />
+      )}
+    </button>
+  )
+}
