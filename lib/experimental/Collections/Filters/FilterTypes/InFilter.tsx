3d7ec834befa511d08be133d3c076a723f68e00b
Dani Moreno <hi@dazlog.com>
2025-02-28 08:14:44 +0100

    hide labels

--- lib/experimental/Collections/Filters/FilterTypes/InFilter.tsx
+++ lib/experimental/Collections/Filters/FilterTypes/InFilter.tsx
@@ -65,7 +65,7 @@ export function InFilter<T>({ filter, value, onChange }: InFilterProps<T>) {
             }}
           >
             <span className="line-clamp-1 w-fit text-left">{option.label}</span>
-            <Checkbox checked={isSelected} presentational />
+            <Checkbox checked={isSelected} presentational hideLabel />
           </button>
         )
       })}
