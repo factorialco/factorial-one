3703368f1943ef3dcba068ac691d5c3d251d6569
Dani Moreno <hi@dazlog.com>
2025-03-03 19:54:04 +0100

    options

--- lib/experimental/AI/Popover/index.tsx
+++ lib/experimental/AI/Popover/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/Actions/Button"
 import { Cross } from "@/icons/app"
 import { cn } from "@/lib/utils"
+import { useState } from "react"
 import { AIIcon } from "../AIIcon"
 import Noise from "./texture.png"
 
@@ -8,11 +9,77 @@ interface AIBoxProps {
   hiddenBorder?: boolean
 }
 
-export const AIBox = ({ hiddenBorder = true }: AIBoxProps) => {
+const AISelection = ({
+  children,
+  selected = false,
+  onClick,
+}: {
+  children: React.ReactNode
+  selected?: boolean
+  onClick?: () => void
+}) => {
   return (
     <div
       className={cn(
-        "relative h-[320px] w-[400px] rounded-xl border border-solid border-f1-border-secondary bg-[#FDEDD5]/50 shadow-md ring-1 ring-inset ring-f1-border-secondary",
+        "group relative rounded px-2 py-2.5 hover:cursor-pointer",
+        selected && "bg-gradient-to-r from-[#FACBBA] to-[#C7BBFA]"
+      )}
+      onClick={onClick}
+    >
+      <div
+        className={cn(
+          "pointer-events-none absolute inset-[1px] z-0 rounded bg-f1-background opacity-0",
+          selected && "opacity-95"
+        )}
+      />
+      <div className="relative z-10">{children}</div>
+      <div
+        className={cn(
+          "absolute right-2.5 top-2.5 z-10 h-5 w-5 rounded-full border border-solid border-f1-border bg-f1-background transition-all after:absolute after:left-1.5 after:top-1.5 after:h-1.5 after:w-1.5 after:rounded-full after:bg-f1-background after:content-['']",
+          selected && "bg-f1-background-selected-bold"
+        )}
+      />
+    </div>
+  )
+}
+
+export const AIBox = ({ hiddenBorder = true }: AIBoxProps) => {
+  const [selectedIndex, setSelectedIndex] = useState<number>(0)
+
+  const selections = [
+    {
+      content: (
+        <span>
+          All time off requests by{" "}
+          <strong className="font-medium">Dani Moreno</strong>
+        </span>
+      ),
+    },
+    {
+      content: (
+        <span>
+          All time off requests by{" "}
+          <strong className="font-medium">Design</strong>
+        </span>
+      ),
+    },
+    {
+      content: (
+        <span>
+          All time off requests under{" "}
+          <strong className="font-medium">5 days</strong>
+        </span>
+      ),
+    },
+    {
+      content: <span>All time off requests</span>,
+    },
+  ]
+
+  return (
+    <div
+      className={cn(
+        "relative h-fit w-[400px] rounded-xl border border-solid border-f1-border-secondary bg-[#FDEDD5]/50 shadow-md ring-1 ring-inset ring-f1-border-secondary",
         hiddenBorder && "overflow-hidden"
       )}
     >
@@ -74,11 +141,22 @@ export const AIBox = ({ hiddenBorder = true }: AIBoxProps) => {
             round
           />
         </div>
-        <div className="h-full flex-grow px-2 pb-2">
+        <div className="h-full flex-grow px-1 pb-1">
           <div className="flex h-full flex-col justify-between rounded-md bg-f1-background/70 backdrop-blur-sm">
             <div className="w-full px-4 pb-3 pt-4 text-f1-foreground-secondary">
               Based on the context of this request task, you can automate these
               actions going forward
+            </div>
+            <div className="flex flex-col gap-1.5 px-3 pb-4">
+              {selections.map((selection, index) => (
+                <AISelection
+                  key={index}
+                  selected={selectedIndex === index}
+                  onClick={() => setSelectedIndex(index)}
+                >
+                  {selection.content}
+                </AISelection>
+              ))}
             </div>
             <div className="flex justify-between gap-2 border border-solid border-transparent border-t-f1-border-secondary px-4 py-3">
               <Button variant="neutral" label="Edit rules" />
