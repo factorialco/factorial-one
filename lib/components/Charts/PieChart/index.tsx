6b3d0aa9b353b1c8c622e786cd5862c67a1374ae
Dani Moreno <hi@dazlog.com>
2025-03-05 11:25:37 +0100

    prettier

--- lib/components/Charts/PieChart/index.tsx
+++ lib/components/Charts/PieChart/index.tsx
@@ -98,7 +98,7 @@ export const _PieChart = (
                     <tspan
                       x={viewBox.cx}
                       y={(viewBox.cy || 0) + 8}
-                      className="text-4xl fill-f1-foreground font-semibold"
+                      className="fill-f1-foreground text-4xl font-semibold"
                     >
                       {overview?.number
                         ? tickFormatter
