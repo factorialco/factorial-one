b42c70fd967944b204765e189ff54dc3e1c47ebc
Eliseo Juan Quintanilla <eliseo.juan@factorial.co>
2025-03-18 13:42:34 +0100

    feat: Add DropdownButton as PrimaryAction in Resource Header (#1349)
    * feat: DropdownButton component
    
    * refactor: reexport internal components without private properties
    
    * refactor: ui button allow sub button
    
    * feat: Enhance BaseHeader with PrimaryDropdownAction support
    
    ---------
    
    Co-authored-by: Sergio Carracedo <sergio.carracedo@factorial.co>

--- lib/experimental/Information/utils.tsx
+++ lib/experimental/Information/utils.tsx
@@ -1,14 +1,23 @@
+import { OneDropdownButtonItem } from "@/components/Actions/OneDropdownButton"
 import { IconType } from "@/components/Utilities/Icon"
 
 export interface PrimaryAction {
-  label: string
-  icon?: IconType
-  onClick: () => void
   disabled?: boolean
   tooltip?: string
   isVisible?: boolean
 }
+export interface PrimaryActionButton extends PrimaryAction {
+  label: string
+  icon?: IconType
+  onClick: () => void
+}
 
-export interface SecondaryAction extends PrimaryAction {
+export interface PrimaryDropdownAction<T> extends PrimaryAction {
+  items: OneDropdownButtonItem<T>[]
+  value?: T
+  onClick: (value: T, item: OneDropdownButtonItem<T>) => void
+}
+
+export interface SecondaryAction extends PrimaryActionButton {
   variant?: "outline" | "critical"
 }
