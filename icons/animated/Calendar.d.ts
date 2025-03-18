eb141450621eaf10dccc2442b8d81f34eae2f1ca
sergiocarracedo <1263666+sergiocarracedo@users.noreply.github.com>
2025-03-18 08:49:01 +0000

    Commit Build

---
+++ icons/animated/Calendar.d.ts
@@ -0,0 +1,7 @@
+import * as React from "react";
+interface CalendarProps extends React.SVGProps<SVGSVGElement> {
+    animate?: "normal" | "animate";
+}
+declare const CalendarAnimated: React.ForwardRefExoticComponent<Omit<CalendarProps, "ref"> & React.RefAttributes<SVGSVGElement>>;
+export default CalendarAnimated;
+//# sourceMappingURL=Calendar.d.ts.map
