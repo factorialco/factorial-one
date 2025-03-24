7731835ab2ee857d5da3292cbbc27b7dfb06c209
Segio Carracedo <sergio.carracedo@factorial.co>
2025-03-24 14:35:19 +0100

    feat: collection actions

--- packages/react/src/experimental/OneDataCollection/item-actions.tsx
+++ packages/react/src/experimental/OneDataCollection/item-actions.tsx
@@ -5,6 +5,12 @@ export type ItemActionsDefinition<T extends RecordType> = (
   item: T
 ) => Array<DropdownItem & { enabled?: boolean }> | undefined
 
+/**
+ * Filters the actions based on the enabled property
+ * @param actions - The actions to filter
+ * @param item - The item to filter the actions for
+ * @returns An array of filtered actions
+ */
 export const filterItemActions = <T extends RecordType>(
   actions: ItemActionsDefinition<T>,
   item: T
