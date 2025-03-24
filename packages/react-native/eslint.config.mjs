3ed953516c3a8e6352f8cc6d23771c549d096dc9
Segio Carracedo <sergio.carracedo@factorial.co>
2025-03-24 10:18:42 +0100

    chore: use composite actions

--- packages/react-native/eslint.config.mjs
+++ packages/react-native/eslint.config.mjs
@@ -1,25 +1,24 @@
-import { fixupConfigRules } from "@eslint/compat"
-import { FlatCompat } from "@eslint/eslintrc"
-import js from "@eslint/js"
-import tsParser from "@typescript-eslint/parser"
-import globals from "globals"
-import path from "node:path"
-import { fileURLToPath } from "node:url"
+import { fixupConfigRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
-})
-
+});
 // Common settings to apply to all configs using React
 const reactSettings = {
   react: {
     version: "detect",
   },
-}
+};
 
 export default [
   // Ignore dist and other config files
@@ -78,4 +77,4 @@ export default [
       ],
     },
   },
-]
+];
