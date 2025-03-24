a186c9f772045d95be1165856c65d51cd297ac64
Segio Carracedo <sergio.carracedo@factorial.co>
2025-03-24 14:35:19 +0100

    feat: collection actions

--- packages/react/src/experimental/OneDataCollection/item-actions.tsx
+++ packages/react/src/experimental/OneDataCollection/item-actions.tsx
@@ -1,11 +1,6 @@
 import { DropdownItem } from "../exports"
 import { RecordType } from "./types"
 
-/**
- * Filters the actions based on the enabled property
- * @param actions - The actions to filter
- * @returns An array of filtered actions
- */
 export type ItemActionsDefinition<T extends RecordType> = (
   item: T
 ) => Array<DropdownItem & { enabled?: boolean }> | undefined
