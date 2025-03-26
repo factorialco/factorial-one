53f7f9d3dc79b7e077877eb4de58af7fb3ace493
Sergio Carracedo Martinez <sergio.carracedo@factorial.co>
2025-03-26 10:57:05 +0100

    feat: publish packages (#1473)

---
+++ packages/react/src/experimental/Information/Tags/TagList/TagList.tsx
@@ -0,0 +1,12 @@
+import { Tag } from "../Tag"
+
+export type TagListProps = {
+  tags: 
+}
+
+export const TagList = (props: TagListProps) => {
+  return (
+    <div>
+      <Tag>Tag</Tag>
+    </div>
+  )
