5b3fa3ef612dabc000fa78effa4e7c0f8beada88
Saúl Domínguez Gómez <saulydominguez@gmail.com>
2025-04-01 11:10:27 +0200

    fix prettier errors (#1523)

--- packages/react/src/icons/app/Menu.tsx
+++ packages/react/src/icons/app/Menu.tsx
@@ -1,5 +1,35 @@
-import type { SVGProps } from "react";
-import { Ref, forwardRef } from "react";
-const SvgMenu = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" ref={ref} {...props}><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M4.99989 7H18.9999" vectorEffect="non-scaling-stroke" /><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M5 12H19" vectorEffect="non-scaling-stroke" /><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M4.99989 17H18.9999" vectorEffect="non-scaling-stroke" /></svg>;
-const ForwardRef = forwardRef(SvgMenu);
-export default ForwardRef;
+import type { SVGProps } from "react"
+import { Ref, forwardRef } from "react"
+const SvgMenu = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    ref={ref}
+    {...props}
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4.99989 7H18.9999"
+      vectorEffect="non-scaling-stroke"
+    />
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M5 12H19"
+      vectorEffect="non-scaling-stroke"
+    />
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4.99989 17H18.9999"
+      vectorEffect="non-scaling-stroke"
+    />
+  </svg>
+)
+const ForwardRef = forwardRef(SvgMenu)
+export default ForwardRef
