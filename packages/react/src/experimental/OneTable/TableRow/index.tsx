b8a10adaacf9fa912015ee80bb9db9e440530a84
Dani Moreno <hi@dazlog.com>
2025-04-01 11:33:29 +0200

    fix sticky styles

--- packages/react/src/experimental/OneTable/TableRow/index.tsx
+++ packages/react/src/experimental/OneTable/TableRow/index.tsx
@@ -14,7 +14,7 @@ export function TableRow({ children, selected, className }: TableRowProps) {
       className={cn(
         selected && "bg-f1-background-selected hover:bg-f1-background-selected",
         className,
-        "relative [&:has(a:focus)]:after:rounded-sm [&:has(a:focus)]:after:ring-1 [&:has(a:focus)]:after:ring-inset [&:has(a:focus)]:after:ring-f1-ring"
+        "relative before:pointer-events-none before:absolute before:inset-0 before:z-10 before:content-[''] [&:has(a:focus)]:before:rounded-sm [&:has(a:focus)]:before:ring-1 [&:has(a:focus)]:before:ring-inset [&:has(a:focus)]:before:ring-f1-ring"
       )}
     >
       {children}
