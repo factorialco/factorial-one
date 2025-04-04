00548815995e1fa2899874b09a5b4bd86286cddb
Raúl Sigüenza Sánchez <raul.siguenza@factorial.co>
2025-04-04 10:38:17 +0200

    fix: Text editor buttons and lists errors (#1548)
    * fix: update RichTextEditor interactions and styles
    
    * fix: better list handling
    
    * fix: simplify event handling in Enhance component

--- packages/react/src/experimental/RichText/RichTextEditor/utils/extensions.tsx
+++ packages/react/src/experimental/RichText/RichTextEditor/utils/extensions.tsx
@@ -136,7 +136,19 @@ const ExtensionsConfiguration = ({
   maxCharacters,
 }: ExtensionsConfigurationProps) => {
   return [
-    StarterKit.configure({ heading: { levels: [1, 2, 3] } }),
+    StarterKit.configure({
+      heading: { levels: [1, 2, 3] },
+      bulletList: {
+        HTMLAttributes: {
+          class: "f1-bullet-list",
+        },
+      },
+      orderedList: {
+        HTMLAttributes: {
+          class: "f1-ordered-list",
+        },
+      },
+    }),
     Underline,
     TextStyle,
     Color,
