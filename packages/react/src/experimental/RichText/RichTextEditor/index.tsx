fe985bc28046dbc828b90e23af5342fdf1f94775
siguenzaraul <raul.siguenza@factorial.co>
2025-04-06 20:55:30 +0200

    fix: make height prop optional in RichTextEditor

--- packages/react/src/experimental/RichText/RichTextEditor/index.tsx
+++ packages/react/src/experimental/RichText/RichTextEditor/index.tsx
@@ -56,7 +56,7 @@ interface RichTextEditorProps {
   toolbarLabels: toolbarLabels
   title: string
   errorConfig?: errorConfig
-  height: heightType
+  height?: heightType
 }
 
 type RichTextEditorHandle = {
