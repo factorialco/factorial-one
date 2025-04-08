8752bbf5d18126f49e6180109182652acba20ec3
siguenzaraul <raul.siguenza@factorial.co>
2025-04-08 09:10:54 +0200

    refactor: update FileAvatar and FileItem

--- packages/react/src/experimental/RichText/FileAvatar/index.tsx
+++ packages/react/src/experimental/RichText/FileAvatar/index.tsx
@@ -1,37 +1,29 @@
 import { cn } from "@/lib/utils"
 import { Avatar, AvatarFallback } from "@/ui/avatar"
-import * as React from "react"
+import { ElementRef, forwardRef } from "react"
 import { getFileTypeInfo } from "./utils"
 
 type FileAvatarProps = React.ComponentPropsWithoutRef<typeof Avatar> & {
   file: File
 }
 
-/**
- * FileAvatar component displays a visual representation of a file type
- * with appropriate coloring based on the file extension or MIME type.
- */
-const FileAvatar = React.forwardRef<
-  React.ElementRef<typeof Avatar>,
-  FileAvatarProps
->(({ file, className, ...props }, ref) => {
-  const { type: fileType, color: fileColor } = getFileTypeInfo(file)
+const FileAvatar = forwardRef<ElementRef<typeof Avatar>, FileAvatarProps>(
+  ({ file, className, ...props }, ref) => {
+    const { type: fileType, color: fileColor } = getFileTypeInfo(file)
 
-  return (
-    <Avatar
-      ref={ref}
-      className={cn(
-        "border border-solid border-f1-border-secondary bg-f1-background",
-        className
-      )}
-      {...props}
-    >
-      <AvatarFallback className={cn("text-xs font-semibold", fileColor)}>
-        {fileType}
-      </AvatarFallback>
-    </Avatar>
-  )
-})
+    return (
+      <Avatar
+        ref={ref}
+        className={cn("bg-f1-background", className)}
+        {...props}
+      >
+        <AvatarFallback className={cn("text-xs font-semibold", fileColor)}>
+          {fileType}
+        </AvatarFallback>
+      </Avatar>
+    )
+  }
+)
 FileAvatar.displayName = "FileAvatar"
 
 export { FileAvatar }
--- packages/react/src/experimental/RichText/exports.tsx
+++ packages/react/src/experimental/RichText/exports.tsx
@@ -1,3 +1,4 @@
+export * from "./FileAvatar"
 export * from "./FileItem"
 export * from "./RichTextDisplay"
 export * from "./RichTextEditor"
