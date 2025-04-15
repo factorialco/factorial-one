97a14497597c5578a8e6f8ebefb84a88ba445ce6
Nicotelo <jose.real@factorial.co>
2025-04-15 16:31:11 +0200

    feat: add banner component to F1

---
+++ packages/react/src/experimental/Banner/index.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { fn } from "@storybook/test"
+import { Banner } from "./index"
+
+const meta: Meta<typeof Banner> = {
+  component: Banner,
+  title: "Home/ProjectManagement/Banner",
+  tags: ["autodocs", "experimental"],
+  parameters: {
+    layout: "centered",
+  },
+  decorators: [
+    (Story) => (
+      <div className="p-2">
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export default meta
+
+type Story = StoryObj<typeof Banner>
+
+export const Default: Story = {
+  args: {
+    title: "Your workspace includes up to 3 invoices.",
+    description: "Start creating invoices: it’s free with your plan!",
+    buttonType: 'outline', 
+    onRequestClick: fn(),
+    onSeeClick: fn(),
+  },
+}
