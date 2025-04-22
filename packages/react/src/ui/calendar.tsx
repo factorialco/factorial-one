2c104eedf6238fac5903f1c8f14e27042677e9b3
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2025-04-22 17:26:25 +0200

    feat: Add `OneCalendar` component (#1634)
    * feat: basic structure, month view
    
    * feat: month view
    
    * feat: date navigation
    
    * feat: year view
    
    * feat: day view
    
    * fix: return dates
    
    * feat: l10n for day picker
    
    * fix: remove old calendar
    
    * feat: l10n test
    
    * fix: color contrast fix
    
    * feat: week view
    
    * fix: disabled a11y
    
    * fix: today tab color
    
    * fix: export
    
    * fix: type safety

--- packages/react/src/ui/calendar.tsx
+++ packages/react/src/ui/calendar.tsx
@@ -3,8 +3,6 @@ import * as React from "react"
 import { DayPicker } from "react-day-picker"
 import { cn } from "../lib/utils"
 
-import { buttonVariants } from "./button"
-
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
 function Calendar({
@@ -16,38 +14,41 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn("p-3", className)}
+      className={className}
       classNames={{
-        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-        month: "space-y-4",
-        caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        months: "flex flex-col",
+        caption: "hidden",
         nav: "space-x-1 flex items-center",
-        nav_button: cn(
-          buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
-        ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
-        table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+        table: "w-full border-collapse",
+        head_row: "flex items-center justify-between",
         head_cell:
-          "text-f1-foreground-secondary rounded-xs w-9 font-normal text-[0.8rem]",
-        row: "flex w-full mt-2",
-        cell: "rounded-full h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-outside)]:bg-f1-background-selected focus-within:relative focus-within:z-20 [&:has([aria-selected].day-range-middle)]:rounded-none first:[&:has([aria-selected].day-range-middle)]:rounded-l-lg last:[&:has([aria-selected].day-range-middle)]:rounded-r-lg [&:has([aria-selected].day-range-start)]:rounded-r-none [&:has([aria-selected].day-range-end)]:rounded-l-none first:[&:has([aria-selected].day-range-end)]:rounded-r-[24px] first:[&:has([aria-selected].day-range-end)]:rounded-l-md last:[&:has([aria-selected].day-range-start)]:rounded-l-[24px] last:[&:has([aria-selected].day-range-start)]:rounded-r-md [&:has([aria-selected].day-range-start.day-range-end)]:rounded-full",
-        day: cn(
-          buttonVariants({ variant: "ghost" }),
-          "rounded-[inherit] h-9 w-9 p-0 font-normal aria-selected:opacity-100"
+          "text-f1-foreground-secondary rounded-xs w-full font-medium h-8 flex justify-center items-center",
+        row: "flex w-full mt-2 items-center justify-between",
+        cell: cn(
+          "rounded-md h-10 w-full text-center font-medium p-0 relative text-f1-foreground",
+          "before:absolute before:inset-0 before:z-0 before:rounded-md before:bg-f1-background-selected-bold before:opacity-0 before:transition-all before:duration-100 before:content-[''] hover:before:bg-f1-background-selected-bold-hover before:pointer-events-none",
+          "[&:has([aria-selected].day-range-start)]:before:opacity-100 [&:has([aria-selected].day-range-end)]:before:opacity-100",
+          "[&:has([aria-selected].day-outside)]:bg-f1-background-selected focus-within:relative focus-within:z-20 [&:has([aria-selected].day-range-middle)]:rounded-none first:[&:has([aria-selected].day-range-middle)]:rounded-l-md last:[&:has([aria-selected].day-range-middle)]:rounded-r-md [&:has([aria-selected].day-range-start)]:rounded-r-none [&:has([aria-selected].day-range-end)]:rounded-l-none first:[&:has([aria-selected].day-range-end)]:rounded-r-md first:[&:has([aria-selected].day-range-end)]:rounded-l-md last:[&:has([aria-selected].day-range-start)]:rounded-l-md last:[&:has([aria-selected].day-range-start)]:rounded-r-md [&:has([aria-selected].day-range-start.day-range-end)]:rounded-md [&:has([aria-selected].day-range-middle)]:bg-f1-background-selected",
+          "[&:has([aria-selected].day-range-start)]:bg-f1-background-selected [&:has([aria-selected].day-range-end)]:bg-f1-background-selected",
+          props.mode === "single" &&
+            "[&:has([aria-selected].day-selected)]:before:opacity-100"
         ),
-        day_range_start: "day-range-start",
-        day_range_end: "day-range-end",
-        day_selected:
-          "bg-f1-background-selected-bold text-f1-foreground-inverse",
-        day_today: "bg-f1-background-secondary text-f1-foreground",
-        day_outside:
-          "day-outside text-f1-foreground-secondary aria-selected:bg-f1-background-selected aria-selected:text-f1-foreground",
-        day_disabled: "text-f1-foreground-secondary opacity-50",
-        day_range_middle: "day-range-middle",
+        day: "rounded-[inherit] h-10 w-10 p-0 aria-selected:opacity-100 z-20 relative",
+        day_range_start:
+          "day-range-start aria-selected:text-f1-foreground-inverse",
+        day_range_end: "day-range-end aria-selected:text-f1-foreground-inverse",
+        day_today:
+          "relative after:absolute after:inset-x-0 after:bottom-1 after:z-20 after:mx-auto after:h-0.5 after:w-1.5 after:rounded-full after:bg-f1-background-selected-bold after:transition-colors after:duration-100 after:content-[''] after:pointer-events-none aria-selected:after:bg-f1-background",
+        day_selected: cn(
+          "day-selected",
+          props.mode === "single" && "aria-selected:text-f1-foreground-inverse"
+        ),
+        day_outside: "day-outside text-f1-foreground-secondary font-normal",
+        day_disabled: "text-f1-foreground-disabled",
+        day_range_middle:
+          "day-range-middle aria-selected:text-f1-foreground-selected",
         day_hidden: "invisible",
         ...classNames,
       }}
