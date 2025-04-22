dad1eebb8e51da1c5c5fe3938231e52c3db2b58e
Segio Carracedo <sergio.carracedo@factorial.co>
2025-04-22 17:47:30 +0200

    chore: simplify types

--- packages/react/src/experimental/OneDataCollection/__stories__/index.stories.tsx
+++ packages/react/src/experimental/OneDataCollection/__stories__/index.stories.tsx
@@ -1222,6 +1222,7 @@ export const WithSyncSearch: Story = {
       },
     ]
 
+    // TODO allow to infer the type of the data source
     const source = useDataSource<
       (typeof mockUserData)[number],
       typeof filters,
