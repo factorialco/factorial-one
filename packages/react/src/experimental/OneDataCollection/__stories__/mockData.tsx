a3f94fbfb03fb3cc9ac9732af62302f2ae6bff9f
Segio Carracedo <sergio.carracedo@factorial.co>
2025-04-22 16:20:23 +0200

    chore: fix types

--- packages/react/src/experimental/OneDataCollection/__stories__/mockData.tsx
+++ packages/react/src/experimental/OneDataCollection/__stories__/mockData.tsx
@@ -421,7 +421,7 @@ export const ExampleComponent = ({
     sortings,
     itemActions: (item) => [
     grouping,
-    currentGrouping: currentGrouping ?? null,
+    currentGrouping: currentGrouping,
     itemActions: (item: MockUser) => [
       {
         label: "Edit",
