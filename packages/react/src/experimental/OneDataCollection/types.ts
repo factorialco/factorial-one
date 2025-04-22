5ff1c131d077602180a29604cc17be3f8d077c04
Segio Carracedo <sergio.carracedo@factorial.co>
2025-04-22 16:20:23 +0200

    chore: fix types

--- packages/react/src/experimental/OneDataCollection/types.ts
+++ packages/react/src/experimental/OneDataCollection/types.ts
@@ -83,9 +83,8 @@ export type DataSourceDefinition<
   totalItemSummary?: (totalItems: number) => string
   /** Grouping configuration */
   grouping?: Grouping
-} & (Grouping extends { mandatory: true }
-  ? { currentGrouping: GroupingState<Record, Grouping> }
-  : { currentGrouping?: GroupingState<Record, Grouping> | null })
+  currentGrouping?: GroupingState<Record, Grouping>
+}
 
 export type CollectionSearchOptions = {
   /** Whether search is enabled */
@@ -412,15 +411,9 @@ export type DataSource<
     : null
   /** Function to update the current grouping state */
   setCurrentGrouping: React.Dispatch<
-    React.SetStateAction<
-      Grouping extends { mandatory: true }
-        ? GroupingState<Record, Grouping>
-        : GroupingState<Record, Grouping> | undefined
-    >
+    React.SetStateAction<GroupingState<Record, Grouping>
   >
-} & (Grouping extends { mandatory: true }
-    ? { currentGrouping: GroupingState<Record, Grouping> }
-    : { currentGrouping?: GroupingState<Record, Grouping> })
+}
 
 /**
  * Utility type for handling both Promise and Observable return types.
