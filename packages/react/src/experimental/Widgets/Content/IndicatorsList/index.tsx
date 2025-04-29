826fc5538f753c3c1e4bf5418d171b841b2822da
Saúl Domínguez Gómez <saulydominguez@gmail.com>
2025-04-29 17:39:23 +0200

    Add support to show emoji in widget highlight items (#1684)
    * refactor: add support to show emoji in widget highlight items
    
    * refactor: code suggestions

--- packages/react/src/experimental/Widgets/Content/IndicatorsList/index.tsx
+++ packages/react/src/experimental/Widgets/Content/IndicatorsList/index.tsx
@@ -12,13 +12,13 @@ export const IndicatorsList = forwardRef<HTMLDivElement, IndicatorsListProps>(
         ref={ref}
         className="grid auto-cols-fr grid-flow-col items-end gap-x-3"
       >
-        {items.map(({ label, content, icon, color }) => (
+        {items.map(({ label, content, color, ...item }) => (
           <Indicator
             key={`${label}-${content}`}
             label={label}
             content={content}
-            icon={icon}
             color={color}
+            {...item}
           />
         ))}
       </div>
