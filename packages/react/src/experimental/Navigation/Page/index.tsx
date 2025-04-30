4e15d31ebb966e36e50e1f2b791ae4e7b8908684
Segio Carracedo <sergio.carracedo@factorial.co>
2025-04-30 16:02:44 +0200

    feat: copilotkit integration in page

--- packages/react/src/experimental/Navigation/Page/index.tsx
+++ packages/react/src/experimental/Navigation/Page/index.tsx
@@ -1,3 +1,7 @@
+import { CopilotKit } from "@copilotkit/react-core"
+import { CopilotSidebar } from "@copilotkit/react-ui"
+import { CopilotButton } from "./components/CopilotButton"
+
 interface PageProps {
   children?: React.ReactNode
   header?: React.ReactNode
@@ -6,15 +10,31 @@ interface PageProps {
 
 export function Page({ children, header, embedded = false }: PageProps) {
   return (
-    <div
-      className={`flex w-full flex-col overflow-hidden ${
-        embedded ? "" : "xs:rounded-xl"
-      } bg-f1-page ring-1 ring-inset ring-f1-border-secondary`}
-    >
-      {header && <div className="flex flex-col">{header}</div>}
-      <div className="isolate flex w-full flex-1 flex-col overflow-auto [&>*]:flex-1">
-        {children}
-      </div>
+    <div className="w-full">
+      <CopilotKit runtimeUrl="/api/copilotkit">
+        <CopilotSidebar
+          Button={CopilotButton}
+          instructions={
+            "You are assisting the user as best as you can. Answer in the best way possible given the data you have."
+          }
+          labels={{
+            title: "Factorial AI",
+            initial: "How can I help you today?",
+          }}
+        >
+          <div
+            className={`flex w-full flex-col overflow-hidden ${
+              embedded ? "" : "xs:rounded-xl"
+            } bg-f1-page ring-1 ring-inset ring-f1-border-secondary`}
+          >
+            {header && <div className="flex flex-col">{header}</div>}
+
+            <div className="isolate flex w-full flex-1 flex-col overflow-auto [&>*]:flex-1">
+              {children}
+            </div>
+          </div>
+        </CopilotSidebar>
+      </CopilotKit>
     </div>
   )
 }
