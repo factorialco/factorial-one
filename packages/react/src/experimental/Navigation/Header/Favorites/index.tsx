9b95e4d311f2d04f5cf8e3f2cb440b45637516d2
Niki Lopin <nik.lopin@factorial.co>
2025-05-08 10:41:48 +0200

    feat: add favorites button

--- packages/react/src/experimental/Navigation/Header/Favorites/index.tsx
+++ packages/react/src/experimental/Navigation/Header/Favorites/index.tsx
@@ -1,7 +1,9 @@
-import { ButtonInternal } from "@/components/Actions/Button/internal"
+import { Icon } from "@/components/Utilities/Icon"
 import Star from "@/icons/app/Star"
 import StarFilled from "@/icons/app/StarFilled"
-import { cn } from "@/lib/utils"
+import { AnimatePresence, motion } from "framer-motion"
+
+const IconMotion = motion.create(Icon)
 
 export const Favorites = ({
   isMarked,
@@ -11,16 +13,35 @@ export const Favorites = ({
   onChange: (newValue: boolean) => void
 }) => {
   return (
-    <ButtonInternal
-      variant="ghost"
-      label="Favorites"
-      hideLabel
-      className={cn(
-        "responsive -left-2",
-        isMarked ? "[&>*>svg]:text-[hsl(var(--promote-50))]" : undefined
-      )}
-      icon={isMarked ? StarFilled : Star}
-      onClick={() => onChange(!isMarked)}
-    />
+    <AnimatePresence mode="wait">
+      <button onClick={() => onChange(!isMarked)}>
+        {isMarked ? (
+          <IconMotion
+            size="sm"
+            key="favorite"
+            icon={StarFilled}
+            className="text-[hsl(var(--promote-50))]"
+            initial={{ scale: 0.5 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0.5 }}
+            transition={{
+              ease: [0.175, 0.885, 0.27, 2],
+            }}
+          />
+        ) : (
+          <IconMotion
+            size="sm"
+            key="not-favorite"
+            icon={Star}
+            initial={{ scale: 0.8 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0.8 }}
+            transition={{
+              ease: [0, 0, 0.58, 1],
+            }}
+          />
+        )}
+      </button>
+    </AnimatePresence>
   )
 }
