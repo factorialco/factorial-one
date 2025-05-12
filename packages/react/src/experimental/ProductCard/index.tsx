f658f166888683b56f6da1509c1321756551b587
Chloé Dumit <70954526+ChloeDumit@users.noreply.github.com>
2025-05-12 13:22:04 -0300

    feat(PageHeader): add cross-selling feature to ProductUpdates component (#1736)
    * feat(PageHeader): add cross-selling feature to ProductUpdates component
    
    * feat(PageHeader): fix button properties
    
    * feat(PageHeader): fix string type
    
    * feat(PageHeader): fix max height
    
    * feat(PageHeader): creating new product card component and its variants
    
    * feat(PageHeader): add label to closing button
    
    * feat(ProductUpdates): enhance cross-selling feature with multiple products
    
    * feat(ProductUpdates): adding key to iterator
    
    * feat(ProductUpdates): track visibility
    
    * feat(ProductUpdates): crosselling optional
    
    * feat(ProductUpdates): product card height
    
    * Small fixes and "More updates" label removing
    
    * fix
    
    * feat(ProductHeader): close dropdown when product clicked
    
    * feat(ProductHeader): dropdown auto height
    
    ---------
    
    Co-authored-by: David García <david.garcia@factorial.co>

---
+++ packages/react/src/experimental/ProductCard/index.tsx
@@ -0,0 +1,76 @@
+import { Button, IconType } from "@/factorial-one"
+import CrossIcon from "@/icons/app/Cross"
+import { useEffect, useState } from "react"
+import { ModuleAvatar } from "../Information/ModuleAvatar"
+
+export type ProductCardProps = {
+  title: string
+  description: string
+  onClick: () => void
+  onClose?: () => void
+  isVisible: boolean
+  icon: IconType
+  dismissable?: boolean
+  trackVisibility?: (open: boolean) => void
+}
+
+const ProductCard = ({
+  title,
+  description,
+  onClick,
+  onClose,
+  isVisible,
+  icon,
+  dismissable = false,
+  trackVisibility,
+}: ProductCardProps) => {
+  const [open, setOpen] = useState(isVisible)
+
+  useEffect(() => {
+    setOpen(isVisible)
+    if (trackVisibility) {
+      trackVisibility(isVisible)
+    }
+  }, [isVisible, trackVisibility])
+
+  const handleClose = () => {
+    setOpen(false)
+    if (onClose) {
+      onClose()
+    }
+  }
+
+  return (
+    open && (
+      <div>
+        <div className="p-2">
+          <div
+            className="flex h-[90px] cursor-pointer flex-row gap-2 rounded-md border-f1-border p-3 text-f1-foreground shadow-md hover:bg-f1-background-secondary"
+            onClick={onClick}
+          >
+            <ModuleAvatar icon={icon} size="lg" />
+            <div className="flex flex-1 flex-col">
+              <div>
+                <h3 className="text-lg font-medium">{title}</h3>
+                <p className="text-f1-foreground-secondary">{description}</p>
+              </div>
+            </div>
+            {dismissable && (
+              <div className="h-6 w-6">
+                <Button
+                  variant="ghost"
+                  icon={CrossIcon}
+                  size="sm"
+                  hideLabel
+                  onClick={handleClose}
+                  label="Close"
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  )
+}
+export default ProductCard
