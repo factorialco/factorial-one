6a3c4721847e41970ef0dfddb302f133a19c6cc9
Segio Carracedo <sergio.carracedo@factorial.co>
2025-05-13 10:26:01 +0200

    feat: select asList

--- packages/react/src/ui/Select/select.stories.tsx
+++ packages/react/src/ui/Select/select.stories.tsx
@@ -12,10 +12,12 @@ import {
 const SelectWithHooks = ({
   options,
   placeholder,
+  asList,
   ...props
 }: {
   options: { value: string; label: string }[]
   placeholder?: string
+  asList?: boolean
 }) => {
   const [value, setValue] = useState("")
 
@@ -30,7 +32,7 @@ const SelectWithHooks = ({
   )
 
   return (
-    <Select value={value} onValueChange={setValue} {...props}>
+    <Select value={value} onValueChange={setValue} {...props} asList={asList}>
       <SelectTrigger>
         {value}
         <SelectValue placeholder={placeholder} />
@@ -79,6 +81,12 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
+
+export const AsList: Story = {
+  args: {
+    asList: true,
+  },
+}
 
 export const WithTopContent: Story = {
   render: ({ options, placeholder }) => {
