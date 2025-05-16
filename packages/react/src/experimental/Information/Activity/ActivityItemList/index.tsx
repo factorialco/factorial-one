d773a33c713bfe4976868064eb6359fa2d460cbd
Saúl Domínguez Gómez <saulydominguez@gmail.com>
2025-05-16 13:18:08 +0200

    fix: add support to know when user reaches end of ActivityItem list (#1822)
    * fix: add support to know when user reaches end of ActivityItem list
    
    * fix: throttle function

--- packages/react/src/experimental/Information/Activity/ActivityItemList/index.tsx
+++ packages/react/src/experimental/Information/Activity/ActivityItemList/index.tsx
@@ -1,25 +1,45 @@
 import { categorizeItemsByDate } from "@/lib/date"
 import { useI18n } from "@/lib/providers/i18n"
 import { withSkeleton } from "@/lib/skeleton"
+import throttle from "lodash/throttle"
 import React from "react"
 import { Section, SectionProps } from "./Section"
 
-export type ActivityItemListProps = Pick<SectionProps, "items" | "onClickItem">
+export type ActivityItemListProps = Pick<
+  SectionProps,
+  "items" | "onClickItem"
+> & {
+  onEndReached?: () => void
+  onEndReachedItemsThreshold?: number
+}
 
 const Separator = () => (
-  <div className="h-px w-full bg-f1-background-secondary" />
+  <div className="-mx-2 h-px bg-f1-background-secondary" />
 )
 
 export const BaseActivityItemList = ({
   items,
   onClickItem,
+  onEndReached,
+  onEndReachedItemsThreshold = 5,
 }: ActivityItemListProps) => {
   const translations = useI18n()
 
   const categorizedItems = categorizeItemsByDate(items, "createdAt")
 
+  const lastItemIds = Object.values(categorizedItems)
+    .slice()
+    .flatMap((items) => items.map((item) => item.id))
+    .slice(-onEndReachedItemsThreshold)
+
+  const handleItemVisible = throttle((id: string) => {
+    if (lastItemIds.includes(id)) {
+      onEndReached?.()
+    }
+  }, 1000)
+
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 px-2">
       {Object.entries(categorizedItems)
         .filter(([_, items]) => !!items.length)
         .map(([group, items]) => (
@@ -32,6 +52,7 @@ export const BaseActivityItemList = ({
               }
               items={items}
               onClickItem={onClickItem}
+              onItemVisible={handleItemVisible}
             />
             <Separator />
           </React.Fragment>
