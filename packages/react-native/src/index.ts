09f2a715e7ddd9e69742d833af428c57712f3125
Carlos Bertomeu <cbertomeu18@gmail.com>
2025-05-21 11:24:28 +0200

    feat: new f1 react native buttons

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -2,6 +2,7 @@
 export * from "./components/ExampleComponent";
 export * from "./components/Icon";
 export { ModuleAvatar } from "./components/Information/ModuleAvatar";
+export { Button } from "./components/Button";
 
 // Export icons
 export * from "./icons";
