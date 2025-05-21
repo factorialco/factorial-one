ff38d5ecd185738b9450521b4155f59852015e55
Carlos Bertomeu <cbertomeu18@gmail.com>
2025-05-21 15:09:53 +0200

    feat: add new activity list component and icon avatar (#1842)
    * feat: add new activity list component and icon avatar
    
    * feat: add tests
    
    * feat: update color and core package
    
    * test: using jest for test
    
    * fix: remove vitest from tsconfig
    
    * test: refactor test structure
    
    * test: adding snapshot
    
    * fix: update unnecessary date formats
    
    ---------
    
    Co-authored-by: Daniel Maza <daniel.maza@daniel.maza>

---
+++ packages/react-native/src/components/IconAvatar/index.tsx
@@ -0,0 +1,31 @@
+import { View } from "react-native";
+import { Icon, IconType } from "../Icon";
+import { cn } from "../../lib/utils";
+
+type Props = {
+  icon: IconType;
+  size?: "sm" | "md" | "lg";
+  className?: string;
+};
+
+const sizes = {
+  sm: "h-6 w-6 rounded-sm",
+  md: "h-9 w-9 rounded-md",
+  lg: "h-10 w-10 rounded-lg",
+};
+
+export const IconAvatar = ({ icon, size = "md", className }: Props) => {
+  return (
+    <View
+      className={cn(
+        "flex aspect-square items-center justify-center border border-solid border-f1-border-secondary",
+        sizes[size],
+        className,
+      )}
+    >
+      <Icon icon={icon} size={size} className="text-f1-foreground-secondary" />
+    </View>
+  );
+};
+
+IconAvatar.displayName = "IconAvatar";
