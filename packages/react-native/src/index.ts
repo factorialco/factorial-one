1531dc3270b0853897b5d51d0da28d7246b406ba
Carlos Bertomeu <cbertomeu18@gmail.com>
2025-05-22 16:27:17 +0200

    feat: new f1 react native buttons (#1854)
    * feat: new f1 react native buttons
    
    * fix: migrate to testing library
    
    * test: fix condition for the last test
    
    * feat: update pnpm-lock
    
    * fix: remove error from eslint
    
    ---------
    
    Co-authored-by: Daniel Maza <daniel.maza@daniel.maza>

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -4,6 +4,7 @@ export * from "./components/Icon";
 export * from "./components/Information/ModuleAvatar";
 export * from "./components/IconAvatar";
 export * from "./components/Activity/ActivityItem";
+export * from "./components/Button";
 
 // Export icons
 export * from "./icons";
