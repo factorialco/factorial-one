15c5b5d16844730392d050dd3787ca701c67069f
dani-moreno <daniel.moreno@factorial.co>
2025-05-26 15:38:57 +0200

    feat: docs

--- packages/react/src/experimental/OneEmptyState/OneEmptyState.tsx
+++ packages/react/src/experimental/OneEmptyState/OneEmptyState.tsx
@@ -3,16 +3,54 @@ import { IconType } from "@/components/Utilities/Icon"
 import { EmojiAvatar } from "@/experimental/Information/Avatars/EmojiAvatar"
 
 type ActionProps = {
+  /**
+   * The label of the action
+   */
   label: string
+
+  /**
+   * The click handler of the action
+   */
   onClick: () => void
+  
+  /**
+   * The variant of the action
+   * @default "default"
+   * @optional
+   */
   variant?: "default" | "outline"
+
+  /**
+   * The icon of the action
+   * @optional
+   */
   icon?: IconType
 }
 
 type EmptyStateProps = {
+  /**
+   * The title of the empty state
+   */
   title: string
+
+  /**
+   * If defined, a description will be displayed in the empty state
+   * @optional
+   */
   description?: string
+
+  /**
+   * If defined, an emoji will be displayed in the empty state
+   * @optional
+   */
   icon?: string
+
+  /**
+   * An array of action objects to display as buttons in the empty state.
+   * Each action represents a user-interactable option, such as "Retry" or "Go Back",
+   * and can include a label, click handler, optional icon, and button variant.
+   * @optional
+   */
   actions?: ActionProps[]
 }
 
