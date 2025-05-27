217d136e2fb693f72f21c506616128ad29ee6cf5
Daniel Maza <39518742+dmaza81@users.noreply.github.com>
2025-05-27 09:41:29 +0200

    feat: add emojiAvatar component (#1887)
    * feat: init stage
    
    * test: adding some test
    
    * feat: error handler
    
    * feat: allow diferent size
    
    * fix: add dependencies
    
    * fix: test
    
    * fix: update test
    
    * fix: remove styling form storibook
    
    * fix: size using nativewind
    
    ---------
    
    Co-authored-by: Daniel Maza <daniel.maza@daniel.maza>

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -1,10 +1,11 @@
 // Export components
-export * from "./components/ExampleComponent";
-export * from "./components/Icon";
-export * from "./components/Information/ModuleAvatar";
-export * from "./components/IconAvatar";
 export * from "./components/Activity/ActivityItem";
 export * from "./components/Button";
+export * from "./components/EmojiAvatar";
+export * from "./components/ExampleComponent";
+export * from "./components/Icon";
+export * from "./components/IconAvatar";
+export * from "./components/Information/ModuleAvatar";
 
 // Export icons
 export * from "./icons";
