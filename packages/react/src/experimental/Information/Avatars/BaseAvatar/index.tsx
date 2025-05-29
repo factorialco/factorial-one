d06f4a727a5ef27788d5325b8e497bbed1562ee0
Desirée Navarro <desiree.navarro@factorial.co>
2025-05-29 12:00:50 +0200

    Update packages/react/src/experimental/Information/Avatars/BaseAvatar/index.tsx
    Co-authored-by: Sergio Carracedo Martinez <sergio.carracedo@factorial.co>

--- packages/react/src/experimental/Information/Avatars/BaseAvatar/index.tsx
+++ packages/react/src/experimental/Information/Avatars/BaseAvatar/index.tsx
@@ -8,11 +8,14 @@ import { Badge, BadgeProps } from "../../Badge"
 import { getAvatarColor, getInitials, getMask } from "./utils"
 
 const getBadgeSize = (size: ShadAvatarProps["size"]) => {
-  if (size === "xlarge") return "lg"
-  if (size === "large") return "md"
-  if (size === "xsmall") return "xs"
-  if (size === "small") return "sm"
-  return "sm"
+  const sizeMap = {
+   xlarge: : "lg",
+   large: : "md",
+   small: "sm",
+   xsmall: "xs"
+}
+
+return sizeMap[size] || sizeMap.small
 }
 
 type ShadAvatarProps = ComponentProps<typeof AvatarComponent>
