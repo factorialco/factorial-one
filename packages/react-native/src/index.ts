cddd4f919115e0aff9ae6811bb54654ca609dca1
Daniel Maza <39518742+dmaza81@users.noreply.github.com>
2025-06-03 14:22:45 +0200

    Fct 30393 Counter and OnePreset component F1 Mobile (#1977)
    * feat: add Counter component to native one
    
    * feat: init stage
    
    * fix: remove unnecesary view
    
    * feat: improve showcase
    
    * test: improve snapshot test
    
    * feat: some setups for Counter component
    
    * feat: add OnePreset component
    
    * test: add test for OnePreset component
    
    * test: redo snapshot
    
    * fix: dark mode
    
    * fix: grow-0 for buttons
    
    * feat: add classes
    
    * fix: remove unnecesary class
    
    ---------
    
    Co-authored-by: Daniel Maza <daniel.maza@daniel.maza>

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -6,6 +6,8 @@ export * from "./components/ExampleComponent";
 export * from "./components/Icon";
 export * from "./components/IconAvatar";
 export * from "./components/Information/ModuleAvatar";
+export * from "./components/Counter";
+export * from "./components/OnePreset";
 
 // Export icons
 export * from "./icons";
