3c5d987a4784cba453e27a69191c6b63e68637b9
Daniel Maza <daniel.maza@daniel.maza>
2025-06-03 17:23:53 +0200

    feat: refactor removing color props

--- packages/react-native/src/components/Tags/AlertTab/index.tsx
+++ packages/react-native/src/components/Tags/AlertTab/index.tsx
@@ -3,7 +3,6 @@ import { cn } from "../../../lib/utils";
 import { useTextFormatEnforcer } from "../../../lib/text";
 import { Icon, IconType } from "../../Icon";
 import { AlertCircle, InfoCircle, Warning } from "../../../icons/app";
-import { IconColorName } from "../../../lib/colors";
 
 type Level = "info" | "warning" | "critical";
 
@@ -41,15 +40,13 @@ export const AlertTag = ({ text, level }: Props) => {
       left={
         <Icon
           icon={iconMap[level]}
-          color={
-            cn(
-              {
-                info: "text-f1-foreground-info",
-                warning: "text-f1-foreground-warning",
-                critical: "text-f1-foreground-critical",
-              }[level],
-            ) as IconColorName
-          }
+          className={cn(
+            {
+              info: "text-f1-foreground-info",
+              warning: "text-f1-foreground-warning",
+              critical: "text-f1-foreground-critical",
+            }[level],
+          )}
           size="md"
           aria-hidden
         />
