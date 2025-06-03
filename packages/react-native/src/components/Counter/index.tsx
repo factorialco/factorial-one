24007e625149a9a6373185d9acd120f2cd08142f
Daniel Maza <daniel.maza@daniel.maza>
2025-06-03 11:09:29 +0200

    feat: some setups for Counter component

--- packages/react-native/src/components/Counter/index.tsx
+++ packages/react-native/src/components/Counter/index.tsx
@@ -1,18 +1,18 @@
 import { cva, type VariantProps } from "cva";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { cn } from "../../lib/utils";
 
-const counterVariants = cva({
-  base: "inline-flex items-center justify-center whitespace-nowrap rounded-xs text-sm font-medium tabular-nums transition-all",
+const counterContainerVariants = cva({
+  base: "flex items-center justify-center rounded-xs transition-all flex-grow-0 px-0.5",
   variants: {
     size: {
-      md: "min-w-5 h-5 p-0.5",
-      sm: "min-w-4 h-4 px-0.5",
+      md: "min-w-5 h-5",
+      sm: "min-w-4 h-4",
     },
     type: {
-      default: "bg-f1-background-secondary outline outline-1 outline-f1-border",
-      selected: "bg-f1-background-selected-bold text-f1-foreground-inverse",
-      bold: "bg-f1-background-accent-bold text-f1-foreground-inverse",
+      default: "bg-f1-background-secondary border border-f1-border",
+      selected: "bg-f1-background-selected-bold outline-f1-border-selected",
+      bold: "bg-f1-background-accent-bold",
     },
   },
   defaultVariants: {
@@ -21,22 +21,37 @@ const counterVariants = cva({
   },
 });
 
+const counterTextVariants = cva({
+  base: "text-center text-sm font-medium tabular-nums whitespace-nowrap",
+  variants: {
+    size: {
+      md: "",
+      sm: "leading-none py-0.5",
+    },
+    type: {
+      default: "text-f1-foreground",
+      selected: "text-f1-foreground-inverse",
+      bold: "text-f1-foreground-inverse",
+    },
+  },
+  defaultVariants: {
+    type: "default",
+  },
+});
+
 type CounterProps = {
   value: number;
   maxValue?: number;
-} & VariantProps<typeof counterVariants>;
+} & VariantProps<typeof counterContainerVariants>;
 
 export function Counter({ size, type, value, maxValue }: CounterProps) {
   const displayValue = maxValue && value > maxValue ? `+${maxValue}` : value;
 
   return (
-    <Text
-      className={cn(
-        "text-center text-f1-foreground",
-        counterVariants({ size, type }),
-      )}
-    >
-      {displayValue}
-    </Text>
+    <View className={cn(counterContainerVariants({ size, type }))}>
+      <Text className={cn(counterTextVariants({ type, size }))}>
+        {displayValue}
+      </Text>
+    </View>
   );
 }
