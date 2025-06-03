d001c54d6ec293226b669bbcee0aa35a770ef4f1
Segio Carracedo <sergio.carracedo@factorial.co>
2025-06-03 14:38:56 +0200

    refactor: deprecated icon

--- packages/react/src/experimental/OneDataCollection/visualizations/property/types/person.tsx
+++ packages/react/src/experimental/OneDataCollection/visualizations/property/types/person.tsx
@@ -1,24 +1,9 @@
-import { IconType } from "@/components/Utilities/Icon"
-import { BadgeProps } from "@/experimental/exports"
 import { Avatar } from "@/experimental/Information/Avatars/Avatar"
 
 export interface PersonValue {
   firstName: string
   lastName: string
   src?: string
-  badge?:
-    | {
-        type: "badge"
-        variant: BadgeProps["variant"]
-      }
-    | {
-        type: "icon"
-        icon: IconType
-      }
-    | {
-        type: "module"
-        module: ModuleId
-      }
 }
 
 export type PersonCellValue = PersonValue
