283ebbd4eb315db4dabf4923bb796c5f43fbdeb8
Sergio Carracedo Martinez <sergio.carracedo@factorial.co>
2025-06-04 10:11:44 +0200

    Feat/infilter options cache (#1765)
    * fix: in filter options
    
    * feat: datacollection infilter cache options

--- packages/react/src/experimental/OneDataCollection/Filters/FilterTypes/InFilter/InFilter.tsx
+++ packages/react/src/experimental/OneDataCollection/Filters/FilterTypes/InFilter/InFilter.tsx
@@ -78,10 +78,10 @@ export function InFilter<T extends string>({
 
   const [searchTerm, setSearchTerm] = useState("")
 
-  const { options, isLoading, error, loadOptions } = useLoadOptions(
-    schema.label,
-    schema.options.options
-  )
+  const { options, isLoading, error, loadOptions } = useLoadOptions({
+    ...schema,
+    type: "in",
+  })
 
   useEffect(() => {
     setSearchTerm("")
