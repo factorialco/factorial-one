038c35f5571a35af1ef693ff4f77b17bb533ebb4
Saul Dominguez <saulydominguez@gmail.com>
2025-06-05 17:00:30 +0200

    fix: get rid of other actions in resource header example

--- packages/react/src/experimental/Modals/OneModal/OneModal.stories.tsx
+++ packages/react/src/experimental/Modals/OneModal/OneModal.stories.tsx
@@ -188,6 +188,7 @@ export const WithResourceHeader: Story = {
             >)}
             primaryAction={undefined}
             secondaryActions={undefined}
+            otherActions={undefined}
           />
         </OneModal.Content>
       </>
