a35c3022604a6e9b9ab924ba268f29adc3d2ed56
Carlos Bertomeu <cbertomeu18@gmail.com>
2025-06-12 17:03:39 +0200

    Fct 31288 create top filters (#2063)
    * feat: inbox list improvements
    
    * feat: list improvements + filters
    
    * feat: change dates
    
    * feat: list improvements
    
    * feat: update safe area context package
    
    * feat: sync storybook
    
    * fix: remove comment
    
    * fix: snapshot
    
    * chore: debug alpha release pipeline
    
    * chore: debug alpha release pipeline
    
    * feat: keep 4.12.0 v for safe area package
    
    * feat: add comment
    
    * feat: improvements
    
    ---------
    
    Co-authored-by: Segio Carracedo <sergio.carracedo@factorial.co>

--- packages/react-native/src/modules/inbox/Separator.tsx
+++ packages/react-native/src/modules/inbox/Separator.tsx
@@ -1,5 +1,5 @@
 import { View } from "react-native";
 
 export const Separator = () => {
-  return <View className="h-px bg-f1-foreground-disabled" />;
+  return <View className="my-2 h-px bg-f1-foreground-disabled" />;
 };
