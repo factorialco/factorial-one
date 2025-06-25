e14fa2591a51e27e8481ef5f623347940cd4f534
Saul Dominguez <saulydominguez@gmail.com>
2025-06-25 13:00:20 +0200

    fix: prevent failing test

--- packages/react/src/experimental/OneDataCollection/components/GroupHeader/__tests__/GroupHeader.test.tsx
+++ packages/react/src/experimental/OneDataCollection/components/GroupHeader/__tests__/GroupHeader.test.tsx
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react"
+import { fireEvent, screen } from "@testing-library/react"
 import { describe, expect, it, vi } from "vitest"
+import { render } from "../../../../../test-utils"
 import { GroupHeader } from "../GroupHeader"
 
 describe("GroupHeader", () => {
