c60e29e6f9825f25a0d474e0538a9405c0b33b8c
Niki Lopin <nik.lopin@factorial.co>
2025-07-16 11:25:41 +0200

    feat: export both provider and component from one place

--- packages/react/src/experimental/AiChat/index.tsx
+++ packages/react/src/experimental/AiChat/index.tsx
@@ -1,10 +1,14 @@
+import { CopilotKit } from "@copilotkit/react-core"
 import { CopilotPopup } from "@copilotkit/react-ui"
 import type { ComponentProps } from "react"
 
-export type AiChatProps = Pick<
-  ComponentProps<typeof CopilotPopup>,
-  "className" | "children"
->
+export type AiChatProps = ComponentProps<typeof CopilotPopup>
+
+export type AiChatProviderProps = ComponentProps<typeof CopilotKit>
+
+export const AiChatProvider = (props: AiChatProviderProps) => {
+  return <CopilotKit {...props} />
+}
 
 export const AiChat = (props: AiChatProps) => {
   return <CopilotPopup {...props} />
