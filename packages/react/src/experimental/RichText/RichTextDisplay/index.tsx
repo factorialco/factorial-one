640902494bc874352232da544ee05f75c67f02cc
siguenzaraul <raul.siguenza@factorial.co>
2025-07-21 16:58:40 +0200

    fix(RichTextDisplay): enhance content processing to support line breaks

--- packages/react/src/experimental/RichText/RichTextDisplay/index.tsx
+++ packages/react/src/experimental/RichText/RichTextDisplay/index.tsx
@@ -12,11 +12,18 @@ type RichTextDisplayHandle = HTMLDivElement
 
 const RichTextDisplay = forwardRef<RichTextDisplayHandle, RichTextDisplayProps>(
   function RichTextDisplay({ content, className, ...props }, ref) {
+    // convert line breaks to <br> if no HTML tags are detected to be retro compatible with the old text
+    const processedContent = /<[^>]*>/.test(content)
+      ? content
+      : content.replace(/\n/g, "<br>")
+
     return (
       <div
         ref={ref}
         className={cn("rich-text-display-container", className)}
-        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }}
+        dangerouslySetInnerHTML={{
+          __html: DOMPurify.sanitize(processedContent),
+        }}
         {...props}
       />
     )
