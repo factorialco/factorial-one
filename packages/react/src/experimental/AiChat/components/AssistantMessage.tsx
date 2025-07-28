1f55921e59af065caeddfda6ad29e0a2a9590229
Niki Lopin <nik.lopin@factorial.co>
2025-07-28 18:25:53 +0200

    feat: add on hover actions

--- packages/react/src/experimental/AiChat/components/AssistantMessage.tsx
+++ packages/react/src/experimental/AiChat/components/AssistantMessage.tsx
@@ -5,12 +5,12 @@ import { useI18n } from "@/lib/providers/i18n"
 import { cn } from "@/lib/utils"
 import { Markdown, type AssistantMessageProps } from "@copilotkit/react-ui"
 import { type TextMessage } from "@copilotkit/runtime-client-gql"
-import { useRef, useState } from "react"
+import { useCallback, useRef, useState } from "react"
 import { markdownRenderers } from "../markdownRenderers"
 
 export const AssistantMessage = ({
   message,
-  isGenerating: _isGenerating,
+  isGenerating,
   isCurrentMessage: _isCurrentMessage,
   isLoading,
   markdownTagRenderers,
@@ -21,17 +21,27 @@ export const AssistantMessage = ({
   const translations = useI18n()
   const containerRef = useRef<HTMLDivElement>(null)
   const [isHovered, setIsHovered] = useState(false)
+  const timeoutRef = useRef<NodeJS.Timeout>()
+
+  const handleMouseEnter = useCallback(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current)
+    }
+    setIsHovered(true)
+  }, [])
+
+  const handleMouseLeave = useCallback(() => {
+    timeoutRef.current = setTimeout(() => {
+      setIsHovered(false)
+    }, 150)
+  }, [])
 
   return (
     <div
-      className="relative isolate flex min-h-[20px] w-fit max-w-[330px] flex-col items-start justify-center last:pb-8"
+      className="relative isolate flex min-h-[20px] w-fit max-w-[330px] flex-col items-start justify-center last:mb-8"
       ref={containerRef}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => {
-        console.log("onMouseLeave")
-        setIsHovered(false)
-        containerRef.current?.focus()
-      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       {isLoading && <Spinner size="small" className="text-f1-foreground" />}
       {message && (
@@ -45,15 +55,19 @@ export const AssistantMessage = ({
 
           <div
             className={cn(
-              "invisible absolute bottom-1 left-0 mt-1 focus-within:visible",
-              isHovered && "visible"
+              // add paddings to make the area bigger and avoid flickering when the mouse is over the actions
+              "invisible absolute left-0 top-full py-1 pr-4 focus-within:visible",
+              isHovered && !isGenerating && "visible"
             )}
+            onMouseEnter={handleMouseEnter}
+            // no onMouseLeave attached because the div is part of the upper container with the handler attached
           >
             <div className="flex gap-1">
               <div>
                 <CopyButton
                   variant="ghost"
                   valueToCopy={message}
+                  disabled={isGenerating}
                   onCopy={(e) => {
                     e.currentTarget.blur()
                     onCopy?.(message)
@@ -67,6 +81,7 @@ export const AssistantMessage = ({
                   label={translations.actions.thumbsUp}
                   icon={ThumbsUp}
                   hideLabel
+                  disabled={isGenerating}
                   onClick={(e) => {
                     // Blocked! new version of the sdk changes the type of the `message` variable so we can pass it directly
                     // the current version does not provide enough information to create `TextMessage` correctly
@@ -82,6 +97,7 @@ export const AssistantMessage = ({
                   label={translations.actions.thumbsDown}
                   icon={ThumbsDown}
                   hideLabel
+                  disabled={isGenerating}
                   onClick={(e) => {
                     // Blocked! new version of the sdk changes the type of the `message` variable so we can pass it directly
                     // the current version does not provide enough information to create `TextMessage` correctly
