4bc3642382b0a1900f086c821383c3b632d86fc5
Dani Moreno <hi@dazlog.com>
2025-07-30 17:34:13 +0200

    fix: remove test story

--- packages/react/src/experimental/OneCard/__stories__/OneCard.stories.tsx
+++ packages/react/src/experimental/OneCard/__stories__/OneCard.stories.tsx
@@ -13,7 +13,6 @@ import image from "@storybook-static/avatars/person04.jpg"
 import type { Meta, StoryObj } from "@storybook/react-vite"
 import { useState } from "react"
 import { fn } from "storybook/test"
-import { CardInternal } from "../CardInternal"
 import { OneCard } from "../OneCard"
 
 const SlotComponent = () => {
@@ -238,11 +237,5 @@ export const Compact: Story = {
   args: {
     ...WithActions.args,
     compact: true,
-  },
-}
-
-export const CompactWithVerticalMetadata: Story = {
-  render: () => {
-    return <CardInternal {...Compact.args} forceVerticalMetadata />
   },
 }
