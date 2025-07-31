58a1f362f73b97090367037ab37eaac7afd6ec2f
Nik Lopin <nik.lopin@factorial.co>
2025-07-31 11:58:49 +0200

    feat: create AiChat component (#2259)
    * feat: create AiChat component
    
    * fix: export the popup instead of provider
    
    * fix: stories
    
    * feat: export both provider and component from one place
    
    * chore: remove dev dependencies
    
    * chore: make copilotkit an external dependency
    
    * feat: custom header
    
    * feat: add custom components
    
    * chore: update broken lock file
    
    * fix: add missing translation
    
    * feat: show focus on the One button
    
    * feat: input and user messages
    
    * feat: add markdown styles
    
    * refactor: split components into files
    
    * feat: add on hover actions
    
    * fix: call callbacks
    
    * chore: add svg icons to core assets
    
    * feat: add on hover actions
    
    * refactor: create custom chat window dialog
    
    I want to avoid making the bottom right position accessible for others
    
    * feat: show status of the like/dislike button
    
    * feat: wrap components with experimental wrapper
    
    * feat: add button label
    
    * fix: combine markdown renderers
    
    * fix: use one state for like/dislike buttons
    
    * fix: give priority to our renderers
    
    * fix: log → warn
    
    * fix: rollback priority change
    
    * fix: a11y warnings

---
+++ packages/react/src/experimental/AiChat/index.tsx
@@ -0,0 +1,53 @@
+import { CopilotKit } from "@copilotkit/react-core"
+import { CopilotPopup } from "@copilotkit/react-ui"
+
+import { experimentalComponent } from "@/lib/experimental"
+import type { ComponentProps } from "react"
+
+import {
+  AssistantMessage,
+  ChatButton,
+  ChatHeader,
+  ChatTextarea,
+  ChatWindow,
+  MessagesContainer,
+  UserMessage,
+} from "./components"
+
+export type AiChatProps = ComponentProps<typeof CopilotPopup>
+
+export type AiChatProviderProps = Omit<
+  ComponentProps<typeof CopilotKit>,
+  "window"
+>
+
+export const AiChatProviderCmp = (props: AiChatProviderProps) => {
+  return <CopilotKit {...props} />
+}
+
+export const AiChatCmp = (props: AiChatProps) => {
+  return (
+    <CopilotPopup
+      Window={ChatWindow}
+      Header={ChatHeader}
+      Messages={MessagesContainer}
+      Button={ChatButton}
+      Input={ChatTextarea}
+      UserMessage={UserMessage}
+      AssistantMessage={AssistantMessage}
+      {...props}
+    />
+  )
+}
+
+/**
+ * @experimental This is an experimental component use it at your own risk
+ */
+const AiChat = experimentalComponent("AiChat", AiChatCmp)
+
+const AiChatProvider = experimentalComponent(
+  "AiChatProvider",
+  AiChatProviderCmp
+)
+
+export { AiChat, AiChatProvider }
