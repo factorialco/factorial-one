2e3915f6ad28df5858fa4d99263d3592be896378
Niki Lopin <nik.lopin@factorial.co>
2025-08-07 17:23:29 +0200

    fix: render subcomponents

--- packages/react/src/experimental/AiChat/components/MessagesContainer.tsx
+++ packages/react/src/experimental/AiChat/components/MessagesContainer.tsx
@@ -70,7 +70,7 @@ export const MessagesContainer = ({
       )}
       ref={messagesContainerRef}
     >
-      <motion.div className="flex flex-col" layout="position">
+      <motion.div className="flex flex-col gap-2" layout="position">
         {showWelcomeBlock && (
           <div className="flex flex-col px-2">
             <OneIcon className="my-4 h-10 w-10 cursor-pointer rounded-xl" />
