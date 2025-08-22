f18fc624fa75fcc1da0cdc09c0d46d595cd4b0af
Segio Carracedo <sergio.carracedo@factorial.co>
2025-08-22 19:41:01 +0200

    chore: report coverage

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -106,5 +106,11 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: ["./vite/vitest.setup.ts"],
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ["text", "json-summary", "json"],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
   },
 })
