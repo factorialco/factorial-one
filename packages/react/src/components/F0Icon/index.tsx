4872a6f58c9030124559d91653aaa207156eee20
Segio Carracedo <sergio.carracedo@factorial.co>
2025-09-02 17:11:00 +0200

    chore: export f0icon

--- packages/react/src/components/F0Icon/index.tsx
+++ packages/react/src/components/F0Icon/index.tsx
@@ -1,11 +1,10 @@
 import { Component } from "../../lib/component"
-
-export type { IconType as IconType } from "./F0Icon"
+export * from "./F0Icon"
 
 import { F0Icon as IconComponent } from "./F0Icon"
-export const Icon = Component(
+export const F0Icon = Component(
   {
-    name: "Icon",
+    name: "F0Icon",
     type: "info",
   },
   IconComponent
