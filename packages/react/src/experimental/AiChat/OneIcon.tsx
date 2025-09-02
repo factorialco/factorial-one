9574eaf68e54857266fd944c561fb5e100e871e6
Dani Moreno <96433370+dani-moreno@users.noreply.github.com>
2025-09-02 16:20:02 +0200

    feat(One): trigger button (#2498)

--- packages/react/src/experimental/AiChat/OneIcon.tsx
+++ packages/react/src/experimental/AiChat/OneIcon.tsx
@@ -1,67 +1,41 @@
 import type { SVGProps } from "react"
-import { Ref, forwardRef } from "react"
+import { Ref, forwardRef, useId } from "react"
 
 const OneIcon = (
-  {
-    onDarkBackground = false,
-    ...svgProps
-  }: { onDarkBackground?: boolean } & SVGProps<SVGSVGElement>,
+  svgProps: SVGProps<SVGSVGElement>,
   ref: Ref<SVGSVGElement>
 ) => {
-  const baseColor = onDarkBackground ? "#FFF" : "#25253D"
-  const [gradientOne, gradientTwo] = ["1", "2"].map((id) => baseColor + id)
+  const gradientId = useId()
+  const clipPathId = useId()
+
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="27"
-      height="26"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
       fill="none"
-      viewBox="0 0 27 26"
+      xmlns="http://www.w3.org/2000/svg"
       ref={ref}
       {...svgProps}
     >
-      <path
-        fill={baseColor}
-        d="m20.44 15.64-2.67-.53.16-.86c.4-2.5.32-4.4-.23-6a7.4 7.4 0 0 0-6.86-4.8 5.13 5.13 0 0 0-3.62 1.6L5.15 3.29A7.78 7.78 0 0 1 10.98.73c4.04 0 7.92 2.76 9.28 6.63.71 2.03.82 4.36.35 7.32l-.17.96Z"
-      />
-      <path
-        fill={baseColor}
-        d="M10.3 25.27a9.84 9.84 0 0 1-7.59-3.62C.51 18.93-.33 14.9.3 10.02l2.69.35c-.37 2.88-.35 6.88 1.83 9.57a7.2 7.2 0 0 0 5.59 2.62 7.21 7.21 0 0 0 5.5-2.82c.83-1.1 1.42-2.54 1.82-4.4l2.65.56a13.52 13.52 0 0 1-2.3 5.48 9.85 9.85 0 0 1-7.62 3.89h-.17Z"
-      />
-      <path
-        fill={`url(#${gradientOne})`}
-        d="M.3 10.02c-.63 4.89.2 8.9 2.41 11.63l2.11-1.71c-2.18-2.7-2.2-6.7-1.83-9.57l-2.7-.35Z"
-      />
-      <path
-        fill={`url(#${gradientTwo})`}
-        d="M17.7 16.9c-1 0-2-.05-3.05-.18-2.45-.3-4.35-.85-5.99-1.74a9.47 9.47 0 0 1-4.44-4.93 7.06 7.06 0 0 1 1-6.86l2.1 1.74a4.37 4.37 0 0 0-.56 4.19 6.77 6.77 0 0 0 3.2 3.48c1.33.72 2.92 1.18 5 1.43 2.77.33 5.3.17 7.5-.47 1.02-.3 1.99-.7 2.88-1.2l1.33 2.36c-1.07.6-2.23 1.1-3.45 1.45-1.7.49-3.56.74-5.53.74Z"
-      />
       <defs>
-        <linearGradient
-          id={gradientOne}
-          x1="1.5"
-          x2="4"
-          y1="10.5"
-          y2="20.5"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="#323E56" />
-          <stop offset="1" stopColor={baseColor} />
+        <linearGradient id={gradientId} x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="0%" stopColor="#E55619" />
+          <stop offset="100%" stopColor="#A1ADE5" />
         </linearGradient>
-        <linearGradient
-          id={gradientTwo}
-          x1=".52"
-          x2="24.02"
-          y1="2.17"
-          y2="18.63"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop offset=".27" stopColor={baseColor} />
-          <stop offset=".54" stopColor="#FF355E" />
-          <stop offset=".62" stopColor="#FB345D" />
-          <stop offset=".69" stopColor="#F1345C" />
-        </linearGradient>
+        <clipPath id={clipPathId}>
+          <path d="M15.9939 24.8399C19.6511 24.8399 23.2335 26.0603 26.0525 28.4219C23.2335 30.7072 19.651 32.001 15.9939 32.001C12.1849 32.0009 8.67993 30.6307 5.93728 28.4219C8.75621 26.1365 12.3369 24.84 15.9939 24.8399Z" />
+          <path d="M3.57986 5.94142C5.86509 8.76031 7.1608 12.3412 7.16092 15.9981C7.16092 19.6551 5.94136 23.2376 3.57986 26.0567C1.29443 23.2376 -0.000215909 19.6552 -0.00021553 15.9981C-0.000100728 12.1889 1.37091 8.6841 3.57986 5.94142Z" />
+          <path d="M28.4236 5.94142C30.7088 8.76031 32.0046 12.3412 32.0047 15.9981C32.0047 19.6551 30.7851 23.2376 28.4236 26.0567C26.1382 23.2376 24.8435 19.6552 24.8435 15.9981C24.8436 12.1889 26.2147 8.6841 28.4236 5.94142Z" />
+          <path d="M15.9939 1.33514e-05C19.6511 1.37386e-05 23.2335 1.22043 26.0525 3.58204C23.2335 5.86737 19.651 7.16115 15.9939 7.16115C12.1849 7.16103 8.67993 5.79089 5.93728 3.58204C8.75621 1.29671 12.3369 0.000125175 15.9939 1.33514e-05Z" />
+        </clipPath>
       </defs>
+      <rect
+        width="32"
+        height="32"
+        fill={`url(#${gradientId})`}
+        clipPath={`url(#${clipPathId})`}
+      />
     </svg>
   )
 }
