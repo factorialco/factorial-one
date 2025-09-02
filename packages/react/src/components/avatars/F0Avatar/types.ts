6e05bb0e2892f116833d23ac6d18bb272af0ae82
Saul Dominguez <saulydominguez@gmail.com>
2025-09-02 13:47:14 +0200

    feat: add support to show file avatar in data collection

--- packages/react/src/components/avatars/F0Avatar/types.ts
+++ packages/react/src/components/avatars/F0Avatar/types.ts
@@ -2,6 +2,7 @@ import { BadgeProps } from "@/experimental/Information/Badge"
 import { ModuleId } from "@/experimental/Information/ModuleAvatar"
 import { ComponentProps } from "react"
 import { F0AvatarCompany } from "../F0AvatarCompany"
+import { F0AvatarFile } from "../F0AvatarFile"
 import { F0AvatarPerson } from "../F0AvatarPerson"
 import { F0AvatarTeam } from "../F0AvatarTeam"
 
@@ -21,8 +22,16 @@ export type AvatarBadge = (
 type PersonAvatarProps = ComponentProps<typeof F0AvatarPerson>
 type TeamAvatarProps = ComponentProps<typeof F0AvatarTeam>
 type CompanyAvatarProps = ComponentProps<typeof F0AvatarCompany>
+type FileAvatarProps = ComponentProps<typeof F0AvatarFile>
 
-export type AvatarVariant =
-  | ({ type: "person" } & Omit<PersonAvatarProps, "size">)
-  | ({ type: "team" } & Omit<TeamAvatarProps, "size">)
-  | ({ type: "company" } & Omit<CompanyAvatarProps, "size">)
+type DistributiveOmit<T, K extends PropertyKey> = T extends unknown
+  ? Omit<T, K>
+  : never
+
+export type AvatarVariantWithSize =
+  | ({ type: "person" } & PersonAvatarProps)
+  | ({ type: "team" } & TeamAvatarProps)
+  | ({ type: "company" } & CompanyAvatarProps)
+  | ({ type: "file" } & FileAvatarProps)
+
+export type AvatarVariant = DistributiveOmit<AvatarVariantWithSize, "size">
