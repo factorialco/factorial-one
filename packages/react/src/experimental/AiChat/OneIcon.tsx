bde43e0add32efb9c2a162a2748f0f9fc78e4690
Dani Moreno <hi@dazlog.com>
2025-09-04 14:14:35 +0200

    fix: rotate each piece

--- packages/react/src/experimental/AiChat/OneIcon.tsx
+++ packages/react/src/experimental/AiChat/OneIcon.tsx
@@ -19,6 +19,37 @@ const sizeVariants = cva({
   defaultVariants: { size: "md" },
 })
 
+const pieces = [
+  {
+    id: "bottom",
+    delay: 2.6,
+    transformOrigin: "center 89%",
+    rotateAxis: "1, 0, 0",
+    path: "M15.9939 24.8399C19.6511 24.8399 23.2335 26.0603 26.0525 28.4219C23.2335 30.7072 19.651 32.001 15.9939 32.001C12.1849 32.0009 8.67993 30.6307 5.93728 28.4219C8.75621 26.1365 12.3369 24.84 15.9939 24.8399Z",
+  },
+  {
+    id: "left",
+    delay: 2.2,
+    transformOrigin: "11% center",
+    rotateAxis: "0, 1, 0",
+    path: "M3.57986 5.94142C5.86509 8.76031 7.1608 12.3412 7.16092 15.9981C7.16092 19.6551 5.94136 23.2376 3.57986 26.0567C1.29443 23.2376 -0.000215909 19.6552 -0.00021553 15.9981C-0.000100728 12.1889 1.37091 8.6841 3.57986 5.94142Z",
+  },
+  {
+    id: "right",
+    delay: 2.4,
+    transformOrigin: "88.5% center",
+    rotateAxis: "0, 1, 0",
+    path: "M28.4236 5.94142C30.7088 8.76031 32.0046 12.3412 32.0047 15.9981C32.0047 19.6551 30.7851 23.2376 28.4236 26.0567C26.1382 23.2376 24.8435 19.6552 24.8435 15.9981C24.8436 12.1889 26.2147 8.6841 28.4236 5.94142Z",
+  },
+  {
+    id: "top",
+    delay: 2,
+    transformOrigin: "center 11%",
+    rotateAxis: "1, 0, 0",
+    path: "M15.9939 1.33514e-05C19.6511 1.37386e-05 23.2335 1.22043 26.0525 3.58204C23.2335 5.86737 19.651 7.16115 15.9939 7.16115C12.1849 7.16103 8.67993 5.79089 5.93728 3.58204C8.75621 1.29671 12.3369 0.000125175 15.9939 1.33514e-05Z",
+  },
+]
+
 const OneIcon = (
   { spin = false, size = "md", ...svgProps }: OneIconProps,
   ref: Ref<SVGSVGElement>
@@ -38,7 +69,7 @@ const OneIcon = (
     <div
       className={cn(sizeVariants({ size }), className)}
       style={{
-        perspective: spin ? "100px" : undefined,
+        perspective: spin ? "10px" : undefined,
         transformStyle: spin ? "preserve-3d" : undefined,
       }}
     >
@@ -51,7 +82,6 @@ const OneIcon = (
         ref={ref}
         animate={{
           "--gradient-angle": ["0deg", "360deg"],
-          "--rotate3d-angle": ["0deg", "360deg"],
         }}
         transition={{
           "--gradient-angle": {
@@ -59,47 +89,70 @@ const OneIcon = (
             ease: "linear",
             repeat: Infinity,
           },
-          "--rotate3d-angle": {
-            delay: 2,
-            duration: 2,
-            ease: [0.65, 0, 0.35, 1],
-          },
         }}
         style={
           {
             "--gradient-angle": "0deg",
-            "--rotate3d-angle": "0deg",
-            transform: spin
-              ? "rotate3d(0, 1, 0, var(--rotate3d-angle))"
-              : undefined,
             ...safeSvgProps.style,
           } as React.CSSProperties
         }
         {...(({ style: _style, ...rest }) => rest)(safeSvgProps)}
       >
         <defs>
-          <clipPath id={clipPathId}>
-            <path d="M15.9939 24.8399C19.6511 24.8399 23.2335 26.0603 26.0525 28.4219C23.2335 30.7072 19.651 32.001 15.9939 32.001C12.1849 32.0009 8.67993 30.6307 5.93728 28.4219C8.75621 26.1365 12.3369 24.84 15.9939 24.8399Z" />
-            <path d="M3.57986 5.94142C5.86509 8.76031 7.1608 12.3412 7.16092 15.9981C7.16092 19.6551 5.94136 23.2376 3.57986 26.0567C1.29443 23.2376 -0.000215909 19.6552 -0.00021553 15.9981C-0.000100728 12.1889 1.37091 8.6841 3.57986 5.94142Z" />
-            <path d="M28.4236 5.94142C30.7088 8.76031 32.0046 12.3412 32.0047 15.9981C32.0047 19.6551 30.7851 23.2376 28.4236 26.0567C26.1382 23.2376 24.8435 19.6552 24.8435 15.9981C24.8436 12.1889 26.2147 8.6841 28.4236 5.94142Z" />
-            <path d="M15.9939 1.33514e-05C19.6511 1.37386e-05 23.2335 1.22043 26.0525 3.58204C23.2335 5.86737 19.651 7.16115 15.9939 7.16115C12.1849 7.16103 8.67993 5.79089 5.93728 3.58204C8.75621 1.29671 12.3369 0.000125175 15.9939 1.33514e-05Z" />
-          </clipPath>
+          {pieces.map((piece) => (
+            <clipPath key={piece.id} id={`${clipPathId}-${piece.id}`}>
+              <path d={piece.path} />
+            </clipPath>
+          ))}
         </defs>
-        <foreignObject
-          x="0"
-          y="0"
-          width="32"
-          height="32"
-          clipPath={`url(#${clipPathId})`}
-        >
-          <div
-            style={{
-              width: "100%",
-              height: "100%",
-              background: `conic-gradient(from var(--gradient-angle) at 50% 50%, #E55619 0%, #A1ADE5 33%, #E51943 66%, #E55619 100%)`,
+
+        {pieces.map((piece) => (
+          <motion.foreignObject
+            key={piece.id}
+            x="0"
+            y="0"
+            width="32"
+            height="32"
+            clipPath={`url(#${clipPathId}-${piece.id})`}
+            animate={{
+              "--rotate3d-angle": ["0deg", "180deg", "180deg", "360deg"],
+              filter: spin
+                ? ["blur(0px)", "blur(8px)", "blur(0px)"]
+                : undefined,
             }}
-          />
-        </foreignObject>
+            transition={{
+              "--rotate3d-angle": {
+                delay: spin ? piece.delay : 0,
+                duration: 1.8,
+                ease: [0.65, 0, 0.35, 1],
+                times: [0, 0.99, 0.9999, 1],
+              },
+              filter: {
+                delay: spin ? piece.delay : 0,
+                duration: 1.8,
+                ease: [0.65, 0, 0.35, 1],
+                times: [0, 0.5, 1],
+              },
+            }}
+            style={
+              {
+                "--rotate3d-angle": "0deg",
+                transform: spin
+                  ? `rotate3d(${piece.rotateAxis}, var(--rotate3d-angle))`
+                  : undefined,
+                transformOrigin: piece.transformOrigin,
+              } as React.CSSProperties
+            }
+          >
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+                background: `conic-gradient(from var(--gradient-angle) at 50% 50%, #E55619 0%, #A1ADE5 33%, #E51943 66%, #E55619 100%)`,
+              }}
+            />
+          </motion.foreignObject>
+        ))}
       </motion.svg>
     </div>
   )
