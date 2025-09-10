1da1f561c58056bea48381b384a30818dc884d81
Dani Moreno <hi@dazlog.com>
2025-09-10 15:59:14 +0200

    fix(CommunityPost): fix comment button growing unexpectedly

--- packages/react/src/experimental/Information/Reactions/index.tsx
+++ packages/react/src/experimental/Information/Reactions/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/Actions/Button"
+import { IconType } from "@/components/F0Icon"
 import { Picker } from "./Picker"
 import { Reaction, ReactionProps } from "./reaction"
 
@@ -5,11 +7,31 @@ export interface ReactionsProps {
   items: ReactionProps[]
   onInteraction?: (emoji: string) => void
   locale?: string
+  action?: {
+    label: string
+    icon: IconType
+    onClick: () => void
+  }
 }
 
-export function Reactions({ items, onInteraction, locale }: ReactionsProps) {
+export function Reactions({
+  items,
+  onInteraction,
+  locale,
+  action,
+}: ReactionsProps) {
   return (
     <div className="flex flex-wrap gap-2">
+      {action && (
+        <Button
+          label={action.label}
+          icon={action.icon}
+          onClick={action.onClick}
+          variant="outline"
+          round
+          hideLabel
+        />
+      )}
       <Picker onSelect={onInteraction} locale={locale} />
       {items.map((item) => (
         <Reaction
