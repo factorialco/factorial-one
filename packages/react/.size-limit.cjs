baba9001304d8820bad387ce98f8d8e047261c8a
Nik Lopin <nik.lopin@factorial.co>
2025-09-12 17:11:19 +0200

    chore: fix size-limit configuration (#2582)
    * chore: test default script
    
    * chore: add permissions
    
    * chore: remove time estimation
    
    * chore(ci): add loading time

--- packages/react/.size-limit.cjs
+++ packages/react/.size-limit.cjs
@@ -2,14 +2,17 @@ module.exports = [
   {
     path: "dist/f0.js",
     name: "JS: Stable",
+    running: false,
   },
   {
     path: "dist/experimental.js",
     name: "JS: Experimental",
+    running: false,
   },
   {
     path: "dist/*.css",
     name: "CSS",
+    running: false,
     modifyEsbuildConfig: (config) => ({
       ...config,
       external: ["*.woff"],
